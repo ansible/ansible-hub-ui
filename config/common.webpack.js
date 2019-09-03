7ba1a74c4b9ff6407dce894926a9591d22ae09b8
Ryan Felton <rfelton@redhat.com>
2019-09-03 15:59:55 -0400

    added common.webpack.js and fixed error in dev.webpack.config.js

---
+++ config/common.webpack.js
@@ -0,0 +1,36 @@
+/* global require, module, __dirname, process */
+
+const path = require('path');
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
+const gitRevisionPlugin = new GitRevisionPlugin({
+    branch: true
+});
+const entry = process.env.NODE_ENV === 'production' ?
+    path.resolve(__dirname, '../src/entry.js') :
+    path.resolve(__dirname, '../src/entry-dev.js');
+
+const gitBranch = process.env.BRANCH || gitRevisionPlugin.branch();
+const betaBranch =
+    gitBranch === 'master' ||
+    gitBranch === 'qa-beta' ||
+    gitBranch === 'prod-beta';
+
+let deploymentEnv = 'apps';
+let release = '';
+
+if ((process.env.NODE_ENV === 'production' && betaBranch) || process.env.BUILD_BETA === 'true') {
+    deploymentEnv = 'beta/apps';
+    release = 'beta';
+}
+
+const publicPath = `/${deploymentEnv}/Ansible/`;
+
+module.exports = {
+    paths: {
+        entry,
+        public: path.resolve(__dirname, '../dist'),
+        publicPath
+    },
+    deploymentEnv,
+    release
+};
