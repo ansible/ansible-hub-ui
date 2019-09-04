24abeea1dc05c0826a3422d5523d3594bc5fa377
Ryan Felton <rfelton@redhat.com>
2019-09-04 09:49:23 -0400

    updated prod webpack config

--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -2,9 +2,20 @@
 /* global require, module, __dirname */
 const { resolve } = require('path');
 const config = require('@redhat-cloud-services/frontend-components-config');
+const commonWPconfig = require('./common.webpack.js');
 const { config: webpackConfig, plugins } = config({
     rootFolder: resolve(__dirname, '../')
 });
+
+webpackConfig.serve = {
+    content: commonWPconfig.paths.public,
+    port: 8002,
+    prod: {
+        publicPath: commonWPconfig.paths.publicPath
+    },
+    // https://github.com/webpack-contrib/webpack-serve/blob/master/docs/addons/history-fallback.config.js
+    add: app => app.use(convert(history({})))
+};
 
 // Override sections of the webpack config to work with TypeScript
 const newWebpackConfig = {
