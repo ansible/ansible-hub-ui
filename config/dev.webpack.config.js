d1fe55945ac40f24463db7a8dc0f1d17533a26c4
Ryan Dillin Felton <ryandillinfelton@gmail.com>
2019-09-04 13:48:13 -0400

    changes for build directory (#26)
    * changes for build directory
    
    * added common.webpack.js and fixed error in dev.webpack.config.js
    
    * updated prod webpack config
    
    * changed ansible in path to lower case

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -1,6 +1,7 @@
 /* global require, module, __dirname */
 const { resolve } = require('path');
 const config = require('@redhat-cloud-services/frontend-components-config');
+const commonWPconfig = require('./common.webpack.js');
 const TSOverrides = require('./webpack-ts-overrides');
 
 const { config: webpackConfig, plugins } = config({
@@ -8,6 +9,16 @@ const { config: webpackConfig, plugins } = config({
     debug: true,
     https: true,
 });
+
+webpackConfig.serve = {
+    content: commonWPconfig.paths.public,
+    port: 8002,
+    dev: {
+        publicPath: commonWPconfig.paths.publicPath
+    },
+    // https://github.com/webpack-contrib/webpack-serve/blob/master/docs/addons/history-fallback.config.js
+    add: app => app.use(convert(history({})))
+};
 
 // Override sections of the webpack config to work with TypeScript
 const newWebpackConfig = {
