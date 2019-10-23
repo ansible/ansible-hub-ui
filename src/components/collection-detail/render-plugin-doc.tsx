ed75c815cf05f06ef7054806b2d9c4bb60e13ecb
David Newswanger <dnewswan@redhat.com>
2019-10-23 09:14:51 -0400

    Fix error catching on plugin docs.

--- src/components/collection-detail/render-plugin-doc.tsx
+++ src/components/collection-detail/render-plugin-doc.tsx
@@ -82,78 +82,88 @@ export class RenderPluginDoc extends React.Component<IProps, IState> {
     render() {
         const { plugin } = this.props;
 
-        const doc: PluginDoc = this.parseDocString(plugin);
-        const example: string = this.parseExamples(plugin);
-        const returnVals: ReturnedValue[] = this.parseReturn(plugin);
-
-        const content: any = {
-            synopsis: this.renderSynopsis(doc),
-            parameters: this.renderParameters(
-                doc.options,
-                plugin.content_type,
-                this.subOptionsMaxDepth,
-            ),
-            notes: this.renderNotes(doc),
-            examples: this.renderExample(example),
-            'return-values': this.renderReturnValues(
-                returnVals,
-                this.returnContainMaxDepth,
-            ),
-        };
-
         if (!this.state.renderError) {
-            return (
-                <div className='pf-c-content'>
-                    <h1>
-                        {plugin.content_type} > {plugin.content_name}
-                    </h1>
-                    <br />
-                    {this.renderShortDescription(doc)}
-                    {this.renderDeprecated(doc, plugin.content_name)}
-                    {this.renderTableOfContents(content)}
-                    {content['synopsis']}
-                    {this.renderRequirements(doc)}
-                    {content['parameters']}
-                    {content['notes']}
-                    {content['examples']}
-                    {content['return-values']}
-                </div>
-            );
-        } else {
-            // There's a good chance that something about the plugin doc data will
-            // be malformed since it isn't validated. When that hapens, show an
-            // error instead of crashing the whole app
-            return (
-                <React.Fragment>
-                    <Alert
-                        isInline
-                        variant='warning'
-                        title='Documentation Syntax Error: cannot parse plugin documention.'
-                    />
-                    <br />
+            // componentDidCatch doesn't seem to be able to catch errors that
+            // are thrown outside of return(), so we'll wrap everything in a
+            // try just in case
+            try {
+                const doc: PluginDoc = this.parseDocString(plugin);
+                const example: string = this.parseExamples(plugin);
+                const returnVals: ReturnedValue[] = this.parseReturn(plugin);
+                const content: any = {
+                    synopsis: this.renderSynopsis(doc),
+                    parameters: this.renderParameters(
+                        doc.options,
+                        plugin.content_type,
+                        this.subOptionsMaxDepth,
+                    ),
+                    notes: this.renderNotes(doc),
+                    examples: this.renderExample(example),
+                    'return-values': this.renderReturnValues(
+                        returnVals,
+                        this.returnContainMaxDepth,
+                    ),
+                };
+
+                return (
                     <div className='pf-c-content'>
-                        {plugin.content_type && plugin.content_name ? (
-                            <h1>
-                                {plugin.content_type} > {plugin.content_name}
-                            </h1>
-                        ) : null}
-                        <p>
-                            The documentation object for this plugin seems to
-                            contain invalid syntax that makes it impossible for
-                            Automation Hub to parse. You can still look at the
-                            unformatted documentation object bellow if you need
-                            to.
-                        </p>
-
-                        <h2>Unformatted Documentation</h2>
-
-                        <pre className='plugin-raw'>
-                            {JSON.stringify(plugin, null, 2)}
-                        </pre>
+                        <h1>
+                            {plugin.content_type} > {plugin.content_name}
+                        </h1>
+                        <br />
+                        {this.renderShortDescription(doc)}
+                        {this.renderDeprecated(doc, plugin.content_name)}
+                        {this.renderTableOfContents(content)}
+                        {content['synopsis']}
+                        {this.renderRequirements(doc)}
+                        {content['parameters']}
+                        {content['notes']}
+                        {content['examples']}
+                        {content['return-values']}
                     </div>
-                </React.Fragment>
-            );
+                );
+            } catch (err) {
+                console.log(err);
+                return this.renderError(plugin);
+            }
+        } else {
+            return this.renderError(plugin);
         }
+    }
+
+    private renderError(plugin) {
+        // There's a good chance that something about the plugin doc data will
+        // be malformed since it isn't validated. When that hapens, show an
+        // error instead of crashing the whole app
+        return (
+            <React.Fragment>
+                <Alert
+                    isInline
+                    variant='warning'
+                    title='Documentation Syntax Error: cannot parse plugin documention.'
+                />
+                <br />
+                <div className='pf-c-content'>
+                    {plugin.content_type && plugin.content_name ? (
+                        <h1>
+                            {plugin.content_type} > {plugin.content_name}
+                        </h1>
+                    ) : null}
+                    <p>
+                        The documentation object for this plugin seems to
+                        contain invalid syntax that makes it impossible for
+                        Automation Hub to parse. You can still look at the
+                        unformatted documentation object bellow if you need to.
+                    </p>
+
+                    <h2>Unformatted Documentation</h2>
+
+                    <pre className='plugin-raw'>
+                        {JSON.stringify(plugin, null, 2)}
+                    </pre>
+                </div>
+            </React.Fragment>
+        );
     }
 
     private parseDocString(plugin: PluginContentType): PluginDoc {
