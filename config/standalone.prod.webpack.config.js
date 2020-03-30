d1de188c96aa96f819207b748d7e0d20423d813f
David Newswanger <dnewswan@redhat.com>
2020-03-30 13:34:42 -0400

    Add npm run build-standalone command.

---
+++ config/standalone.prod.webpack.config.js
@@ -0,0 +1,12 @@
+const webpackBase = require('./webpack.base.config');
+
+// Compile configuration for stnadalone mode
+module.exports = webpackBase({
+  API_HOST: '',
+  API_BASE_PATH: '/api/automation-hub/',
+  UI_BASE_PATH: '',
+  DEPLOYMENT_MODE: 'standalone',
+  UI_USE_HTTPS: false,
+  UI_DEBUG: false,
+  TARGET_ENVIRONMENT: 'prod',
+});
