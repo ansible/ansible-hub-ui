84d0adf6076785f283011bf2edab8ef56ba9d241
David Newswanger <dnewswan@redhat.com>
2020-05-07 01:01:49 -0400

    Fix weird API_HOST issue

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const custom = require('../config/webpack-ts-overrides');
+const webpack = require('webpack');
 
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
@@ -12,6 +13,15 @@ module.exports = {
         filename: '[name].css',
         chunkFilename: '[id].css',
         ignoreOrder: false,
+      }),
+    );
+
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        API_HOST: JSON.stringify(''),
+        API_BASE_PATH: JSON.stringify(''),
+        UI_BASE_PATH: JSON.stringify(''),
+        DEPLOYMENT_MODE: JSON.stringify('standalone'),
       }),
     );
 
