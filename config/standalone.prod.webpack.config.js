864310ae19dad3905a4ab58d93e6919db660d9cb
David Newswanger <dnewswan@redhat.com>
2020-05-12 12:31:55 -0400

    Change standalone api url to /api/galaxy

--- config/standalone.prod.webpack.config.js
+++ config/standalone.prod.webpack.config.js
@@ -3,7 +3,7 @@ const webpackBase = require('./webpack.base.config');
 // Compile configuration for stnadalone mode
 module.exports = webpackBase({
   API_HOST: '',
-  API_BASE_PATH: '/api/automation-hub/',
+  API_BASE_PATH: '/api/galaxy/',
   UI_BASE_PATH: '/ui/',
   DEPLOYMENT_MODE: 'standalone',
   UI_USE_HTTPS: false,
