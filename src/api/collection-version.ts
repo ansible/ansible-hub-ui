7f1d32d825b9a2bdc7a82b76fb5f99ced2cab8ac
Zita Nemeckova <znemecko@redhat.com>
2020-07-03 15:30:45 +0200

    Remove setCertifiation

--- src/api/collection-version.ts
+++ src/api/collection-version.ts
@@ -1,27 +1,10 @@
 import { BaseAPI } from './base';
-import { CertificationStatus } from '../api';
 
 export class API extends BaseAPI {
   apiPath = 'v3/_ui/collection-versions/';
 
   constructor() {
     super();
-  }
-
-  setCertifiation(
-    namespace: string,
-    collection: string,
-    version: string,
-    certification: CertificationStatus,
-  ) {
-    const id = `${namespace}/${collection}/${version}/certified`;
-
-    return this.update(id, {
-      namespace: namespace,
-      name: collection,
-      version: version,
-      certification: certification,
-    });
   }
 
   setRepository(
