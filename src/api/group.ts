b104673279f9ee403c46cbc8a43b4b0e5f2021be
ZitaNemeckova <znemecko@redhat.com>
2020-09-04 15:25:36 +0200

    Add Group management page (#184)
    * Add Group management page
    
    * Add Detail page
    
    * Add Users
    
    * Functionality: Add a Group
    
    * Delete group
    
    * Functionality: Edit Gtoup name
    
    * Fix filter by group name
    
    * Functionality: Remove a User from a Group
    
    * Functionality: Add User to a Group
    
    * Fix deselect
    
    * Fix names
    
    * Add groups to Sidebar
    
    * Fix user filter
    
    * Functionality: Add/Remove/View permissions
    
    * Remove Edit util pulp fix
    
    * Fix placeholder text, add alerts when error, remove extra ids
    
    * Fix reload after adding a user to a group

--- src/api/group.ts
+++ src/api/group.ts
@@ -6,6 +6,21 @@ class API extends BaseAPI {
   constructor() {
     super();
   }
+
+  getPermissions(id) {
+    return this.http.get(
+      this.apiPath + id + '/model-permissions/?limit=100000&offset=0',
+    );
+  }
+
+  addPermission(id, data) {
+    return this.http.post(this.apiPath + id + '/model-permissions/', data);
+  }
+  removePermission(id, permissionId) {
+    return this.http.delete(
+      this.apiPath + id + '/model-permissions/' + permissionId,
+    );
+  }
 }
 
 export const GroupAPI = new API();
--- src/components/permissions/permission-chip-selector.tsx
+++ src/components/permissions/permission-chip-selector.tsx
@@ -5,7 +5,9 @@ interface IProps {
   availablePermissions: string[];
   selectedPermissions: string[];
   setSelected: (selected: string[]) => void;
-
+  isDisabled?: boolean;
+  onSelect?: (event, selection) => void;
+  onClear?: () => void;
   menuAppendTo?: 'parent' | 'inline';
 }
 
@@ -26,17 +28,27 @@ export class PermissionChipSelector extends React.Component<IProps, IState> {
         variant={SelectVariant.typeaheadMulti}
         typeAheadAriaLabel='Select permissions'
         onToggle={this.onToggle}
-        onSelect={this.onSelect}
-        onClear={this.clearSelection}
+        onSelect={!!this.props.onSelect ? this.props.onSelect : this.onSelect}
+        onClear={
+          !!this.props.onClear ? this.props.onClear : this.clearSelection
+        }
         selections={this.props.selectedPermissions}
         isOpen={this.state.isOpen}
-        placeholderText='Select permissions'
+        placeholderText={this.placeholderText()}
+        isDisabled={!!this.props.isDisabled}
       >
         {this.props.availablePermissions.map((option, index) => (
           <SelectOption key={index} value={option} />
         ))}
       </Select>
     );
+  }
+
+  private placeholderText() {
+    if (!this.props.isDisabled) {
+      return 'Select permissions';
+    }
+    return this.props.selectedPermissions.length === 0 ? 'No permission' : '';
   }
 
   private clearSelection = () => {
