5da92da76f01a000fa4dbdcd28bac59a52aa2c75
Zita Nemeckova <znemecko@redhat.com>
2020-09-21 11:27:17 +0200

    Update navbar

--- src/loaders/standalone/standalone-loader.tsx
+++ src/loaders/standalone/standalone-loader.tsx
@@ -124,81 +124,77 @@ class App extends React.Component<RouteComponentProps, IState> {
         nav={
           <Nav theme='dark'>
             <NavList>
-              <NavGroup title='Content'>
-                <NavItem className={'nav-select'}>
-                  <Select
-                    className='nav-select'
-                    variant='single'
-                    isOpen={this.state.selectExpanded}
-                    selections={this.getRepoName(this.state.selectedRepo)}
-                    isPlain={false}
-                    onToggle={isExpanded => {
-                      this.setState({ selectExpanded: isExpanded });
-                    }}
-                    onSelect={(event, value) => {
-                      const originalRepo = this.state.selectedRepo;
-                      this.setState(
-                        {
-                          selectedRepo: this.getRepoBasePath(value.toString()),
-                          selectExpanded: false,
-                        },
-                        () => {
-                          this.props.history.push(
-                            formatPath(Paths.searchByRepo, {
-                              repo: this.getRepoBasePath(value.toString()),
-                            }),
-                          );
-                          // history.go(0) forces a reload of the page
-                          this.props.history.go(0);
-                        },
-                      );
-                    }}
-                  >
-                    <SelectOption key={'published'} value={'Published'} />
-                    <SelectOption
-                      key={'rh-certified'}
-                      value={'Red Hat Certified'}
-                    />
-                    <SelectOption key={'community'} value={'Community'} />
-                  </Select>
-                </NavItem>
+              <NavItem className={'nav-select'}>
+                <Select
+                  className='nav-select'
+                  variant='single'
+                  isOpen={this.state.selectExpanded}
+                  selections={this.getRepoName(this.state.selectedRepo)}
+                  isPlain={false}
+                  onToggle={isExpanded => {
+                    this.setState({ selectExpanded: isExpanded });
+                  }}
+                  onSelect={(event, value) => {
+                    const originalRepo = this.state.selectedRepo;
+                    this.setState(
+                      {
+                        selectedRepo: this.getRepoBasePath(value.toString()),
+                        selectExpanded: false,
+                      },
+                      () => {
+                        this.props.history.push(
+                          formatPath(Paths.searchByRepo, {
+                            repo: this.getRepoBasePath(value.toString()),
+                          }),
+                        );
+                        // history.go(0) forces a reload of the page
+                        this.props.history.go(0);
+                      },
+                    );
+                  }}
+                >
+                  <SelectOption key={'published'} value={'Published'} />
+                  <SelectOption
+                    key={'rh-certified'}
+                    value={'Red Hat Certified'}
+                  />
+                  <SelectOption key={'community'} value={'Community'} />
+                </Select>
+              </NavItem>
+              <NavItem>
+                <Link
+                  to={formatPath(Paths.searchByRepo, {
+                    repo: this.state.selectedRepo,
+                  })}
+                >
+                  Collections
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link to={Paths.partners}>Namespaces</Link>
+              </NavItem>
+              <NavItem>
+                <Link to={Paths.myNamespaces}>My Namespaces</Link>
+              </NavItem>
+              <NavItem>
+                <Link to={Paths.token}>API Token</Link>
+              </NavItem>
+              {!!user && user.model_permissions.view_user && (
                 <NavItem>
-                  <Link
-                    to={formatPath(Paths.searchByRepo, {
-                      repo: this.state.selectedRepo,
-                    })}
-                  >
-                    Collections
-                  </Link>
+                  <Link to={Paths.userList}>Users</Link>
                 </NavItem>
+              )}
+              <NavItem>
+                <Link to={Paths.groupList}>Groups</Link>
+              </NavItem>
+              {!!user && user.model_permissions.move_collection && (
                 <NavItem>
-                  <Link to={Paths.partners}>Namespaces</Link>
+                  <Link to={Paths.certificationDashboard}>Certification</Link>
                 </NavItem>
-                <NavItem>
-                  <Link to={Paths.myNamespaces}>My Namespaces</Link>
-                </NavItem>
-              </NavGroup>
-              <NavGroup title='Configuration'>
-                <NavItem>
-                  <Link to={Paths.token}>API Token</Link>
-                </NavItem>
-                {!!user && user.model_permissions.view_user && (
-                  <NavItem>
-                    <Link to={Paths.userList}>Users</Link>
-                  </NavItem>
-                )}
-                <NavItem>
-                  <Link to={Paths.groupList}>Groups</Link>
-                </NavItem>
-                {!!user && user.model_permissions.move_collection && (
-                  <NavItem>
-                    <Link to={Paths.certificationDashboard}>Certification</Link>
-                  </NavItem>
-                )}
-                <NavItem>
-                  <Link to={Paths.repositories}>Repo Management</Link>
-                </NavItem>
-              </NavGroup>
+              )}
+              <NavItem>
+                <Link to={Paths.repositories}>Repo Management</Link>
+              </NavItem>
             </NavList>
           </Nav>
         }
