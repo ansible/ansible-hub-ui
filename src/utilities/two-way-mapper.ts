20e089e7bebf2f030fa920b771dfc66c2f5a86dc
Zita Nemeckova <znemecko@redhat.com>
2020-09-24 15:12:18 +0200

    Change attribute name and comment to be more universal

--- src/utilities/two-way-mapper.ts
+++ src/utilities/two-way-mapper.ts
@@ -1,10 +1,10 @@
-// Returns human value for DB permission and vise verse
-export function twoWayMapper(permission: string, mapper: any) {
-  if (Object.values(mapper).includes(permission)) {
-    return Object.keys(mapper).find(key => mapper[key] === permission);
+// returns value/key based on given value/key and a mapper
+export function twoWayMapper(value: string, mapper: any) {
+  if (Object.values(mapper).includes(value)) {
+    return Object.keys(mapper).find(key => mapper[key] === value);
   }
-  if (Object.keys(mapper).includes(permission)) {
-    return mapper[permission];
+  if (Object.keys(mapper).includes(value)) {
+    return mapper[value];
   }
   return undefined;
 }
