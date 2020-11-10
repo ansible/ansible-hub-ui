489c56da0922824adf3444431db36d6c7f922f16
ZitaNemeckova <znemecko@redhat.com>
2020-11-10 14:45:52 +0100

    Remove packages with old node-fetch (#208)
    * Closes-Issue: AHH-87
    
    Update or remove(if not used) packages with old node-fetch
    
    * Remove storybook
    
    * Add missing package

--- src/components/tags/deprecated-tag.stories.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-import { DeprecatedTag } from './deprecated-tag';
-
-export default {
-  title: 'Components / DeprecatedTag',
-};
-
-export const basic = () => <DeprecatedTag />;
--- src/components/tags/tag.stories.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-import { Tag } from './tag';
-
-export default {
-  title: 'Components / Tag',
-};
-
-export const basic = () => <Tag>i_am_tag</Tag>;
