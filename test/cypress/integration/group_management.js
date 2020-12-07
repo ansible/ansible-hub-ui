548a65c72b266eb395e32c8f1d2504d2ca7b0ad5
Zita Nemeckova <znemecko@redhat.com>
2020-12-07 13:53:08 +0100

    Admin user can add and remove users from a group

--- test/cypress/integration/group_management.js
+++ test/cypress/integration/group_management.js
@@ -7,7 +7,7 @@ describe('Hub Group Management Tests', () => {
         cy.visit(host);
     });
 
-    it('admin user can create and delete a group', () => {
+    it.skip('admin user can create and delete a group', () => {
         var name = 'testGroup';
         cy.login(adminUsername, adminPassword);
         cy.createGroup(name);
@@ -16,4 +16,39 @@ describe('Hub Group Management Tests', () => {
         cy.deleteGroup(name);
         cy.contains(name).should('not.exist');
     });
+
+    it('admin user can add a user toa  group', () => {
+        var groupName = 'testGroup';
+        var userName = 'testUser';
+        cy.login(adminUsername, adminPassword);
+        cy.createGroup(groupName);
+        cy.createUser(userName);
+        cy.addUserToGroup(groupName, userName);
+        cy.removeUserFromGroup(groupName, userName);
+        cy.deleteGroup(groupName);
+        cy.deleteUser(userName);
+    });
+
+    it.skip('admin user can edit a group', () => {
+        var name = 'testGroup';
+        cy.login(adminUsername, adminPassword);
+        cy.createGroup(name);
+        cy.contains(name).should('exist');
+        cy.addAllPermissions(name);
+        cy.removePermissions(name, [{
+            group: 'namespaces', permissions: ['Add namespace', 'Change namespace', 'Upload to namespace']
+        }, {
+            group: 'collections', permissions: ['Modify Ansible repo content']
+        },{
+            group: 'users', permissions: ['View user', 'Delete user', 'Add user', 'Change user']
+        },{
+            group: 'groups', permissions: ['View group', 'Delete group', 'Add group', 'Change group']
+        },{
+            group: 'remotes', permissions: ['Change collection remote', 'View collection remote']
+        }]);
+        [ 'namespaces', 'collections', 'users', 'groups', 'remotes' ].forEach(permGroup => cy.get(`.pf-l-flex.pf-m-align-items-center.${permGroup}  [placeholder="No permission"]`).should('exist'));;
+        cy.deleteGroup(name);
+        cy.contains(name).should('not.exist');
+    });
+
 });
--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -58,7 +58,7 @@ Cypress.Commands.add('login', {}, (username, password) => {
     cy.wait('@me');
 });
 
-Cypress.Commands.add('createUser', {}, (username, password, firstName = null, lastName = null, email = null) => {
+Cypress.Commands.add('createUser', {}, (username, password = null, firstName = null, lastName = null, email = null) => {
     cy.contains('#page-sidebar a', 'Users').click();
 
     const user = {
@@ -66,7 +66,7 @@ Cypress.Commands.add('createUser', {}, (username, password, firstName = null, la
         lastName: lastName || 'Last Name',
         username: username,
         email: email || 'firstName@example.com',
-        password: password,
+        password: password || 'I am a complicated passw0rd',
     };
     cy.contains('Create user').click();
     cy.get('#first_name').type(user.firstName);
@@ -105,6 +105,22 @@ Cypress.Commands.add('addPermissions', {}, (groupName, permissions) => {
     cy.contains('button', 'Save').click({'force': true});
 });
 
+Cypress.Commands.add('removePermissions', {}, (groupName, permissions) => {
+    cy.contains('#page-sidebar a', 'Groups').click({'force': true});
+    cy.get(`[aria-labelledby=${groupName}] a`).click({'force': true});
+    cy.contains('button', 'Edit').click({'force': true});
+    permissions.forEach(permissionElement => {
+        if (permissionElement.permissions.length > 3) {
+            // Make sure all permissions are visible
+            cy.containsnear(`.pf-l-flex.pf-m-align-items-center.${permissionElement.group} `, '1 more').first().click({'force': true});
+        }
+        permissionElement.permissions.forEach(permission => {
+            cy.containsnear(`.pf-l-flex.pf-m-align-items-center.${permissionElement.group} `, permission).findnear('button').first().click({'force': true});
+        });
+    });
+    cy.contains('button', 'Save').click({'force': true});
+});
+
 Cypress.Commands.add('addAllPermissions', {}, (groupName) => {
     var allPerms = [{
         group: 'namespaces', permissions: ['Add namespace', 'Change namespace', 'Upload to namespace']
@@ -118,6 +134,26 @@ Cypress.Commands.add('addAllPermissions', {}, (groupName) => {
         group: 'remotes', permissions: ['Change collection remote', 'View collection remote']
     }];
     cy.addPermissions(groupName, allPerms);
+});
+
+Cypress.Commands.add('addUserToGroup', {}, (groupName, userName) => {
+    cy.contains('#page-sidebar a', 'Groups').click({'force': true});
+    cy.get(`[aria-labelledby=${groupName}] a`).click({'force': true});
+    cy.contains('button', 'Users').click({'force': true});
+    cy.contains('button', 'Add').click({'force': true});
+    cy.get('input.pf-c-select__toggle-typeahead').type(userName);
+    cy.contains('button', userName).click({'force': true});
+    cy.contains('footer > button', 'Add').click({'force': true});
+    cy.get(`[aria-labelledby=${userName}]`).should('exist');
+});
+
+Cypress.Commands.add('removeUserFromGroup', {}, (groupName, userName) => {
+    cy.contains('#page-sidebar a', 'Groups').click({'force': true});
+    cy.get(`[aria-labelledby=${groupName}] a`).click({'force': true});
+    cy.contains('button', 'Users').click({'force': true});
+    cy.get(`[aria-labelledby=${userName}] [aria-label=Actions]`).click({'force': true});
+    cy.containsnear(`[aria-labelledby=${userName}] [aria-label=Actions]`, 'Remove').click({'force': true});
+    cy.contains(userName).should('not.exist');
 });
 
 Cypress.Commands.add('deleteUser', {}, (username) => {
