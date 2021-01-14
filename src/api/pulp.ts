3dfd3c362a419248409938b800900c807b0e6266
Zita Nemeckova <znemecko@redhat.com>
2021-01-14 16:30:43 +0100

    Add paginatin logic

--- src/api/pulp.ts
+++ src/api/pulp.ts
@@ -33,6 +33,16 @@ export class PulpAPI {
     params['ordering'] = sort;
     delete params['sort'];
 
+    const pageSize =
+      parseInt(params['page_size']) || Constants.DEFAULT_PAGE_SIZE;
+    const page = parseInt(params['page']) || 1;
+
+    delete params['page'];
+    delete params['page_size'];
+
+    params['offset'] = page * pageSize - pageSize;
+    params['limit'] = pageSize;
+
     return params;
   }
 }
