564c41eff51fdc6f4d378a1f90f1d3820c82175a
Henderson Hummel <hhummel@redhat.com>
2021-02-11 07:51:17 -0800

    Add test for AAH-160 (#301)
    The change in commands.js was simply a missing parenthesis, and the
    change in package.json is just whitespace (seems like it was automated,
    I don't remember making that change.)

---
+++ test/cypress/integration/profile.js
@@ -0,0 +1,24 @@
+describe('My Profile Tests', () => {
+    var host = Cypress.env('host');
+    var adminUsername = Cypress.env('username');
+    var adminPassword = Cypress.env('password');
+
+    beforeEach(() => {
+        cy.visit(host);
+        cy.login(adminUsername, adminPassword);
+        // open the dropdown labeled with the username and then...
+        cy.get('[aria-label="user-dropdown"] button').click();
+        // a little hacky, but basically
+        // just click the one link that says 'My profile'.
+        cy.get('a').contains('My profile').click(); 
+    });
+
+    it('only has input fields for name, email, username, password and pass confirmation', () => {
+        let inputs = ['first_name', 'last_name', 'email', 'username', 'password', 'password-confirm'];
+        cy.get('.body').within(() => {
+            cy.get('input').each(($el, index, $list) => {
+                expect(inputs).to.include($el.attr('id'));
+            });
+        });
+    });
+});
