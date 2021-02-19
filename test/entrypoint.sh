deabcc4acdb892b3c8a5c4c0e8f15e0684a15e1a
Calvin Spealman <cspealma@redhat.com>
2021-02-19 09:39:11 -0500

    Clean up stray uses of removed host setting

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-
-if [[ ! -z "$CYPRESS_HOST" ]]; then
-    echo "CYPRESS_HOST = '$CYPRESS_HOST'"
-else
-    unset CYPRESS_HOST
-fi
-
 if [[ ! -z "$CYPRESS_USERNAME" ]]; then
     echo "CYPRESS_USERNAME = '$CYPRESS_USERNAME'"
 else
@@ -23,13 +16,6 @@ if [[ ! -z "$CYPRESS_BASE_URL" ]]; then
     echo "CYPRESS_BASE_URL = '$CYPRESS_BASE_URL'"
 else
     unset CYPRESS_BASE_URL
-fi
-
-if [[ -z "$CYPRESS_BASE_URL" ]]; then
-    if [[ -e "$CYPRESS_HOST" ]]; then
-        export CYPRESS_BASE_URL="http://$CYPRESS_HOST/"
-        echo "CYPRESS_BASE_URL = $CYPRESS_BASE_URL"
-    fi
 fi
 
 npx cypress run --browser chrome
