2d7611f3e73f1d654afa8d41c4f53559e9aefc8e
Zita Nemeckova <znemecko@redhat.com>
2021-06-07 13:23:11 +0200

    Remove Skeleton, PageHeader and PageHeaderTitle

--- src/components/loading/loading-with-header.tsx
+++ src/components/loading/loading-with-header.tsx
@@ -1,24 +1,22 @@
 import * as React from 'react';
 
-import {
-  Skeleton,
-  PageHeaderTitle,
-  PageHeader,
-} from '@redhat-cloud-services/frontend-components';
+import { Skeleton, Title } from '@patternfly/react-core';
 
-import { Main } from 'src/components';
-
-import { LoadingPageSpinner } from 'src/components';
+import { Main, LoadingPageSpinner } from 'src/components';
 
 export class LoadingPageWithHeader extends React.Component<{}> {
   render() {
     return (
       <React.Fragment>
-        <PageHeader>
-          <PageHeaderTitle
-            title={<Skeleton size='sm'></Skeleton>}
-          ></PageHeaderTitle>
-        </PageHeader>
+        <section
+          className={
+            'pf-l-page-header pf-c-page-header pf-l-page__main-section pf-c-page__main-section pf-m-light'
+          }
+        >
+          <Title headingLevel='h1'>
+            <Skeleton></Skeleton>
+          </Title>
+        </section>
         <Main>
           <section>
             <LoadingPageSpinner></LoadingPageSpinner>
