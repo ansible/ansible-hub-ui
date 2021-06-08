412afe538b27127a5e22ed736d0d7af07a053a08
Shaiah Emigh-Doyle <sde2111@columbia.edu>
2021-06-08 16:43:21 -0400

    formatting

--- src/loaders/standalone/standalone-loader.tsx
+++ src/loaders/standalone/standalone-loader.tsx
@@ -321,7 +321,7 @@ class App extends React.Component<RouteComponentProps, IState> {
           url: formatPath(Paths.searchByRepo, {
             repo: this.state.selectedRepo,
           }),
-          'data-cy':'collections',
+          'data-cy': 'collections',
         }),
         menuItem('Namespaces', {
           url: Paths[NAMESPACE_TERM],
