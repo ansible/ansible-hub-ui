361b44d6c0af376abe7ac3022cd808b2fcec2b29
Shaiah Emigh-Doyle <sde2111@columbia.edu>
2021-06-10 15:15:31 -0400

    edit

--- test/cypress/integration/namespace-edit.js
+++ test/cypress/integration/namespace-edit.js
@@ -1,9 +1,24 @@
-const { idText } = require("typescript")
+let baseUrl = Cypress.config().baseUrl;
+  let adminUsername = Cypress.env('username');
+  let adminPassword = Cypress.env('password');
+
+  beforeEach(() => {
+    cy.visit(baseUrl);
+  });
 
 
 describe('edit an existing namespace', () => {
+    describe('create a namespace', () => {
+        
+    })
     describe('test all fields', () => {
         it('tests first field', () => {
+            cy.login(adminUsername, adminPassword);
+            cy.on('uncaught:exception', (err, runnable) => {
+              return false;
+            });
+            const collectionTab = cy.menuPresent('Namespaces');
+            collectionTab.click({ force: true });
 
         })
     })
