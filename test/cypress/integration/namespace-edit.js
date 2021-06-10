2a3036f926f28edc67e45d9ade57ec41e4e28d80
Shaiah Emigh-Doyle <sde2111@columbia.edu>
2021-06-10 15:02:31 -0400

    set up test

---
+++ test/cypress/integration/namespace-edit.js
@@ -0,0 +1,18 @@
+const { idText } = require("typescript")
+
+
+describe('edit an existing namespace', () => {
+    describe('test all fields', () => {
+        it('tests first field', () => {
+
+        })
+    })
+    describe('add and removes links', () => {
+        it('adds a link', () => {
+
+        })
+        it('removes a link', () => {
+
+        })
+    })
+})
