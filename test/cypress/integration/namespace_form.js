ddc807900d0eacb9f09ff849b60420dc949eb111
Shaiah Emigh-Doyle <sde2111@columbia.edu>
2021-06-11 17:04:45 -0400

    final commit

--- test/cypress/integration/namespace_form.js
+++ test/cypress/integration/namespace_form.js
@@ -1,87 +1,77 @@
-describe('creating a new namespace', () => {
+describe('A namespace form', () => {
   let baseUrl = Cypress.config().baseUrl;
   let adminUsername = Cypress.env('username');
   let adminPassword = Cypress.env('password');
+  
+  
+  let getCreateNamespace = () => {return cy.get('[data-cy=create-namespace]');};
+  let getMessage = () => {return cy.get('.pf-c-form__helper-text');};
+  let getCreateButton = () => {return cy.get('[data-cy=create]');};
+  let getInputBox = () => {return cy.get('#pf-modal-part-2 #newNamespaceName');};
+  let clearInput = () => {return  cy.get('#pf-modal-part-2 #newNamespaceName').clear();}
+  let createNamespace = () => {return cy.galaxykit("-i namespace create", "testns1");}
+  let getUrl = () => {return cy.url()};
 
   beforeEach(() => {
     cy.visit(baseUrl);
-  });
-
-  it('creates a new namespace', () => {
     cy.login(adminUsername, adminPassword);
     cy.on('uncaught:exception', (err, runnable) => {
       return false;
     });
-    const collectionTab = cy.menuPresent('Namespaces');
-    collectionTab.click({ force: true });
-    const createButton = cy.get('[data-cy=create-namespace]');
-    createButton.click();
-    cy.get('#pf-modal-part-2 #newNamespaceName').type('my3newnamespace24');
-    const message = cy.get('.pf-c-form__helper-text');
-    message.should('have.text', 'Please, provide the namespace name');
-    const create = cy.get('[data-cy=create]');
-    create.should('be.enabled');
-    create.click();
-
-    const url = cy.url();
-    url.should(
-      'eq',
-      'http://localhost:8002/ui/my-namespaces/my3newnamespace24',
-    );
+    createNamespace();
+    cy.menuGo('Collections > Namespaces');
+    getCreateNamespace().click();
   });
 
-  it('throws error message with incorrect name', () => {
-    cy.login(adminUsername, adminPassword);
-    cy.on('uncaught:exception', (err, runnable) => {
-      return false;
-    });
-    const collectionTab = cy.menuPresent('Namespaces');
-    collectionTab.click({ force: true });
-    const createButton = cy.get('[data-cy=create-namespace]');
-    createButton.click();
-    const create = cy.get('[data-cy=create]');
-    create.should('be.disabled');
-    // before typing anything
-    const message = cy.get('.pf-c-form__helper-text');
-    message.should('have.text', 'Please, provide the namespace name');
-    // type space
-    const inputBox = cy.get('#pf-modal-part-2 #newNamespaceName');
-    inputBox.type(' ');
-    cy.get('[data-cy=create]').should('be.disabled');
-    const message = cy.get('.pf-c-form__helper-text');
-    message.should('have.text', 'Name can only contain [A-Za-z0-9_]');
-    cy.get('#pf-modal-part-2 #newNamespaceName').clear();
-    // incorrect characters
-    const inputBox = cy.get('#pf-modal-part-2 #newNamespaceName');
-    inputBox.type('!/^[a-zA-Z0-9_]+$/.');
-    cy.get('[data-cy=create]').should('be.disabled');
-    const message = cy.get('.pf-c-form__helper-text');
-    message.should('have.text', 'Name can only contain [A-Za-z0-9_]');
-    cy.get('#pf-modal-part-2 #newNamespaceName').clear();
-    // shorter than 3 characters
-    const inputBox = cy.get('#pf-modal-part-2 #newNamespaceName');
-    inputBox.type('na');
-    cy.get('[data-cy=create]').should('be.disabled');
-    const message = cy.get('.pf-c-form__helper-text');
-    message.should('have.text', 'Name must be longer than 2 characters');
-    cy.get('#pf-modal-part-2 #newNamespaceName').clear();
-    //   begins with underscore
-    const inputBox = cy.get('#pf-modal-part-2 #newNamespaceName');
-    inputBox.type('_my8new8namespace');
-    cy.get('[data-cy=create]').should('be.disabled');
-    const message = cy.get('.pf-c-form__helper-text');
-    message.should('have.text', "Name cannot begin with '_'");
-    cy.get('#pf-modal-part-2 #newNamespaceName').clear();
-
-  // same name as existing namespace
-    const inputBox = cy.get('#pf-modal-part-2 #newNamespaceName');
-    inputBox.type('my4new8namespace24');
-    const create = cy.get('[data-cy=create]');
-    create.click();
-    const message = cy.get('.pf-c-form__helper-text');
-    message.should(
-      'have.text',
-      'A namespace named my4new8namespace24 already exists.',
-    );
+  it('should give message before typing', () => {
+    getMessage().should('have.text', 'Please, provide the namespace name');
+    getCreateButton().should('be.disabled');
   });
-});
+  it('should give message if input is empty', () => {
+    getInputBox().type(' ');
+    getMessage().should('have.text', 'Name can only contain [A-Za-z0-9_]');
+    getCreateButton().should('be.disabled');
+    clearInput();
+  });
+  it('should give message if input has incorrect characters', () => {
+    getInputBox().type('!/^[a-zA-Z0-9_]+$/.');
+    getMessage().should('have.text', 'Name can only contain [A-Za-z0-9_]');
+    getCreateButton().should('be.disabled');
+    clearInput();
+  });
+  it('should give message if input is shorter than 3 characters', () => {
+    getInputBox().type('na');
+    getMessage().should('have.text', 'Name must be longer than 2 characters');
+    getCreateButton().should('be.disabled');
+    clearInput();
+  });
+  if('should give message if input begins with underscore', () => {
+    getInputBox().type('_namespace');
+    getMessage().should("have.text', 'Name cannot begin with '_'");
+    getCreateButton().should('be.disabled');
+    clearInput();
+  });
+  it('should give message if name already exists', () => {  
+    getInputBox().type('testns1');
+    getCreateButton().click();    
+    getCreateButton().should('be.disabled');
+    getMessage().should('have.text', 'A namespace named testns1 already exists.');    
+    clearInput();
+  });
+
+// Note: the next test should work the first time, but not on rerun. to reset DB via CLI (from galaxy_ng dir):
+
+// ./compose down --volume
+// ./compose up -d postgres redis
+// ./compose run --rm api manage migrate
+// ./compose run --rm -e PULP_FIXTURE_DIRS='["/src/galaxy_ng/dev/automation-hub"]' api manage loaddata initial_data.json
+
+  it('creates a new namespace with no error messages', () => {
+    getInputBox().type('testns2');
+    getCreateButton().click();   
+    getUrl().should(
+          'eq',
+          'http://localhost:8002/ui/my-namespaces/testns2',
+        );
+  });
+})
