82a4eea556c8c97da2ea8de562c99ec60dd38bbc
Zita Nemeckova <znemecko@redhat.com>
2021-06-15 10:07:10 +0200

    Turn Maintenance page on (#537)
    (cherry picked from commit 4853892966e8ae14900ac5596442a4bf2bc204cf)
    
    updated to remove unused imports

--- src/entry.js
+++ src/entry.js
@@ -1,21 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { init } from './store';
-import App from './loaders/insights/insights-loader';
-import getBaseName from './utilities/getBaseName';
+import MaintenancePage from './components/maintenance-page/maintenance-page';
 
-// Entrypoint for compiling the app to run in insights production mode.
-
-const basename = getBaseName(window.location.pathname);
-
-ReactDOM.render(
-  <Provider store={init().getStore()}>
-    <Router basename={basename}>
-      <App basename={basename} />
-    </Router>
-  </Provider>,
-
-  document.getElementById('root'),
-);
+ReactDOM.render(<MaintenancePage />, document.getElementById('root'));
