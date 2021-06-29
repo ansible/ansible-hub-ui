4d0917b63ffea8627c079c81b433126a89f6bfbe
Shaiah Emigh-Doyle <sde2111@columbia.edu>
2021-06-29 11:01:44 -0400

    cont. working on task-management feature

---
+++ src/api/response-types/task.ts
@@ -0,0 +1,10 @@
+export class TaskType {
+    pulp_created: string;
+    state: string;
+    name: string;
+    started_at: string;
+    finished_at: string;
+    error: string;
+    parent_task: string;
+    child_tasks: string;
+}
