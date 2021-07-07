29448d438a2320670f8e07b2ac65772a230d9c64
Shaiah Emigh-Doyle <sde2111@columbia.edu>
2021-07-07 09:37:07 -0400

    add icons

--- src/containers/task-management/task-list-view.tsx
+++ src/containers/task-management/task-list-view.tsx
@@ -11,7 +11,13 @@ import {
   Label,
 } from '@patternfly/react-core';
 import { ParamHelper } from '../../utilities';
-import { WarningTriangleIcon, CheckCircleIcon, ExclamationCircleIcon, SyncAltIcon, OutlinedClockIcon} from '@patternfly/react-icons';
+import {
+  WarningTriangleIcon,
+  CheckCircleIcon,
+  ExclamationCircleIcon,
+  SyncAltIcon,
+  OutlinedClockIcon,
+} from '@patternfly/react-icons';
 import {
   AlertList,
   AlertType,
@@ -37,7 +43,6 @@ interface IState {
     page_size?: number;
   };
   loading: boolean;
-  //What would 'items' be here? The list of tasks from the API?
   items: Array<TaskType>;
   itemCount: number;
   alerts: AlertType[];
@@ -62,7 +67,6 @@ export class TaskListView extends React.Component<RouteComponentProps, IState> {
 
     this.state = {
       params: params,
-      //items here would be the tasks, I believe
       items: [],
       loading: true,
       itemCount: 0,
@@ -80,8 +84,6 @@ export class TaskListView extends React.Component<RouteComponentProps, IState> {
     });
   }
 
-  // My fetch attempt, pretty sure it's not working...
-  //Is there somewhere we need to add our tasks path /ui/task_management with react router?
 
   render() {
     return (
@@ -90,240 +92,7 @@ export class TaskListView extends React.Component<RouteComponentProps, IState> {
         <Main>{this.renderTable()}</Main>
       </React.Fragment>
     );
-    //   const { params, itemCount, loading, alerts } = this.state;
 
-    //   if (!params['sort']) {
-    //     params['sort'] = 'name';
-    //   }
-
-    //   return (
-    //     <React.Fragment>
-    //       <AlertList
-    //         alerts={alerts}
-    //         closeAlert={i => this.closeAlert(i)}
-    //       ></AlertList>
-    //       <BaseHeader title='Task Management'></BaseHeader>
-    //       //Is something like this 'noData' state applicable for the tasks?
-    //       {noData && !loading ? (
-    //         <EmptyStateNoData
-    //           title={'No container repositories yet'}
-    //           description={
-    //             'You currently have no container repositories. Add a container repository via the CLI to get started.'
-    //           }
-    //           //I don't think a button to add images is applicable here...
-    //           button={pushImagesButton}
-    //         />
-    //       ) : (
-    //         <Main>
-    //           {loading ? (
-    //             <LoadingPageSpinner />
-    //           ) : (
-    //             <Section className='body'>
-    //               <div className='toolbar'>
-    //                 <Toolbar>
-    //                   <ToolbarContent>
-    //                     <ToolbarGroup>
-    //                       <ToolbarItem>
-    //                         <CompoundFilter
-    //                           updateParams={p => {
-    //                             p['page'] = 1;
-    //                             this.updateParams(p, () =>
-    //                               // rather than query environments, this would be something like query tasks...
-    //                               this.queryEnvironments(),
-    //                             );
-    //                           }}
-    //                           params={params}
-    //                           filterConfig={[
-    //                             {
-    //                               id: 'name',
-    //                               title: 'Task name',
-    //                             },
-    //                           ]}
-    //                         />
-    //                       </ToolbarItem>
-    //                       //I assume pushImageButton is not applicable here
-    //                       <ToolbarItem>{pushImagesButton}</ToolbarItem>
-    //                     </ToolbarGroup>
-    //                   </ToolbarContent>
-    //                 </Toolbar>
-    //                 <Pagination
-    //                   params={params}
-    //                   updateParams={p =>
-    //                     // again, queryTasks or so
-    //                     this.updateParams(p, () => this.queryEnvironments())
-    //                   }
-    //                   count={itemCount}
-    //                   isTop
-    //                 />
-    //               </div>
-    //               <div>
-    //                 <AppliedFilters
-    //                   updateParams={p =>
-    //                     //queryTasks
-    //                     this.updateParams(p, () => this.queryEnvironments())
-    //                   }
-    //                   params={params}
-    //                   ignoredParams={['page_size', 'page', 'sort']}
-    //                 />
-    //               </div>
-    //               {this.renderTable(params)}
-    //               <div style={{ paddingTop: '24px', paddingBottom: '8px' }}>
-    //                 <Pagination
-    //                   params={params}
-    //                   updateParams={p =>
-    //                     //queryTasks
-    //                     this.updateParams(p, () => this.queryEnvironments())
-    //                   }
-    //                   count={itemCount}
-    //                 />
-    //               </div>
-    //             </Section>
-    //           )}
-    //         </Main>
-    //       )}
-    //     </React.Fragment>
-    //   );
-    // }
-
-    // // Will we be using something like this for renderTable?
-    // private renderTable(params) {
-    //   const { items } = this.state;
-    //   if (items.length === 0) {
-    //     return <EmptyStateFilter />;
-    //   }
-
-    //   let sortTableOptions = {
-    //     headers: [
-    //       {
-    //         title: 'Task name',
-    //         type: 'alpha',
-    //         id: 'name',
-    //       },
-    //       {
-    //         title: 'State',
-    //         type: 'alpha',
-    //         id: 'state',
-    //       },
-    //       {
-    //         title: 'Error',
-    //         type: 'alpha',
-    //         id: 'error',
-    //       },
-    //       {
-    //         title: 'Date created',
-    //         type: 'none',
-    //         id: 'pulp_created',
-    //       },
-    //       {
-    //         title: 'Started at',
-    //         type: 'none',
-    //         id: 'started_at',
-    //       },
-    //       {
-    //         title: 'Finished at',
-    //         type: 'none',
-    //         id: 'finished_at',
-    //       },
-    //       {
-    //         title: 'Progress reports',
-    //         type: 'none',
-    //         id: 'progress_reports',
-    //       },
-    //       {
-    //         title: 'Parent task',
-    //         type: 'none',
-    //         id: 'parent_task',
-    //       },
-    //       {
-    //         title: 'Child tasks',
-    //         type: 'none',
-    //         id: 'child_tasks',
-    //       },
-    //     ],
-    //   };
-
-    //   return (
-    //     <table aria-label='Task list' className='content-table pf-c-table'>
-    //       <SortTable
-    //         options={sortTableOptions}
-    //         params={params}
-    //         updateParams={p =>
-    //           //How would this be different for the task list? Does this refer to the API call / setState down on 229?
-    //           this.updateParams(p, () => this.queryEnvironments())
-    //         }
-    //       />
-    //       <tbody>{items.map((user, i) => this.renderTableRow(user, i))}</tbody>
-    //     </table>
-    //   );
-    // }
-
-    // private renderTableRow(item: any, index: number) {
-    //   const {
-    //     name,
-    //     pulp_href,
-    //     state,
-    //     error,
-    //     pulp_created,
-    //     started_at,
-    //     finished_at,
-    //     progress_reports,
-    //     parent_task,
-    //     child_tasks,
-    //   } = item;
-    //   return (
-    //     <tr aria-labelledby={name} key={index}>
-    //       <td>
-    //         <Link
-    //           //this path would be to the task detail, correct? Or to the pulp_href
-    //           to={formatPath(Paths.taskList, {
-    //             container: item.pulp.distribution.base_path,
-    //           })}
-    //         >
-    //           {name}
-    //         </Link>
-    //       </td>
-    //       <td>{state}</td>
-    //       // error is an object. Is this the proper way to render it?
-    //       {error ? (
-    //         <td className={'pf-m-truncate'}>
-    //           <Tooltip content={error}>{error}</Tooltip>
-    //         </td>
-    //       ) : (
-    //         <td></td>
-    //       )}
-    //       <td>
-    //         <DateComponent date={pulp_created} />
-    //       </td>
-    //       <td>
-    //         <DateComponent date={started_at} />
-    //       </td>
-    //       {finished_at ? (
-    //         <td>
-    //           <DateComponent date={finished_at} />
-    //         </td>
-    //       ) : (
-    //         <td></td>
-    //       )}
-    //       {progress_reports ? (
-    //         <td className={'pf-m-truncate'}>
-    //           //given this is an Array, is this the proper way to render it?
-    //           <Tooltip content={progress_reports}>{progress_reports}</Tooltip>
-    //         </td>
-    //       ) : (
-    //         <td></td>
-    //       )}
-    //     </tr>
-    //   );
-    // }
-
-    // // This must be where we call the pulp API and set the state appropriately.
-
-    // private get updateParams() {
-    //   return ParamHelper.updateParamsMixin();
-    // }
-
-    // private get closeAlert() {
-    //   return closeAlertMixin('alerts');
   }
 
   private renderTable() {
@@ -340,7 +109,7 @@ export class TaskListView extends React.Component<RouteComponentProps, IState> {
           id: 'name',
         },
         {
-          title: 'State',
+          title: 'Status',
           type: 'alpha',
           id: 'state',
         },
@@ -350,7 +119,7 @@ export class TaskListView extends React.Component<RouteComponentProps, IState> {
           id: 'error',
         },
         {
-          title: 'Date created',
+          title: 'Created on',
           type: 'none',
           id: 'pulp_created',
         },
@@ -389,24 +158,37 @@ export class TaskListView extends React.Component<RouteComponentProps, IState> {
         <td>{name}</td>
         {state === 'failed' ? (
           <td>
-            <Label variant='outline' color='red' icon={<ExclamationCircleIcon />}>{state}</Label>
+            <Label
+              variant='outline'
+              color='red'
+              icon={<ExclamationCircleIcon />}
+            >
+              {state}
+            </Label>
           </td>
         ) : state === 'completed' ? (
           <td>
-            <Label variant='outline' color='green' icon={<CheckCircleIcon />}>{state}</Label>
+            <Label variant='outline' color='green' icon={<CheckCircleIcon />}>
+              {state}
+            </Label>
           </td>
         ) : state === 'running' ? (
           <td>
-            <Label variant='outline' color='blue' icon={<SyncAltIcon />}>{state}</Label>
+            <Label variant='outline' color='blue' icon={<SyncAltIcon />}>
+              {state}
+            </Label>
           </td>
         ) : state === 'waiting' ? (
           <td>
-            <Label variant='outline' color='grey' icon={<OutlinedClockIcon />}>{state}</Label>
+            <Label variant='outline' color='grey' icon={<OutlinedClockIcon />}>
+              {state}
+            </Label>
           </td>
-        ) :    <td>
-        <Label variant='outline'>{state}</Label>
-      </td>
-     }
+        ) : (
+          <td>
+            <Label variant='outline'>{state}</Label>
+          </td>
+        )}
 
         <td>{error}</td>
         <td>
