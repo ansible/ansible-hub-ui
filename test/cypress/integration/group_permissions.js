825740c1663f96079da205c9fe65a3d451a26b4d
Milan Pospisil <arkanus@seznam.cz>
2021-07-20 11:36:00 +0200

    Sequential tests with no loop :)

--- test/cypress/integration/group_permissions.js
+++ test/cypress/integration/group_permissions.js
@@ -4,31 +4,21 @@ describe('Group Permissions Tests', () => {
   let adminUsername = Cypress.env('username');
   let adminPassword = Cypress.env('password');
 
-  let users = {};
-  let groups = {};
-
   let groupsUrl = urljoin(baseUrl, 'ui/group-list');
 
   function addTestUser(user, group) {
-    users[user] = {};
-    users[user].group = group;
     cy.galaxykit('user create', user, user + 'Password');
     if (group != null) {
-      cy.addUserToGroup(group, user);
-      cy.addUserToGroup('allGroup', user);
-    }
+	  cy.galaxykit('user group add', user, group);
+	}
   }
 
   function addTestGroup(group, permissions) {
-    groups[group] = {};
-    groups[group].permissions = permissions;
+	cy.wait(1000);
     cy.createGroup(group);
-    cy.addPermissions(group, [{ group: 'groups', permissions: permissions }]);
-  }
-
-  function userHasPermission(user, permission) {
-    if (!users[user].group) return false;
-    if (groups[users[user].group].permissions.includes(permission)) return true;
+	cy.wait(1000);
+    //cy.galaxykit('-i group create', group);
+	cy.addPermissions(group, [{ group: 'groups', permissions: permissions }]);
   }
 
   before(() => {
@@ -36,80 +26,112 @@ describe('Group Permissions Tests', () => {
     cy.deleteTestGroups();
 
     cy.login(adminUsername, adminPassword);
-    cy.visit(baseUrl);
 
     // base group is needed because if no group is in list, you can always add Group even if you dont have permissions
-    addTestGroup('baseGroup', []);
-    addTestGroup('allGroup', []);
-
+    //addTestGroup('baseGroup', []);
     //addTestGroup('group2', []);
     //addTestGroup('group3', ['View group']);
-    //addTestGroup('group4', ['View group', 'Add group', 'Change group']);
-    addTestGroup('group5', ['View group', 'Delete group']);
+    addTestGroup('group4', ['View group', 'Delete group', 'Add group', 'Change group']);
 
     //addTestUser('user1', null);
     //addTestUser('user2', 'group2');
     //addTestUser('user3', 'group3');
-    //addTestUser('user4', 'group4');
-    addTestUser('user5', 'group5');
+    addTestUser('user4', 'group4');
+	cy.logout();
   });
-
-  it('Check user privilleges', () => {
-    Object.keys(users).forEach(user => {
-      cy.login(user, user + 'Password');
-      cy.visit(baseUrl);
-      cy.contains(user);
-
-      let canViewGroups = userHasPermission(user, 'View group');
-      let canChangeGroups = userHasPermission(user, 'Change group');
-      let canAddGroups = userHasPermission(user, 'Add group');
-      let canDeleteGroups = userHasPermission(user, 'Delete group');
-
-      let userGroup = users[user].group;
-
-      if (canViewGroups) {
-        cy.menuGo('User Access > Groups');
-        cy.contains('Groups');
-        cy.contains('a', userGroup).click();
-        cy.contains('namespaces');
-        cy.contains('collections');
-        cy.contains('users');
-        cy.contains('groups');
-        cy.contains('remotes');
-        cy.contains('containers');
-
-        if (canChangeGroups) {
-          cy.contains('button', 'Edit').click();
-
-          groups[userGroup].permissions.forEach(permission => {
-            cy.contains(permission);
-          });
-
-          cy.menuGo('User Access > Groups');
-        } else {
-          cy.contains('button', 'Edit').should('not.exist');
-        }
-
-        if (canAddGroups) {
-          cy.createGroup(userGroup + 'NewGroup');
-          cy.menuGo('User Access > Groups');
-        } else {
-          cy.contains('button', 'Add').should('not.exist');
-        }
-
-        if (canDeleteGroups) {
-          cy.contains('button', 'Delete').click();
-          //cy.contains('button', 'Cancel').click();
-          cy.contains('button', 'Delete').click();
-          //cy.contains('button', 'Delete').click({force : true});
-        } else {
-          cy.contains('button', 'Delete').should('not.exist');
-        }
-      } else {
-        cy.contains('Groups').should('not.exist');
-        cy.visit(groupsUrl);
-        cy.contains('Groups').should('not.exist');
-      }
-    });
+  
+  it('beforeAll', () => {});
+  
+  /*
+  it('it cant see groups', () => {
+	// test user without any group at all
+	let user = 'user1';
+	cy.login(user, user + 'Password');
+	cy.wait(2000);
+	cy.contains('Groups').should('not.exist');
+	cy.visit(groupsUrl);
+	cy.contains('You do not have have access to Automation Hub');
+	cy.logout();
+	
+	// test user in group with no privilleges 
+	user = 'user2';
+	cy.login(user, user + 'Password');
+	cy.wait(2000);
+	cy.contains('Groups').should('not.exist');
+	cy.visit(groupsUrl);
+	cy.contains('You do not have have access to Automation Hub');
+  });
+	
+  
+  it('can view groups', () => {
+	let user = 'user3';
+	cy.login(user, user + 'Password');
+	cy.wait(2000);
+	cy.menuGo('User Access > Groups');
+	cy.contains('Groups');
+  });
+	
+  it('can not change groups', () => {
+	let user = 'user3';
+	cy.login(user, user + 'Password');
+	cy.wait(2000);
+	cy.menuGo('User Access > Groups');
+	cy.contains('button', 'Edit').should('not.exist');
+  });
+  
+  it('can not add groups', () => {
+	let user = 'user3';
+	cy.login(user, user + 'Password');
+	cy.wait(2000);
+	cy.menuGo('User Access > Groups');
+	cy.contains('button', 'View').should('not.exist');
+  });
+  
+  it('can not delete groups', () => {
+	let user = 'user3';
+	cy.login(user, user + 'Password');
+	cy.wait(2000);
+	cy.menuGo('User Access > Groups');
+	cy.contains('button', 'Delete').should('not.exist');
+  });
+  
+  it('can add group', () => {
+	let user = 'user4';
+	cy.login(user, user + 'Password');
+	cy.wait(2000);
+	cy.createGroup('NewGroup');
+  });
+  
+  it.skip('can delete group', () => {
+	// this is not working yet, API is returning error, however group is still deleted 
+	// The group should be deleted, because user has rights for it, API should not return error
+	let user = 'user4';
+	cy.login(user, user + 'Password');
+	cy.wait(2000);
+	
+	cy.menuGo('User Access > Groups');
+	cy.intercept('DELETE', Cypress.env('prefix') + '_ui/v1/groups/**').as('deleteGroup',);
+	cy.get(`[aria-labelledby=${'NewGroup'}] [aria-label=Delete]`).click();
+	cy.contains('[role=dialog] button', 'Delete').click();
+	cy.wait('@deleteGroup').then(({ request, response }) => {
+		expect(response.statusCode).to.eq(204);
+	});
+	cy.wait(2000);
+  });*/
+  
+  it('can change group', () => {
+	let user = 'user4';
+	cy.login(user, user + 'Password');
+	cy.wait(2000);
+	
+	cy.menuGo('User Access > Groups');
+	cy.contains('a', 'group4').click();
+	cy.contains('button', 'Edit').click();
+	
+	cy.contains('Add group').siblings('button[aria-label=Remove]').click();
+	cy.contains('button', 'Save').click();
+	
+	cy.contains('button', 'Edit').click();
+	cy.contains('div', 'groups').get('[aria-label="Select permissions"]').type('Add group');
   });
 });
