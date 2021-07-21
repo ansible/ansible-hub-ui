c0c8d34847499d2115515d3aae7e4815419bcf3f
Shaiah Emigh-Doyle <semighdo@redhat.com>
2021-07-21 10:28:29 -0400

    update filter names

--- src/containers/user-management/user-list.tsx
+++ src/containers/user-management/user-list.tsx
@@ -226,10 +226,10 @@ class UserList extends React.Component<RouteComponentProps, IState> {
     const { users } = this.state;
     if (users.length === 0) {
       return filterIsSet(params, [
-        'username',
-        'first_name',
-        'last_name',
-        'email',
+        'username__contains',
+        'first_name__contains',
+        'last_name__contains',
+        'email__contains',
       ]) ? (
         <EmptyStateFilter />
       ) : (
@@ -250,23 +250,23 @@ class UserList extends React.Component<RouteComponentProps, IState> {
         {
           title: 'Username',
           type: 'alpha',
-          id: 'username',
+          id: 'username__contains',
         },
         {
           title: 'First name',
           type: 'alpha',
-          id: 'first_name',
+          id: 'first_name__contains',
           className: 'pf-m-wrap',
         },
         {
           title: 'Last name',
           type: 'alpha',
-          id: 'last_name',
+          id: 'last_name__contains',
         },
         {
           title: 'Email',
           type: 'alpha',
-          id: 'email',
+          id: 'email__contains',
         },
         {
           id: 'groups',
