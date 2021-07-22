bf24d0cd34218a1291a781b085b2635a72fd55c7
jerabekjiri <jerabekjirka@email.cz>
2021-07-22 17:30:23 +0200

    add token management test

---
+++ test/cypress/integration/token_management.js
@@ -0,0 +1,31 @@
+describe('Token Management Tests', () => {
+  const adminUsername = Cypress.env('username');
+  const adminPassword = Cypress.env('password');
+
+  before(() => {
+    cy.deleteTestUsers();
+    cy.galaxykit('user create', adminUsername, adminPassword);
+  });
+
+  beforeEach(() => {
+    cy.visit('/');
+    cy.login(adminUsername, adminPassword);
+  });
+
+  it('user can generatecy.visit token', () => {
+    cy.visit('/ui/token');
+    cy.intercept('POST', Cypress.env('prefix') + 'v3/auth/token/').as(
+      'tokenPost',
+    );
+
+    cy.contains('Load token').click();
+    cy.get('.pf-c-clipboard-copy').should('exist');
+
+    cy.server();
+    cy.wait('@tokenPost')
+      .its('response.body.token')
+      .then(token => {
+        cy.get('[aria-label="Copyable input"]').should('have.value', token);
+      });
+  });
+});
