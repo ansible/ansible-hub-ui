e140186a9f550d70592d8aca3868145a40c8c583
Milan Pospisil <arkanus@seznam.cz>
2021-07-22 13:48:11 +0200

    Clearing of cookie perservation.

--- test/cypress/integration/api_token.js
+++ test/cypress/integration/api_token.js
@@ -1,20 +1,9 @@
 describe('API Token Tests', () => {
-  let urljoin = require('url-join');
-  var baseUrl = Cypress.config().baseUrl;
   var adminUsername = Cypress.env('username');
   var adminPassword = Cypress.env('password');
 
-  let apiUrl = urljoin(baseUrl, 'ui/token');
-
-  before(() => {
-    cy.visit(baseUrl);
-    cy.login(adminUsername, adminPassword);
-  });
-
   beforeEach(() => {
-    Cypress.Cookies.preserveOnce('sessionid', 'csrftoken');
-    cy.visit(baseUrl);
-    cy.server();
+    cy.login(adminUsername, adminPassword);
   });
 
   it('token is generated', () => {
