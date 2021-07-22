8d7e18436288febf8861b505e2797a687336f29d
David Newswanger <dnewswan@redhat.com>
2021-07-22 09:21:14 -0400

    Add npm run start-pulp command to run with vagrant backend.

--- config/standalone.dev.webpack.config.js
+++ config/standalone.dev.webpack.config.js
@@ -3,13 +3,14 @@ const webpackBase = require('./webpack.base.config');
 // Used for getting the correct host when running in a container
 const proxyHost = process.env.API_PROXY_HOST || 'localhost';
 const proxyPort = process.env.API_PROXY_PORT || '5001';
+const apiBasePath = process.env.API_BASE_PATH || '/api/automation-hub/';
 
 module.exports = webpackBase({
   // The host where the API lives. EX: https://localhost:5001
   API_HOST: '',
 
   // Path to the API on the API host. EX: /api/automation-hub
-  API_BASE_PATH: '/api/automation-hub/',
+  API_BASE_PATH: apiBasePath,
 
   // Path on the host where the UI is found. EX: /apps/automation-hub
   UI_BASE_PATH: '/ui/',
@@ -36,7 +37,7 @@ module.exports = webpackBase({
   // https://webpack.js.org/configuration/dev-server/#devserverproxy
   // used to get around CORS requirements when running in dev mode
   WEBPACK_PROXY: {
-    '/api/automation-hub/': `http://${proxyHost}:${proxyPort}`,
+    '/api/': `http://${proxyHost}:${proxyPort}`,
     '/pulp/api/': `http://${proxyHost}:${proxyPort}`,
   },
 });
