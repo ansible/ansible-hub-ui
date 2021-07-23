e86b9303be11bc04c60d88b3090ef8fb2b7d7012
jerabekjiri <jerabekjirka@email.cz>
2021-07-23 22:01:02 +0200

    add docs menu test

---
+++ test/cypress/integration/docs_menu.js
@@ -0,0 +1,41 @@
+describe('Token Management Tests', () => {
+  const adminUsername = Cypress.env('username');
+  const adminPassword = Cypress.env('password');
+
+  before(() => {
+    cy.deleteTestUsers();
+    cy.galaxykit('user create', adminUsername, adminPassword);
+  });
+
+  beforeEach(() => {
+    cy.visit('/');
+    cy.login(adminUsername, adminPassword);
+  });
+
+  it('user can open docs dropdown menu', () => {
+    cy.get('[aria-label="docs-dropdown"]').click();
+
+    cy.get('.pf-c-dropdown__menu')
+      .contains('Customer Support')
+      .should('have.attr', 'href')
+      .and('contain', 'https://access.redhat.com/support');
+
+    cy.get('.pf-c-dropdown__menu')
+      .contains('Training')
+      .should('have.attr', 'href')
+      .and('contain', 'https://www.ansible.com/resources/webinars-training');
+
+    cy.get('.pf-c-dropdown__menu').contains('About');
+  });
+
+  it('user can toggle about modal', () => {
+    cy.get('[aria-label="docs-dropdown"]').click();
+    cy.get('.pf-c-dropdown__menu')
+      .contains('About')
+      .click();
+    cy.get('.pf-c-about-modal-box').should('be.visible');
+    cy.get('h1').contains('Galaxy NG');
+    cy.get('[aria-label="Close Dialog"]').click();
+    cy.get('.pf-c-about-modal-box').should('not.exist');
+  });
+});
