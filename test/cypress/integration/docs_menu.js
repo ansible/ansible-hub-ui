8d104ed7ff1662f738ac488cfb479628db92cb1d
jerabekjiri <jerabekjirka@email.cz>
2021-07-28 11:48:00 +0200

    removed before test

--- test/cypress/integration/docs_menu.js
+++ test/cypress/integration/docs_menu.js
@@ -2,11 +2,6 @@ describe('Token Management Tests', () => {
   const adminUsername = Cypress.env('username');
   const adminPassword = Cypress.env('password');
 
-  before(() => {
-    cy.deleteTestUsers();
-    cy.galaxykit('user create', adminUsername, adminPassword);
-  });
-
   beforeEach(() => {
     cy.visit('/');
     cy.login(adminUsername, adminPassword);
