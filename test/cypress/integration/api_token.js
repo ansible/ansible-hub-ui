8aae8d352cf22c3d511405e76fb7e12b4f815724
MilanPospisil <arkanus@seznam.cz>
2021-08-04 20:10:17 +0200

    Test that token is generated and has correct form (length and characters) (#679)
    * API test
    
    * correcting login
    
    * Clearing of cookie perservation.
    
    * Regexp corrected.
    
    * Delete directory.

---
+++ test/cypress/integration/api_token.js
@@ -0,0 +1,17 @@
+describe('API Token Tests', () => {
+  var adminUsername = Cypress.env('username');
+  var adminPassword = Cypress.env('password');
+
+  beforeEach(() => {
+    cy.login(adminUsername, adminPassword);
+  });
+
+  it('token is generated', () => {
+    cy.menuGo('Collections > API Token');
+    cy.contains('Load token').click();
+
+    cy.get('[aria-label="Copyable input"]')
+      .invoke('val')
+      .should('match', /^[0-9a-f]{40}$/);
+  });
+});
