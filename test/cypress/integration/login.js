8eb283722a915e059175cd58911a6cc6bd57bd66
MilanPospisil <arkanus@seznam.cz>
2021-08-16 20:45:58 +0200

    API login in Cypress (#768)
    * apiLogin command.
    
    * Prettier...
    
    * Minor cosmetic changes.
    
    * Update test/cypress/support/commands.js
    
    Co-authored-by: Martin Hradil <mhradil@redhat.com>

--- test/cypress/integration/login.js
+++ test/cypress/integration/login.js
@@ -12,38 +12,18 @@ describe('Login helpers', () => {
   });
 
   it('can login manually and logout as admin or different user', () => {
-    cy.login(username, password);
+    cy.manualLogin(username, password);
     cy.contains(username);
     cy.logout();
-    cy.login(adminUsername, adminPassword);
+    cy.manualLogin(adminUsername, adminPassword);
     cy.contains(adminUsername);
   });
 
-  it('can login as user and store cookie', () => {
-    cy.cookieLogin(username, password);
+  it('can use apiLogin', () => {
+    cy.apiLogin(adminUsername, adminPassword);
+    cy.contains(adminUsername);
+
+    cy.apiLogin(username, password);
     cy.contains(username);
-  });
-
-  it('can login as admin and store cookie', () => {
-    cy.cookieLogin(adminUsername, adminUsername);
-    cy.contains(adminUsername);
-  });
-
-  it('it can switch back to user using cookie storage', () => {
-    cy.cookieLogin(username, password);
-    cy.contains(username);
-  });
-
-  it('it can switch back to admin using cookie storage', () => {
-    cy.cookieLogin(adminUsername, adminUsername);
-    cy.contains(adminUsername);
-  });
-
-  it('can cookieLogin without cookie removal between its as admin or different user', () => {
-    cy.cookieLogin(username, password);
-    cy.contains(username);
-
-    cy.cookieLogin(adminUsername, adminPassword);
-    cy.contains(adminUsername);
   });
 });
