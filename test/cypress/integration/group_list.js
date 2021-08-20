a0fb0ed90a5e19fb732f67f2d126f7df45b6aa1b
Milan Pospisil <arkanus@seznam.cz>
2021-08-20 15:38:54 +0200

    All columns, paging, sorting, filtering

---
+++ test/cypress/integration/group_list.js
@@ -0,0 +1,57 @@
+import { range, sortBy } from 'lodash';
+
+describe('Group list tests for sorting, paging and filtering', () => {
+  let items = [];
+  let adminUsername = Cypress.env('username');
+  let adminPassword = Cypress.env('password');
+
+  before(() => {
+    cy.deleteTestGroups();
+    cy.deleteTestGroups();
+    cy.deleteTestGroups();
+    cy.deleteTestGroups();
+
+    range(21).forEach((i) => {
+      let item = { name: 'group_test' + i };
+      items.push(item);
+      cy.galaxykit('-i group create', item.name);
+    });
+
+    items = sortBy(items, 'name');
+  });
+
+  beforeEach(() => {
+    cy.login(adminUsername, adminPassword);
+  });
+
+  it('table contains all columns', () => {
+    cy.contains('Group');
+  });
+
+  it('items are sorted alphabetically and paging is working', () => {
+    cy.visit('/ui/group-list');
+    cy.get('.body:first').contains(items[0].name);
+
+    cy.get('.body:first').get('[aria-label="Go to next page"]:first').click();
+    cy.get('.body:first').contains(items[10].name);
+
+    cy.get('.body:first').get('[aria-label="Go to next page"]:first').click();
+    cy.get('.body:first').contains(items[20].name);
+  });
+
+  it('sorting is working', () => {
+    cy.visit('/ui/group-list');
+    cy.get('.body:first').get('[data-cy="sort_name"]').click();
+    cy.get('.body:first').contains(items[20].name);
+    cy.get('.body:first').contains(items[0].name).should('not.exist');
+  });
+
+  it('filter is working', () => {
+    cy.visit('/ui/group-list');
+    cy.get('.body:first')
+      .get('[placeholder="Filter by group"]:first')
+      .type('group_test0{enter}');
+    cy.get('.body:first').contains('group_test0');
+    cy.get('.body:first').contains('group_test1').should('not.exist');
+  });
+});
