2f07adce81058d31a5e862ca4f51fe9e042554b5
Milan Pospisil <arkanus@seznam.cz>
2021-09-14 10:29:07 +0200

    Basic test for task detail

---
+++ test/cypress/integration/task_management_detail.js
@@ -0,0 +1,38 @@
+describe('Task detail', () => {
+    let adminUsername = Cypress.env('username');
+    let adminPassword = Cypress.env('password');
+  
+    before(() => {
+      cy.login(adminUsername, adminPassword);
+      cy.visit('/ui/repositories?tab=remote');
+  
+      cy.intercept(
+        'POST',
+        Cypress.env('prefix') + '/content/rh-certified/v3/sync/',
+      ).as('sync');
+      cy.contains('button', 'Sync').click();
+      cy.wait('@sync');
+    });
+  
+    it('contains correct headers and field names.', () => {
+      cy.login(adminUsername, adminPassword);
+      cy.visit('/ui/tasks');
+      cy.contains('Pulp Ansible: Collections sync').click();
+
+      cy.contains('Pulp Ansible: Collections sync');
+      cy.contains('Task detail');
+      cy.contains('Task name');
+      cy.contains('Created on');
+      cy.contains('Finished at');
+      cy.contains('Task groups');
+      cy.contains('Task group');
+      cy.contains('Parent task');
+      cy.contains('Child task');
+      cy.contains('Reserve resources');
+      cy.contains('Type');
+      cy.contains('Name');
+      cy.contains('remotes');
+      cy.contains('rh-certified');
+      cy.contains('repositories');
+    });
+  });
