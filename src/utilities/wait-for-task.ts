d7e722b750e6f63353dd5e072dab00d24f0ef6eb
Martin Hradil <mhradil@redhat.com>
2021-09-24 20:17:12 +0000

    waitForTask - add a 10 attempt limit, reject after

--- src/utilities/wait-for-task.ts
+++ src/utilities/wait-for-task.ts
@@ -1,10 +1,17 @@
+import { t } from '@lingui/macro';
 import { TaskAPI } from 'src/api';
 
-export function waitForTask(task) {
+export function waitForTask(task, bailAfter = 10) {
   return TaskAPI.get(task).then((result) => {
     if (result.data.state !== 'completed') {
+      if (!bailAfter) {
+        return Promise.reject(
+          new Error(t`Giving up waiting for task after 10 attempts.`),
+        );
+      }
+
       return new Promise((r) => setTimeout(r, 5000)).then(() =>
-        waitForTask(task),
+        waitForTask(task, bailAfter - 1),
       );
     }
   });
