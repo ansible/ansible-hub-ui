c1fe78e36b8c038eb5ae5599c5dffbb83ab36439
Shaiah Emigh-Doyle <semighdo@redhat.com>
2021-10-07 11:59:25 -0400

    alert SSO user of token expiration

--- src/containers/token/token-standalone.tsx
+++ src/containers/token/token-standalone.tsx
@@ -9,6 +9,7 @@ import {
   Main,
   ClipboardCopy,
   EmptyStateUnauthorized,
+  DateComponent,
 } from 'src/components';
 import { ActiveUserAPI } from 'src/api';
 import { AppContext } from 'src/loaders/app-context';
@@ -29,6 +30,8 @@ class TokenPage extends React.Component<RouteComponentProps, IState> {
   render() {
     const { token } = this.state;
     let unauthorised = !this.context.user || this.context.user.is_anonymous;
+    const expiration = this.context.settings.GALAXY_TOKEN_EXPIRATION;
+    const expirationDate = new Date(Date.now() + 1000 * 60 * expiration);
 
     return (
       <React.Fragment>
@@ -45,6 +48,17 @@ class TokenPage extends React.Component<RouteComponentProps, IState> {
                   client.
                 </Trans>
               </p>
+              {!this.context.user.auth_provider.includes('django') && (
+                <div>
+                  <h2>{t`Expiration`}</h2>
+                  <p>
+                    <Trans>
+                      You are an SSO user. Your token will expire{' '}
+                      <DateComponent date={expirationDate.toISOString()} />.
+                    </Trans>
+                  </p>
+                </div>
+              )}
               <div className='pf-c-content'>
                 <Trans>
                   <b>WARNING</b> loading a new token will delete your old token.
