43254c5ea2b6f5df6d119f004337b2eb516ddbb0
Shaiah Emigh-Doyle <semighdo@redhat.com>
2021-11-02 12:54:59 -0400

    remove registries/containers before testing

--- test/cypress/integration/execution_environments_edit.js
+++ test/cypress/integration/execution_environments_edit.js
@@ -3,8 +3,57 @@ describe('execution environments', () => {
   let adminPassword = Cypress.env('password');
   let num = (~~(Math.random() * 1000000)).toString();
 
+  function deleteRegistries() {
+    cy.intercept(
+      'GET',
+      Cypress.env('prefix') + '_ui/v1/execution-environments/registries/?*',
+    ).as('registries');
+
+    cy.visit('/ui/registries');
+
+    cy.wait('@registries').then((result) => {
+      var data = result.response.body.data;
+      data.forEach((element) => {
+        cy.get(
+          'tr[aria-labelledby="' +
+            element.name +
+            '"] button[aria-label="Actions"]',
+        ).click();
+        cy.contains('a', 'Delete').click();
+        cy.contains('button', 'Delete').click();
+      });
+    });
+  }
+
+  function deleteContainers() {
+    cy.intercept(
+      'GET',
+      Cypress.env('prefix') + '_ui/v1/execution-environments/repositories/?*',
+    ).as('listLoad');
+
+    cy.visit('/ui/containers');
+
+    cy.wait('@listLoad').then((result) => {
+      var data = result.response.body.data;
+      data.forEach((element) => {
+        cy.get(
+          'tr[aria-labelledby="' +
+            element.name +
+            '"] button[aria-label="Actions"]',
+        ).click();
+        cy.contains('a', 'Delete').click();
+        cy.get('input[id=delete_confirm]').click();
+        cy.contains('button', 'Delete').click();
+        cy.wait('@listLoad', { timeout: 50000 });
+        cy.get('.pf-c-alert__action').click();
+      });
+    });
+  }
+
   before(() => {
     cy.login(adminUsername, adminPassword);
+    deleteRegistries();
+    deleteContainers();
     cy.addRemoteRegistry(`docker${num}`, 'https://registry.hub.docker.com/');
     cy.addRemoteContainer({
       name: `remotepine${num}`,
