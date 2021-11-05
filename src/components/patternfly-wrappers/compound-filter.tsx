0d41170871bf09b0fc7dbc42e72e88e2ef970e6b
MilanPospisil <arkanus@seznam.cz>
2021-11-05 10:42:37 +0100

    Empty search correct (#1195)
    * repair compound filter to not accept blank string
    
    * prettier
    
    (cherry picked from commit ee0a93d1959343eae8e8b6f615ef3ba72e773bef)

--- src/components/patternfly-wrappers/compound-filter.tsx
+++ src/components/patternfly-wrappers/compound-filter.tsx
@@ -97,7 +97,7 @@ export class CompoundFilter extends React.Component<IProps, IState> {
         <Button
           onClick={() => this.submitFilter()}
           variant={ButtonVariant.control}
-          isDisabled={!this.props.inputText}
+          isDisabled={!this.props.inputText.trim().length}
         >
           <SearchIcon></SearchIcon>
         </Button>
