bb5f0455ac9b3e27d5dd14f16eea06921ecea73a
ZitaNemeckova <znemecko@redhat.com>
2021-11-08 16:38:32 +0100

    Add test for manual Collection upload (#1084)
    * Add test for manual Collection upload
    
    Fixes https://issues.redhat.com/browse/AAH-1011
    
    * Use smaller file to prevent 413
    
    * Add missing files
    
    * Fix waiting and add check that it was uploaded successfully
    
    * Add wait
    
    * Fix upload
    
    * Move ignoring of fixtures from package.json to prettierignore to make less complicated

---
+++ test/cypress/integration/collection_upload.js
@@ -0,0 +1,39 @@
+describe('Collection Upload Tests', () => {
+  var adminUsername = Cypress.env('username');
+  var adminPassword = Cypress.env('password');
+
+  beforeEach(() => {
+    cy.login(adminUsername, adminPassword);
+  });
+
+  it('collection is uploaded', () => {
+    cy.intercept(
+      'GET',
+      Cypress.env('prefix') + '_ui/v1/collection-versions/?namespace=*',
+    ).as('upload');
+    cy.galaxykit('-i namespace create', 'ansible');
+    cy.menuGo('Collections > Namespaces');
+    cy.intercept('GET', Cypress.env('prefix') + '_ui/v1/repo/published/*').as(
+      'namespaces',
+    );
+
+    cy.get('a[href="/ui/repo/published/ansible"]').click();
+    cy.wait('@namespaces');
+    cy.contains('Upload collection').click();
+    cy.fixture('collections/ansible-network-1.2.0.tar.gz', 'binary')
+      .then(Cypress.Blob.binaryStringToBlob)
+      .then((fileContent) => {
+        cy.get('input[type="file"]').attachFile({
+          fileContent,
+          fileName: 'ansible-network-1.2.0.tar.gz',
+          mimeType: 'application/gzip',
+        });
+      });
+    cy.get('[data-cy="confirm-upload"]').click();
+    cy.wait('@upload');
+    cy.contains('My imports');
+    cy.get('.pf-c-label__content').contains('Running').should('exist');
+    cy.wait('@upload', { timeout: 10000 });
+    cy.get('.pf-c-label__content').contains('Completed').should('exist');
+  });
+});
