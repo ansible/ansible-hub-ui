085b5db2c3d131b84c52ffa1e580b89dd2789f11
Milan Pospisil <arkanus@seznam.cz>
2021-11-19 12:28:52 +0100

    Correction of test of empty string

--- test/cypress/integration/namespace_form.js
+++ test/cypress/integration/namespace_form.js
@@ -32,6 +32,8 @@ describe('A namespace form', () => {
   });
 
   it('should give message before typing', () => {
+    // error is shown only when start typing and then left empty
+    getInputBox().type('A{backspace}');
     getMessage().should('have.text', 'Please, provide the namespace name');
     getCreateButton().should('be.disabled');
   });
