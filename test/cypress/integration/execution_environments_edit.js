02d3b86c38fda4a8aa1892c308b54f6c523b2834
Milan Pospisil <arkanus@seznam.cz>
2021-11-19 12:37:40 +0100

    Fix the test

--- test/cypress/integration/execution_environments_edit.js
+++ test/cypress/integration/execution_environments_edit.js
@@ -26,7 +26,7 @@ describe('execution environments', () => {
 
   it('edits a remote container', () => {
     cy.contains('a', `remotepine${num}`).click();
-    cy.get('.pf-c-button.pf-m-primary').contains('Edit').click();
+    cy.get('.pf-c-button.pf-m-secondary').contains('Edit').click();
     cy.get('#description').type('This is the description.');
     cy.contains('button', 'Save').click();
     cy.wait(10000); // FIXME have a reload request, wait for it; can't wait for an unspecified number of task requests
@@ -38,7 +38,7 @@ describe('execution environments', () => {
 
   it('edits a local container', () => {
     cy.contains('a', `localpine${num}`).click();
-    cy.get('.pf-c-button.pf-m-primary').contains('Edit').click();
+    cy.get('.pf-c-button.pf-m-secondary').contains('Edit').click();
     cy.get('#description').type('This is the description.');
     cy.contains('button', 'Save').click();
     cy.wait(10000); // FIXME have a reload request, wait for it; can't wait for an unspecified number of task requests
