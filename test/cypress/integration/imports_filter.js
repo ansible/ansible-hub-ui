34feaf2f93e8ffa51da9ecebbe02f9785ce4627a
Milan Pospisil <arkanus@seznam.cz>
2021-12-06 12:46:49 +0100

    Add partial and exact fitler for name Issue AAH-788

---
+++ test/cypress/integration/imports_filter.js
@@ -0,0 +1,44 @@
+import { range, sortBy } from 'lodash';
+
+describe('Imports filter test', () => {
+  var adminUsername = Cypress.env('username');
+  var adminPassword = Cypress.env('password');
+
+  before(() => {
+    cy.login(adminUsername, adminPassword);
+
+    // insert test data
+
+    cy.galaxykit('-i collection upload filter_test_namespace my_collection1');
+    cy.galaxykit('-i collection upload filter_test_namespace my_collection2');
+    cy.galaxykit('-i collection upload filter_test_namespace different_name');
+    cy.galaxykit('-i collection upload filter_test_namespace different_name');
+  });
+
+  beforeEach(() => {
+    cy.login(adminUsername, adminPassword);
+    cy.visit('/ui/my-imports?namespace=filter_test_namespace');
+  });
+
+  it('partial filter for name is working.', () => {
+    cy.get('input[aria-label="keywords"').type('my_collection{enter}');
+    cy.get('.import-list-data').contains('my_collection1');
+    cy.get('.import-list-data').contains('my_collection2');
+    cy.get('.import-list-data').contains('different_name').should('not.exist');
+  });
+
+  it('exact filter for name is working.', () => {
+    cy.get('input[aria-label="keywords"').type('my_collection1{enter}');
+    cy.get('.import-list-data').contains('my_collection1');
+    cy.get('.import-list-data').contains('my_collection2').should('not.exist');
+    cy.get('.import-list-data').contains('different_name').should('not.exist');
+  });
+
+  it('Exact search for completed is working.', () => {
+    cy.get('.import-list button:first').click();
+    cy.contains('a', 'Status').click();
+
+    cy.get('.import-list button').click();
+    cy.contains('a', 'Completed').click();
+  });
+});
