c96a5283d8de186fbe63555a6bd46424fb87b3b3
Martin Maroši <marvusm.mmi@gmail.com>
2021-12-14 09:27:34 +0100

    Remove obsolete InsightsRoute.

--- src/loaders/insights/Routes.js
+++ src/loaders/insights/Routes.js
@@ -1,24 +1,8 @@
 import { Route, Switch, Redirect } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import React from 'react';
 import asyncComponent from 'src/utilities/asyncComponent';
-import some from 'lodash/some';
 import { Paths } from 'src/paths';
 
-/**
- * Aysnc imports of components
- *
- * https://webpack.js.org/guides/code-splitting/
- * https://reactjs.org/docs/code-splitting.html
- *
- * pros:
- *      1) code splitting
- *      2) can be used in server-side rendering
- * cons:
- *      1) nameing chunk names adds unnecessary docs to code,
- *         see the difference with DashboardMap and InventoryDeployments.
- *
- */
 const EditNamespace = asyncComponent(() =>
   import(
     /* webpackChunkName: "namespace_detail" */
@@ -131,20 +115,6 @@ const Repository = asyncComponent(() =>
   ),
 );
 
-const InsightsRoute = ({ component: Component, rootClass, ...rest }) => {
-  const root = document.getElementById('root');
-  root.removeAttribute('class');
-  root.classList.add(`page__${rootClass}`, 'pf-c-page__main');
-  root.setAttribute('role', 'main');
-
-  return <Route {...rest} component={Component} />;
-};
-
-InsightsRoute.propTypes = {
-  component: PropTypes.func,
-  rootClass: PropTypes.string,
-};
-
 /**
  * the Switch component changes routes depending on the path.
  *
@@ -158,145 +128,114 @@ export const Routes = (props) => {
 
   return (
     <Switch>
-      <InsightsRoute
+      <Route
         path={Paths.repositories}
         component={Repository}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.approvalDashboard}
         component={CertificationDashboard}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.notFound}
         component={NotFound}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.token}
         component={TokenPage}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths[NAMESPACE_TERM]}
         component={Partners}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.editNamespace}
         component={EditNamespace}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.myCollections}
         component={ManageNamespace}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.myCollectionsByRepo}
         component={ManageNamespace}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.myNamespaces}
         component={MyNamespaces}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.collectionDocsPageByRepo}
         component={CollectionDocs}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.collectionDocsIndexByRepo}
         component={CollectionDocs}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.collectionContentDocsByRepo}
         component={CollectionDocs}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.collectionContentListByRepo}
         component={CollectionContent}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.collectionImportLogByRepo}
         component={CollectionImportLog}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.collectionDependenciesByRepo}
         component={CollectionDependencies}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.collectionByRepo}
         component={CollectionDetail}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.namespaceByRepo}
         component={PartnerDetail}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.searchByRepo}
         component={Search}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.collectionDocsPage}
         component={CollectionDocs}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.collectionDocsIndex}
         component={CollectionDocs}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.collectionContentDocs}
         component={CollectionDocs}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.collectionContentList}
         component={CollectionContent}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.collectionImportLog}
         component={CollectionImportLog}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.myImports}
         component={MyImports}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.collection}
         component={CollectionDetail}
-        rootClass='root'
       />
-      <InsightsRoute
+      <Route
         path={Paths.namespace}
         component={PartnerDetail}
-        rootClass='root'
       />
-      <InsightsRoute path={Paths.search} component={Search} rootClass='root' />
-      {/* Finally, catch all unmatched routes */}
-      <Route
-        render={() =>
-          some(Paths, (p) => p === path) ? null : (
-            <Redirect push to={Paths.notFound} />
-          )
-        }
-      />
+      <Route path={Paths.search} component={Search} />
+      <Route>
+        <Redirect push to={Paths.notFound} />
+      </Route>
     </Switch>
   );
 };
