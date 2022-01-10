efab7ee1f56cab4ab335c19014a6470e08a74c87
Shaiah Emigh-Doyle <semighdo@redhat.com>
2022-01-10 17:19:08 -0500

    certification update success alert
    Issue: AAH-1236

--- src/containers/certification-dashboard/certification-dashboard.tsx
+++ src/containers/certification-dashboard/certification-dashboard.tsx
@@ -479,15 +479,24 @@ class CertificationDashboard extends React.Component<
           originalRepo,
           destinationRepo,
         )
-          .then((result) =>
-            // Since pulp doesn't reply with the new object, perform a
-            // second query to get the updated data
-            {
-              this.setState({
-                updatingVersions: [version],
-              });
-              this.waitForUpdate(result.data.remove_task_id, version);
-            },
+          .then(
+            (result) =>
+              // Since pulp doesn't reply with the new object, perform a
+              // second query to get the updated data
+              {
+                this.setState({
+                  updatingVersions: [version],
+                });
+                this.waitForUpdate(result.data.remove_task_id, version);
+              },
+            () =>
+              this.context.setAlerts([
+                ...this.context.alerts,
+                {
+                  variant: 'success',
+                  title: t`Certification status for ${version.namespace}.${version.name}.${version.version} has been successfully updated.`,
+                },
+              ]),
           )
           .catch((error) => {
             this.setState({
