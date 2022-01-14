b2091b80189730543c780b09ff0c4da5fd62e314
Milan Pospisil <arkanus@seznam.cz>
2022-01-14 11:19:13 +0100

    Added data-cy into the tests.

--- test/cypress/integration/approval_dashboard_list.js
+++ test/cypress/integration/approval_dashboard_list.js
@@ -3,26 +3,6 @@ import { range, sortBy } from 'lodash';
 describe('Approval Dashboard list tests for sorting, paging and filtering', () => {
   let items = [];
 
-  function deleteData() {
-    /*var intercept_url =
-      Cypress.env('prefix') +
-      '_ui/v1/collection-versions/?sort=-pulp_created&offset=0&limit=100';
-
-    cy.visit('/ui/approval-dashboard?page_size=100');
-    cy.intercept('GET', intercept_url).as('data');
-    cy.contains('button', 'Clear all filters').click();
-
-    cy.wait('@data').then((res) => {
-      var data = res.response.body.data;
-      data.forEach((record) => {
-        cy.galaxykit(
-          'collection delete ' + record.namespace + ' ' + record.name,
-        );
-      });
-    });*/
-    cy.deleteNamespacesAndCollections();
-  }
-
   function createData() {
     cy.galaxykit('-i namespace create approval_dashboard_namespace_test');
     range(21).forEach((i) => {
@@ -63,14 +43,12 @@ describe('Approval Dashboard list tests for sorting, paging and filtering', () =
       });
 
       items = sortBy(items, 'name');
-      cy.log(items.length);
-      cy.log(JSON.stringify(items));
     });
   }
 
   before(() => {
     cy.login();
-    deleteData();
+    cy.deleteNamespacesAndCollections();
     createData();
     loadData();
   });
@@ -84,7 +62,7 @@ describe('Approval Dashboard list tests for sorting, paging and filtering', () =
   it('table contains all columns', () => {
     ['Namespace', 'Collection', 'Version', 'Date created', 'Status'].forEach(
       (item) => {
-        cy.get('tr[aria-labelledby="headers"] th').contains(item);
+        cy.get('[data-cy="table_header"]').contains(item);
       },
     );
   });
@@ -93,85 +71,111 @@ describe('Approval Dashboard list tests for sorting, paging and filtering', () =
     cy.get('[data-cy="sort_collection"]').click();
     cy.get('[data-cy="sort_collection"]').click();
 
-    cy.get('.body').contains(items[0].name);
+    cy.get('[data-cy="body"]').contains(items[0].name);
 
-    cy.get('.body').get('[aria-label="Go to next page"]:first').click();
-    cy.get('.body').contains(items[10].name);
+    cy.get('[data-cy="body"]')
+      .get('[aria-label="Go to next page"]:first')
+      .click();
+    cy.get('[data-cy="body"]').contains(items[10].name);
 
-    cy.get('.body').get('[aria-label="Go to next page"]:first').click();
-    cy.get('.body').contains(items[20].name);
+    cy.get('[data-cy="body"]')
+      .get('[aria-label="Go to next page"]:first')
+      .click();
+    cy.get('[data-cy="body"]').contains(items[20].name);
   });
 
   it('sorting is working for collection', () => {
     cy.get('[data-cy="sort_collection"]').click();
-    cy.get('.body').contains('approval');
+    cy.get('[data-cy="body"]').contains('approval');
 
-    cy.get('.body tbody tr:first').contains(items[items.length - 1].name);
-    cy.get('.body tbody').contains(items[items.length - 2].name);
-    cy.get('.body tbody').contains(items[items.length - 3].name);
+    cy.get('[data-cy="body"] [data-cy="table_row"]:first').contains(
+      items[items.length - 1].name,
+    );
+    cy.get('[data-cy="body"] [data-cy="table_row"]').contains(
+      items[items.length - 2].name,
+    );
+    cy.get('[data-cy="body"] [data-cy="table_row"]').contains(
+      items[items.length - 3].name,
+    );
   });
 
   it('table contains some time informations', () => {
-    cy.contains('a few seconds ago');
+    cy.contains('[data-cy="body"]', 'a few seconds ago');
   });
 
   it('collection filter is working', () => {
-    cy.get('.body .toolbar button:first').click();
-    cy.contains('.body .toolbar a', 'Collection Name').click();
+    cy.get('[data-cy="body"] [data-cy="compound_filter"] button:first').click();
+    cy.contains(
+      '[data-cy="body"] [data-cy="compound_filter"] a',
+      'Collection Name',
+    ).click();
     cy.get('[data-cy="sort_collection"]').click();
     cy.get('[data-cy="sort_collection"]').click();
 
-    cy.get('.body .toolbar input').type(
+    cy.get('[data-cy="body"] [data-cy="compound_filter"] input').type(
       'approval_dashboard_collection_test0{enter}',
     );
-    cy.get('.body').contains('approval_dashboard_collection_test0');
-    cy.get('.body')
+    cy.get('[data-cy="body"]').contains('approval_dashboard_collection_test0');
+    cy.get('[data-cy="body"]')
       .contains('approval_dashboard_collection_test1')
       .should('not.exist');
   });
 
   it('collection filter and namespace filter is working', () => {
-    cy.get('.body .toolbar button:first').click();
-    cy.contains('.body .toolbar a', 'Collection Name').click();
-    cy.get('.body .toolbar input').type(
+    cy.get('[data-cy="body"] [data-cy="compound_filter"] button:first').click();
+    cy.contains(
+      '[data-cy="body"] [data-cy="compound_filter"] a',
+      'Collection Name',
+    ).click();
+    cy.get('[data-cy="body"] .toolbar input').type(
       'approval_dashboard_collection_test0{enter}',
     );
 
-    cy.get('.body .toolbar button:first').click();
-    cy.contains('.body .toolbar a', 'Namespace').click();
-    cy.get('.body .toolbar input').type(
+    cy.get('[data-cy="body"] .toolbar button:first').click();
+    cy.contains(
+      '[data-cy="body"] [data-cy="compound_filter"] a',
+      'Namespace',
+    ).click();
+    cy.get('[data-cy="body"] [data-cy="compound_filter"] input').type(
       'approval_dashboard_namespace_test{enter}',
     );
 
     cy.get('[data-cy="sort_collection"]').click();
     cy.get('[data-cy="sort_collection"]').click();
 
-    cy.get('.body').contains('approval_dashboard_collection_test0');
-    cy.get('.body')
+    cy.get('[data-cy="body"]').contains('approval_dashboard_collection_test0');
+    cy.get('[data-cy="body"]')
       .contains('approval_dashboard_collection_test1')
       .should('not.exist');
-    cy.get('.body')
+    cy.get('[data-cy="body"]')
       .contains('approval_dashboard_namespace_test_additional_data')
       .should('not.exist');
   });
 
   it('filter non existing namespace is working and not showing any data', () => {
-    cy.get('.body .toolbar button:first').click();
-    cy.contains('.body .toolbar a', 'Namespace').click();
-    cy.get('.body .toolbar input').type('namespace1354564sdfhdfhhfdf{enter}');
+    cy.get('[data-cy="body"] [data-cy="compound_filter"] button:first').click();
+    cy.contains(
+      '[data-cy="body"] [data-cy="compound_filter"] a',
+      'Namespace',
+    ).click();
+    cy.get('[data-cy="body"] [data-cy="compound_filter"] input').type(
+      'namespace1354564sdfhdfhhfdf{enter}',
+    );
 
-    cy.get('.body').contains('No results found');
+    cy.get('[data-cy="body"]').contains('No results found');
   });
 
   it('set page size is working', () => {
-    cy.get('.body').get('button[aria-label="Items per page"]:first').click();
-    cy.get('.body').contains('20 per page').click();
+    cy.get('[data-cy="body"]')
+      .get('button[aria-label="Items per page"]:first')
+      .click();
+    cy.get('[data-cy="body"]').contains('20 per page').click();
 
     cy.get('[data-cy="sort_collection"]').click();
     cy.get('[data-cy="sort_collection"]').click();
 
     range(20).forEach((i) => {
-      cy.get('.body').contains(items[i].name);
+      cy.get('[data-cy="body"]').contains(items[i].name);
     });
   });
 });
