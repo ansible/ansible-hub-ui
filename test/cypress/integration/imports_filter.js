fecc056ecaed7b81c40add6fd418f11225de1a04
Milan Pospisil <arkanus@seznam.cz>
2022-01-26 16:11:49 +0100

    Waiting for 10 seconds.

--- test/cypress/integration/imports_filter.js
+++ test/cypress/integration/imports_filter.js
@@ -54,7 +54,7 @@ describe('Imports filter test', () => {
   });
 
   it('Exact search for completed is working.', () => {
-    cy.wait(10);
+    cy.wait(10000);
     cy.get('.import-list button:first').click();
     cy.contains('a', 'Status').click();
 
