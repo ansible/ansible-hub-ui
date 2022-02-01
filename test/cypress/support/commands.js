637165ce2f70307b00ea5637ff5be6c22abb1f17
Milan Pospisil <arkanus@seznam.cz>
2022-02-01 09:48:09 +0100

    Try admin

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -699,7 +699,7 @@ Cypress.Commands.add('clearDatabase', {}, () => {
     cy.log('Dumping database to pg_dump.dump');
     let dump = Cypress.env('dump');
     dump =
-      'podman exec pulp pg_dump -U galaxy_ng -d galaxy_ng -Fc > pg_dump.dump;';
+      'podman exec pulp pg_dump -U admin -d galaxy_ng -Fc > pg_dump.dump;';
     cy.log(dump);
     cy.exec(dump);
 
