576f8dc3be51049d7a9237d94acbd6b5404835c1
Martin Hradil <mhradil@redhat.com>
2022-02-02 19:27:04 +0000

    TODO pulp api prefix

--- src/api/pulp.ts
+++ src/api/pulp.ts
@@ -2,6 +2,7 @@ import { BaseAPI } from './base';
 
 export class PulpAPI extends BaseAPI {
   constructor() {
+    // FIXME this will have a prefix, also update containers/task-management/task_detail.tsx
     super('/pulp/api/v3/');
   }
 }
