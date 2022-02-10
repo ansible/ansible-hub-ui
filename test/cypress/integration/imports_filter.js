a02e4372f53babc35bdc3e7866b61a603244806a
MilanPospisil <arkanus@seznam.cz>
2022-02-10 09:08:02 +0100

    Testing and searching for sporadic failures (#1528)
    * Testing and searching for sporadic failures
    Issue: AAH-1282
    
    * Correct error
    
    * Correct wait.
    
    * Change order of waitings to ensure the right execution.:
    
    * Added intercepts.
    
    * Try atrbitrary waiting
    
    * Waiting for 10 seconds.
    
    * WIP

--- test/cypress/integration/imports_filter.js
+++ test/cypress/integration/imports_filter.js
@@ -10,29 +10,48 @@ describe('Imports filter test', () => {
     cy.galaxykit('-i collection upload filter_test_namespace different_name');
   });
 
+  after(() => {
+    cy.deleteNamespacesAndCollections();
+  });
+
   beforeEach(() => {
     cy.login();
     cy.visit('/ui/my-imports?namespace=filter_test_namespace');
   });
 
   it('partial filter for name is working.', () => {
+    cy.intercept(
+      'GET',
+      Cypress.env('prefix') + '_ui/v1/collection-versions/?namespace=*',
+    ).as('wait');
+
     cy.get('input[aria-label="keywords"').type('my_collection{enter}');
-    cy.get('[data-cy="import-list-data"]').contains('my_collection1');
-    cy.get('[data-cy="import-list-data"]').contains('my_collection2');
+    cy.wait('@wait');
+
     cy.get('[data-cy="import-list-data"]')
       .contains('different_name')
       .should('not.exist');
+    cy.get('[data-cy="import-list-data"]').contains('my_collection1');
+    cy.get('[data-cy="import-list-data"]').contains('my_collection2');
   });
 
   it('exact filter for name is working.', () => {
+    cy.intercept(
+      'GET',
+      Cypress.env('prefix') + '_ui/v1/collection-versions/?namespace=*',
+    ).as('wait');
+
     cy.get('input[aria-label="keywords"').type('my_collection1{enter}');
-    cy.get('[data-cy="import-list-data"]').contains('my_collection1');
+    cy.wait('@wait');
+
     cy.get('[data-cy="import-list-data"]')
       .contains('my_collection2')
       .should('not.exist');
     cy.get('[data-cy="import-list-data"]')
       .contains('different_name')
       .should('not.exist');
+    cy.get('[data-cy="import-list-data"]').contains('my_collection1');
+    cy.wait(10000);
   });
 
   it('Exact search for completed is working.', () => {
@@ -48,11 +67,11 @@ describe('Imports filter test', () => {
     ).as('wait');
     cy.contains('a', 'Completed').click();
 
+    cy.wait('@wait');
+
     cy.get('[data-cy="import-list-data"]').contains('my_collection1');
     cy.get('[data-cy="import-list-data"]').contains('my_collection2');
     cy.get('[data-cy="import-list-data"]').contains('different_name');
-
-    cy.wait('@wait');
   });
 
   it('Exact search for waiting is working.', () => {
@@ -78,12 +97,12 @@ describe('Imports filter test', () => {
     ).as('wait');
     cy.contains('a', 'Completed').click();
 
-    cy.get('[data-cy="import-list-data"]').contains('my_collection1');
+    cy.wait('@wait');
+
     cy.get('[data-cy="import-list-data"]')
       .contains('different_name')
       .should('not.exist');
-
-    cy.wait('@wait');
+    cy.get('[data-cy="import-list-data"]').contains('my_collection1');
   });
 
   it('Partial search for name and completed is working.', () => {
@@ -99,12 +118,11 @@ describe('Imports filter test', () => {
       Cypress.env('prefix') + '_ui/v1/collection-versions/?namespace=*',
     ).as('wait');
     cy.contains('a', 'Completed').click();
+    cy.wait('@wait');
 
     cy.get('[data-cy="import-list-data"]').contains('my_collection2');
     cy.get('[data-cy="import-list-data"]')
       .contains('different_name')
       .should('not.exist');
-
-    cy.wait('@wait');
   });
 });
