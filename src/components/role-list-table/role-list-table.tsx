1032252905153b039c17951fd2f485cdd308eb2e
jerabekjiri <jerabekjirka@email.cz>
2022-03-30 23:41:08 +0200

    role list table rework
    No-Issue

--- src/components/role-list-table/role-list-table.tsx
+++ src/components/role-list-table/role-list-table.tsx
@@ -1,15 +1,11 @@
 import { t } from '@lingui/macro';
-import { i18n } from '@lingui/core';
 
 import React, { useState } from 'react';
 
 import {
-  StatefulDropdown,
   SortTable,
-  PermissionChipSelector,
 } from 'src/components';
 
-import { Flex, FlexItem } from '@patternfly/react-core';
 import {
   TableComposable,
   Tr,
@@ -17,21 +13,11 @@ import {
   Td,
   ExpandableRowContent,
 } from '@patternfly/react-table';
-
-import { RoleType } from 'src/api';
-
-import { twoWayMapper } from 'src/utilities';
-
-import { Constants } from 'src/constants';
-
 interface Props {
-  roles: RoleType[];
-  dropdownActions?: JSX.Element[];
-  isUserManagementDisabled?: boolean;
+  children;
   params?: object;
   updateParams?: (params) => void;
   isCompact?: boolean;
-  variant?: 'expandable' | 'checkbox' | 'radio';
   tableHeader?: {
     headers: {
       title: string;
@@ -39,79 +25,17 @@ interface Props {
       id: string;
     }[];
   };
-  selected?: RoleType[] | RoleType;
-  onSelect?: (selectedRole: RoleType, selectedRoles?: RoleType[]) => void;
   isStickyHeader?: boolean;
 }
 
-export const RoleListTable = ({
-  roles,
-  isUserManagementDisabled,
-  dropdownActions,
+export const RoleListTable: React.FC<Props> = ({
+  children,
   params,
   updateParams,
   isCompact,
-  variant = 'expandable',
   tableHeader,
-  selected,
-  onSelect,
   isStickyHeader = false,
 }: Props) => {
-  if (variant === 'checkbox' && !Array.isArray(selected)) {
-    throw new Error(
-      'Invalid type passed into `selected`. Variant `checkbox` expected type Array.',
-    );
-  }
-
-  if (variant === 'radio' && Array.isArray(selected)) {
-    throw new Error(
-      'Invalid type passed into `selected`. Variant `radio` expected type Object.',
-    );
-  }
-
-  const [expandedRoles, setExpandedRoles] = useState<string[]>([]);
-
-  const isRoleExpanded = (role) => expandedRoles.includes(role);
-
-  const isRoleSelected = (role: RoleType) => {
-    return (selected as RoleType[])
-      .map((role) => role.name)
-      .includes(role.name);
-  };
-
-  const handleToggleRole = (role) => {
-    setExpandedRoles((prevState) =>
-      prevState.includes(role)
-        ? prevState.filter((r) => r !== role)
-        : [...prevState, role],
-    );
-  };
-
-  const onSelectRole = (role) => {
-    if (variant === 'checkbox') {
-      const selectedRoles = selected as RoleType[];
-      const updatedRoles = isRoleSelected(role)
-        ? selectedRoles.filter((r) => r.name !== role.name)
-        : [...selectedRoles, role];
-      onSelect(role, updatedRoles);
-    }
-
-    if (variant === 'radio') {
-      onSelect(role as RoleType);
-    }
-  };
-
-  const allPermissions = [...Constants.PERMISSIONS];
-  const filteredPermissions = { ...Constants.HUMAN_PERMISSIONS };
-
-  if (isUserManagementDisabled) {
-    Constants.USER_GROUP_MGMT_PERMISSIONS.forEach((perm) => {
-      if (perm in filteredPermissions) {
-        delete filteredPermissions[perm];
-      }
-    });
-  }
-
   const defaultTableHeader = {
     headers: [
       {
@@ -149,123 +73,77 @@ export const RoleListTable = ({
         params={params}
         updateParams={updateParams}
       />
-      {roles.map((role, rowIndex) => (
-        <Tbody key={rowIndex} isExpanded={isRoleExpanded(role.name)}>
-          <Tr>
-            {variant === 'expandable' && (
-              <Td
-                expand={{
-                  isExpanded: isRoleExpanded(role.name),
-                  onToggle: () => handleToggleRole(role.name),
-                  rowIndex,
-                }}
-              />
-            )}
-
-            {variant === 'checkbox' && (
-              <Td
-                select={{
-                  rowIndex,
-                  onSelect: () => onSelectRole(role),
-                  isSelected: isRoleSelected(role),
-                  variant: 'checkbox',
-                }}
-              />
-            )}
-
-            {variant === 'radio' && (
-              <Td
-                select={{
-                  rowIndex,
-                  onSelect: () => onSelectRole(role),
-                  isSelected: (selected as RoleType)?.name === role.name,
-                  variant: 'radio',
-                }}
-              />
-            )}
-            <Td>{role.name}</Td>
-            <Td>{role.description}</Td>
-            {dropdownActions && (
-              <Td style={{ paddingRight: '0px', textAlign: 'right' }}>
-                <div data-cy='kebab-toggle'>
-                  <StatefulDropdown
-                    items={dropdownActions.map((item, i) => {
-                      if (!item?.props?.onClick) {
-                        return item;
-                      }
-                      const updatedProps = { ...item.props };
-                      delete updatedProps['onClick'];
-
-                      const updatedItem = {
-                        ...item,
-                        props: updatedProps,
-                      };
-
-                      return (
-                        <div
-                          key={i}
-                          onClick={(e) => {
-                            e.preventDefault();
-                            item.props.onClick(role);
-                          }}
-                        >
-                          {updatedItem}
-                        </div>
-                      );
-                    })}
-                  />
-                </div>
-              </Td>
-            )}
-          </Tr>
-          <Tr isExpanded={isRoleExpanded(role.name)}>
-            <Td colSpan={4}>
-              <ExpandableRowContent>
-                {Object.keys(role?.permissions).length > 0 &&
-                  allPermissions.map((group) => (
-                    <Flex
-                      style={{ marginTop: '16px' }}
-                      alignItems={{ default: 'alignItemsCenter' }}
-                      key={group.name}
-                      className={group.name}
-                    >
-                      <FlexItem style={{ minWidth: '200px' }}>
-                        {i18n._(group.label)}
-                      </FlexItem>
-                      <FlexItem grow={{ default: 'grow' }}>
-                        <PermissionChipSelector
-                          availablePermissions={group.object_permissions
-                            .filter(
-                              (perm) =>
-                                !role.permissions.find(
-                                  (selected) => selected === perm,
-                                ),
-                            )
-                            .map((value) =>
-                              twoWayMapper(value, filteredPermissions),
-                            )
-                            .sort()}
-                          selectedPermissions={role.permissions
-                            .filter((selected) =>
-                              group.object_permissions.find(
-                                (perm) => selected === perm,
-                              ),
-                            )
-                            .map((value) =>
-                              twoWayMapper(value, filteredPermissions),
-                            )}
-                          menuAppendTo='inline'
-                          multilingual={true}
-                          isViewOnly={true}
-                        />
-                      </FlexItem>
-                    </Flex>
-                  ))}
-              </ExpandableRowContent>
-            </Td>
-          </Tr>
-        </Tbody>
-      ))}
+      {children}
     </TableComposable>
   );
 };
+
+export const ExpandableRow: React.FC<{
+  rowIndex?: number;
+  expandableRowContent?: React.ReactNode;
+  colSpan?: number;
+}> = ({ rowIndex, children, expandableRowContent, colSpan }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <Tbody isExpanded={isExpanded}>
+      <Tr>
+        <Td
+          expand={{
+            onToggle: () => setIsExpanded(!isExpanded),
+            isExpanded,
+            rowIndex,
+          }}
+        />
+        {children}
+      </Tr>
+      {expandableRowContent && (
+        <Tr isExpanded={isExpanded}>
+          <Td colSpan={colSpan ?? 4}>
+            <ExpandableRowContent>{expandableRowContent}</ExpandableRowContent>
+          </Td>
+        </Tr>
+      )}
+    </Tbody>
+  );
+};
+
+export const CheckboxRow: React.FC<{
+  rowIndex?: number;
+  isSelected: boolean;
+  onSelect: (value) => void;
+}> = ({ rowIndex, children, isSelected, onSelect }) => (
+  <Tbody>
+    <Tr>
+      <Td
+        select={{
+          variant: 'checkbox',
+          rowIndex,
+          onSelect,
+          isSelected,
+        }}
+      />
+      {children}
+    </Tr>
+  </Tbody>
+);
+
+export const RadioRow: React.FC<{
+  rowIndex?: number;
+  isSelected: boolean;
+  onSelect: (value) => void;
+}> = ({ rowIndex, children, isSelected, onSelect }) => (
+  <Tbody>
+    <Tr>
+      <Td
+        select={{
+          variant: 'radio',
+          rowIndex,
+          onSelect,
+          isSelected,
+        }}
+      />
+      {children}
+    </Tr>
+  </Tbody>
+);
