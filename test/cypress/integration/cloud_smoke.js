0abfa10cd1b92e713aefa3421a9c4060dbf213ea
James Tanner <tanner.jc@gmail.com>
2022-03-31 15:43:35 -0400

    Alter cloud smoke to work with ephemeral
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- test/cypress/integration/cloud_smoke.js
+++ test/cypress/integration/cloud_smoke.js
@@ -11,6 +11,15 @@ const cKwargs = { timeout: 10000 };
 
 describe(['cloud'], 'cloud smoketest', () => {
   before(() => {
+
+    // TODO create a single collection ...
+    const suffix = Math.floor(Math.random() * 10000);
+    const collectionNamespace = `namespace_${suffix}`;
+    const collectionName = `collection_${suffix}`;
+    cy.galaxykit(`namespace create ${collectionNamespace}`);
+    cy.galaxykit(`collection upload ${collectionNamespace} ${collectionName}`);
+    cy.galaxykit(`collection move ${collectionNamespace} ${collectionName}`);
+
     // make the browser larger so that the menus are visible
     cy.viewport(2000, 2000);
 
@@ -40,16 +49,23 @@ describe(['cloud'], 'cloud smoketest', () => {
 
   describe('with download', () => {
     it('can load a Collection', () => {
+
+      // TODO create a single collection ...
+      //cy.galaxykit('namespace create my_namespace');
+      //cy.galaxykit('-i collection upload my_namespace my_collection');
+
       // wait for collections to appear
       cy.get('.collection-container', cKwargs).should('be.visible');
 
       // wait for collections to load
-      cy.get('.hub-c-card-collection-container .name a', cKwargs).should(
+      //cy.get('.hub-c-card-collection-container .name a', cKwargs).should(
+      cy.get('.collection-card-container .name a', cKwargs).should(
         'exist',
       );
 
       // click on the first available collection
-      cy.get('.hub-c-card-collection-container .name a', cKwargs)
+      //cy.get('.hub-c-card-collection-container .name a', cKwargs)
+      cy.get('.collection-card-container .name a', cKwargs)
         .first()
         .click();
 
@@ -61,19 +77,27 @@ describe(['cloud'], 'cloud smoketest', () => {
     });
 
     it('can load a Partners list of collections', () => {
+
+      // TODO create a single collection ...
+      //cy.galaxykit('namespace create my_namespace');
+      //cy.galaxykit('-i collection upload my_namespace my_collection');
+
       // wait for the Partners button to appear and then click on it
       cy.get('[data-ouia-component-id="Partners"]', cKwargs).click();
 
       // wait for the the Partners list to appear
-      cy.get('.hub-namespace-page', cKwargs).should('be.visible');
+      //cy.get('.hub-namespace-page', cKwargs).should('be.visible');
+      cy.get('.namespace-page', cKwargs).should('be.visible');
 
       // click on the first available Partner
-      cy.get('.hub-c-card-ns-container .pf-c-card__footer a', cKwargs)
+      //cy.get('.hub-c-card-ns-container .pf-c-card__footer a', cKwargs)
+      cy.get('.ns-card-container .pf-c-card__footer a', cKwargs)
         .first()
         .click();
 
-      // wait for the summary to load
-      cy.get('.namespace-detail', cKwargs).should('be.visible');
+      // wait for the summary|list to load
+      //cy.get('.namespace-detail', cKwargs).should('be.visible');
+      cy.get('[aria-label="List of Collections"]', cKwargs).should('be.visible');
 
       // ensure the upload collection button appears
       cy.contains('button', 'Upload collection').should('exist');
@@ -89,8 +113,8 @@ describe(['cloud'], 'cloud smoketest', () => {
       // wait for the the repository list to appear
       cy.get('.repository-list', cKwargs).should('be.visible');
 
-      // ensure the get token button appears
-      cy.contains('button', 'Get token').should('exist');
+      // ensure the get token button appears ... NOT IN EPHEMERAL?
+      //cy.contains('button', 'Get token').should('exist');
     });
 
     it('can load the Connect to hub page', () => {
