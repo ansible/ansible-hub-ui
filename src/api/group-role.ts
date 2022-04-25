676be256ac74cbe64090d5784189801816d0563b
Martin Hradil <mhradil@redhat.com>
2022-04-25 04:27:00 +0000

    GroupRoleAPI, PulpAPI - move getRolesWithPermissions outside api, keep only listRoles; use apiPath instead of custom constructor

--- src/api/group-role.ts
+++ src/api/group-role.ts
@@ -1,47 +1,21 @@
 import { PulpAPI } from './pulp';
-import { RoleAPI } from './role';
 
 class API extends PulpAPI {
-  constructor() {
-    super('groups/');
-  }
+  apiPath = 'groups/';
 
-  getRolesWithPermissions(id, params?) {
-    const assignedRoles = this.list(params, `${id}/roles/`);
-
-    const allRoles = RoleAPI.list();
-
-    return Promise.all([assignedRoles, allRoles]).then(([assigned, all]) => {
-      // match roles with assigned roles
-      const results = assigned.data.results
-        .map(({ role, pulp_href }) => {
-          const data = all['data'].results.find(({ name }) => name === role);
-          if (data) {
-            return {
-              ...data,
-              // swap pulp_href role with assigned pulp_href role
-              // to delete the assigned role
-              pulp_href,
-            };
-          }
-        })
-        .filter(Boolean);
-
-      return {
-        data: results,
-        count: results.length,
-      };
+  listRoles(groupId, params?) {
+    return this.http.get(`${this.apiPath}${groupId}/roles/`, {
+      params: this.mapPageToOffset(params),
     });
   }
 
-  removeRole(id, pulpId) {
-    return this.http.delete(`${id}/roles/${pulpId}/`);
+  removeRole(groupId, roleId) {
+    return this.http.delete(`${this.apiPath}${groupId}/roles/${roleId}/`);
   }
 
-  addRoleToGroup(id, role, content_object = null) {
-    return this.http.post(`${id}/roles/`, {
+  addRoleToGroup(groupId, role) {
+    return this.http.post(`${this.apiPath}${groupId}/roles/`, {
       role: role.name,
-      content_object,
     });
   }
 }
