45cbf44510c4f9ba396b5bf7a0ff753918f1f930
Martin Hradil <mhradil@redhat.com>
2022-04-28 04:46:19 +0000

    docker/entrypoint.sh - show an error when using ANSIBLE_HUB_UI_PATH in insights mode
    this would technically work as configured,
    except `npm run start` now involves running docker to start more container
    
    dockerd won't start in the existing alpine env, failing on missing cgroups v2
    and there's no podman apk
    switching to bullseye based node container might still work though

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 if [[ $DEPLOYMENT_MODE == 'insights' ]]
 then
-  npm run start
+  echo 1>&2
+  echo 'ANSIBLE_HUB_UI_PATH not supported in insights mode' 1>&2
+  echo 1>&2
+  echo 'please run' 1>&2
+  echo '  npm run start' 1>&2
+  echo 'in the UI dir manually' 1>&2
+  echo 1>&2
 else
   npm run start-standalone
 fi
