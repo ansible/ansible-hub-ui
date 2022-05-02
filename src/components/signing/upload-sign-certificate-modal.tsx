581c9c49d2309fb080afcefd2f8c368b064c7f14
Levente Berky <levente.berky@gmail.com>
2022-05-02 17:10:52 +0200

    Fixed translation strings
    No-Issue

--- src/components/signing/upload-sign-certificate-modal.tsx
+++ src/components/signing/upload-sign-certificate-modal.tsx
@@ -54,7 +54,7 @@ export const UploadSingCertificateModal: React.FC<Props> = ({
       <FileUpload
         id='certificate-file'
         filename={filename}
-        filenamePlaceholder='Drag and drop a file or upload one'
+        filenamePlaceholder={t`Drag and drop a file or upload one'`}
         browseButtonText={t`Select file`}
         onFileInputChange={handleFileInputChange}
         onClearClick={() => setFilename('')}
