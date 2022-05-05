5c1ec4b57f5f5c2ec121bc995609f0a799a158f3
Shaiah Emigh-Doyle <semighdo@redhat.com>
2022-05-05 09:25:53 -0400

    cleanup unused packages
    No-Issue

--- test/cypress/support/index.js
+++ test/cypress/support/index.js
@@ -16,7 +16,6 @@
 // Import commands.js using ES2015 syntax:
 import './commands';
 import 'cypress-file-upload';
-import 'cypress-real-events/support';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
