425356949c7635e71aee7d61fa609c148543a547
Shaiah Emigh-Doyle <semighdo@redhat.com>
2022-05-10 16:32:42 -0400

    remove collection from ns
    No-Issue

--- test/cypress/integration/namespace_delete.js
+++ test/cypress/integration/namespace_delete.js
@@ -32,30 +32,9 @@ describe('Delete a namespace', () => {
     cy.galaxykit('-i namespace create', 'ansible');
     cy.menuGo('Collections > Namespaces');
     cy.wait('@reload');
-    cy.get('a[href*="ui/repo/published/ansible"]').click();
-
-    //upload a collection
-
-    cy.contains('Upload collection').click();
-    cy.fixture('collections/ansible-network-1.2.0.tar.gz', 'binary')
-      .then(Cypress.Blob.binaryStringToBlob)
-      .then((fileContent) => {
-        cy.get('input[type="file"]').attachFile({
-          fileContent,
-          fileName: 'ansible-network-1.2.0.tar.gz',
-          mimeType: 'application/gzip',
-        });
-      });
-    cy.intercept(
-      'GET',
-      Cypress.env('prefix') + '_ui/v1/collection-versions/?namespace=*',
-    ).as('upload');
-    cy.get('[data-cy="confirm-upload"]').click();
-    cy.wait('@upload');
 
     // attempt deletion
 
-    cy.menuGo('Collections > Namespaces');
     cy.get('a[href*="ui/repo/published/ansible"]').click();
     cy.get('[data-cy=ns-kebab-toggle]').click();
     cy.contains('Delete namespace').click({ force: true });
