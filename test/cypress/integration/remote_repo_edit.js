e3ec89baa7b6fe51372ededa0498a5db4a331352
Shaiah Emigh-Doyle <semighdo@redhat.com>
2022-05-13 11:46:41 -0400

    adjust tests for rh-certified
    Issue: AAH-618

--- test/cypress/integration/remote_repo_edit.js
+++ test/cypress/integration/remote_repo_edit.js
@@ -10,9 +10,17 @@ describe('edit a remote repository', () => {
     cy.get('[aria-label="Actions"]:first').click(); // click the kebab menu on the 'community' repo
     cy.contains('Edit').click();
     cy.get('input[id="name"]').should('be.disabled'); //has a readonly name field
-    cy.contains('Show advanced options').click(); // advanced options button is working
+    cy.get('input[id="url"]').clear();
+    cy.get('button').contains('Save').should('be.disabled'); // Save button disabled when required fields are missing
+    cy.get('[id="url-helper"]').should(
+      'have.text',
+      `The URL needs to be in 'http(s)://' format.`,
+    );
+
+    cy.contains('Show advanced options').click();
 
     // enter new values
+    cy.get('input[id="url"]').type('https://galaxy.ansible.com/api/');
     cy.get('input[id="username"]').type('test');
     cy.get('input[id="password"]').type('test');
     cy.get('input[id="proxy_url"]').type('https://example.org');
@@ -65,8 +73,18 @@ describe('edit a remote repository', () => {
     cy.visit(remoteRepoUrl);
     cy.get('[data-cy="kebab-toggle"]').eq(1).click(); // click the kebab menu on the 'rh-certified' repo
     cy.contains('Edit').click();
+    cy.get('input[id="name"]').should('be.disabled'); //has a readonly name field
+    cy.get('input[id="url"]').clear();
+    cy.get('button').contains('Save').should('be.disabled'); // Save button disabled when required fields are missing
+    cy.get('[id="url-helper"]').should(
+      'have.text',
+      `The URL needs to be in 'http(s)://' format.`,
+    );
     cy.contains('Show advanced options').click();
     // enter new values
+    cy.get('input[id="url"]').type(
+      'https://cloud.redhat.com/api/automation-hub/',
+    );
     cy.get('input[id="username"]').type('test');
     cy.get('input[id="password"]').type('test');
 
@@ -81,30 +99,29 @@ describe('edit a remote repository', () => {
     cy.wait('@editRemote');
 
     // verify values have been saved properly.
-    cy.get('[aria-label="Actions"]').eq(1).click(); // click the kebab menu on the 'community' repo
+    cy.get('[aria-label="Actions"]').eq(1).click(); // click the kebab menu on the 'rh-certified' repo
     cy.contains('Edit').click();
     cy.contains('Show advanced options').click();
     cy.get('input[id="username"]').should('have.value', 'test');
     cy.get('input[id="proxy_url"]').should('have.value', 'https://example.org');
     cy.get('input[id="proxy_username"]').should('have.value', 'test');
+    cy.intercept(
+      'PUT',
+      Cypress.env('prefix') + 'content/community/v3/sync/config/',
+    ).as('editRemote');
     cy.contains('Save').click();
-  });
+    cy.wait('@editRemote');
 
-  //requirements
-
-  it('has Save button disabled when required fields are msising', () => {
-    cy.get('button').contains('Save').should('be.disabled');
-  });
-
-  it('has a readonly name field', () => {
-    cy.get('button').contains('Save').should('be.disabled');
-  });
-
-  it('has error messages for wrongly filled fields', () => {
-    cy.get('button').contains('Save').should('be.disabled');
-  });
-
-  it('shows and hides advanced options', () => {
-    cy.get('button').contains('Save').should('be.disabled');
+    // clear all values
+    cy.get('[aria-label="Actions"]').eq(1).click(); // click the kebab menu on the 'rh-certified' repo
+    cy.contains('Edit').click();
+    cy.contains('Show advanced options').click();
+    cy.get('input[id="username"]').clear();
+    cy.get('input[type="password"]')
+      .siblings('button')
+      .click({ multiple: true });
+    cy.get('input[id="proxy_url"]').clear();
+    cy.get('input[id="proxy_username"]').clear();
+    cy.contains('Save').click();
   });
 });
