7730a6414b0ebd936c8a09bba82b903f81bd095b
Shaiah Emigh-Doyle <semighdo@redhat.com>
2022-06-13 09:58:15 -0400

    use canEdit to determind sync buton visibility
    Issue: AAH-1694

--- src/containers/execution-environment-list/execution_environment_list.tsx
+++ src/containers/execution-environment-list/execution_environment_list.tsx
@@ -351,12 +351,11 @@ class ExecutionEnvironmentList extends React.Component<
           {t`Edit`}
         </DropdownItem>
       ),
-      item.pulp.repository.remote &&
-        this.context.user?.model_permissions?.change_containernamespace && (
-          <DropdownItem key='sync' onClick={() => this.sync(item.name)}>
-            {t`Sync from registry`}
-          </DropdownItem>
-        ),
+      item.pulp.repository.remote && canEdit && (
+        <DropdownItem key='sync' onClick={() => this.sync(item.name)}>
+          {t`Sync from registry`}
+        </DropdownItem>
+      ),
       <DropdownItem
         key='publish-to-controller'
         onClick={() => {
