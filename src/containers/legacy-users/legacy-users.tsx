10244e62d23a74f7eb6d4088ac83f85d4fe37b0c
James Tanner <tanner.jc@gmail.com>
2022-06-16 14:40:28 -0400

    user pagination

--- src/containers/legacy-users/legacy-users.tsx
+++ src/containers/legacy-users/legacy-users.tsx
@@ -26,10 +26,12 @@ import { AppContext } from 'src/loaders/app-context';
 interface IProps {
   legacyusers: LegacyUserListType[];
   //numberOfResults: number;
-  itemCount: number;
+  mounted: boolean;
+  count: number;
   params: {
     page?: number;
     page_size?: number;
+    order_by?: string;
     keywords?: string;
     tags?: string[];
     view_type?: string;
@@ -45,18 +47,74 @@ class LegacyUsers extends React.Component<RouteComponentProps, IProps> {
     super(props);
     this.state = {
       ...props,
+      params: {
+        page: 1,
+        page_size: 10,
+        order_by: 'name'
+      },
+      mounted: false,
+      count: 0,
       legacyusers: [],
     };
   }
 
   componentDidMount() {
+
+    const thisPath = window.location.pathname;
+    console.log("thisPath", thisPath);
+    const thisHref = window.location.href;
+    console.log("thisHref", thisHref);
+    const thisQS = window.location.search;
+    console.log('thisQS', thisQS);
+    const urlParams = new URLSearchParams(thisQS);
+    console.log('urlParams', urlParams);
+    let page = parseInt(urlParams.get('page')) || 1;
+    console.log('page', page);
+    let page_size = parseInt(urlParams.get('page_size')) || 10;
+    console.log('page_size', page_size);
+    let order_by = urlParams.get('order_by') || 'name';
+    console.log('order_by', order_by);
+
     LegacyUserAPI.get('users').then((response) => {
       console.log(response.data);
       //this.setState({legacyroles: response.data});
       this.setState((state, props) => ({
+        mounted: true,
+        params: {
+            page: page,
+            page_size: page_size,
+            order_by: order_by,
+        },
+        count: response.data.count,
         legacyusers: response.data.results,
       }));
     });
+  }
+
+  updateParams = (p) => {
+    console.log('updateParams', p);
+    const {
+        page,
+        page_size,
+        order_by
+    } = p;
+
+    const url = `users/?page=${page}&page_size=${page_size}&order_by=${order_by}`;
+    LegacyUserAPI.get(url).then((response) => {
+      console.log(response.data);
+      //this.setState({legacyroles: response.data});
+      this.setState((state, props) => ({
+        mounted: true,
+        params: {
+            page: page,
+            page_size: page_size,
+            order_by: order_by,
+        },
+        count: response.data.count,
+        legacyusers: response.data.results,
+      }));
+    });
+
   }
 
   render() {
@@ -64,12 +122,22 @@ class LegacyUsers extends React.Component<RouteComponentProps, IProps> {
       <div>
         <BaseHeader title={t`Legacy Authors`}></BaseHeader>
         <React.Fragment>
-          <DataList aria-label={t`List of Legacy Authors`}>
-            {this.state.legacyusers &&
-              this.state.legacyusers.map((luser) => (
-                <LegacyUserListItem key={luser.id} user={luser} />
-              ))}
-          </DataList>
+          { this.state.mounted && 
+              <DataList aria-label={t`List of Legacy Authors`}>
+                {this.state.legacyusers &&
+                  this.state.legacyusers.map((luser) => (
+                    <LegacyUserListItem key={luser.id} user={luser} />
+                  ))}
+              </DataList>
+          }
+          { this.state.mounted && 
+              <Pagination
+                params={this.state.params}
+                updateParams={this.updateParams}
+                count={this.state.count}
+              />
+          }
+
         </React.Fragment>
       </div>
     );
