8496c20ceac6c2f0e02f073ec6b2dcc9b48500b7
Martin Hradil <mhradil@redhat.com>
2022-06-25 12:51:42 +0000

    fix script filename
    + django-admin add-signing-service ansible-default /var/lib/pulp/scripts/collection_sign.sh 15250E9EC0A62577
    CommandError: [Errno 2] No such file or directory: '/var/lib/pulp/scripts/collection_sign.sh'

--- .github/workflows/scripts/signing-setup.sh
+++ .github/workflows/scripts/signing-setup.sh
@@ -16,5 +16,5 @@ echo "${KEY_FINGERPRINT}:6:" | gpg --import-ownertrust
 
 HAS_SIGNING=$(django-admin shell -c 'from pulpcore.app.models import SigningService;print(SigningService.objects.filter(name="ansible-default").count())' || true)
 if [[ "$HAS_SIGNING" -eq "0" ]]; then
-   django-admin add-signing-service ansible-default /var/lib/pulp/scripts/collection_sign.sh ${KEY_ID}
+   django-admin add-signing-service ansible-default /var/lib/pulp/scripts/collection-sign.sh ${KEY_ID}
 fi
