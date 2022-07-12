d2b62098fbc24bc27035e62833662f3694e56297
Martin Hradil <mhradil@redhat.com>
2022-07-12 13:08:12 +0000

    custom_release: stop pushing to ci-beta and ci-stable (now only qa- and prod-)

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -3,19 +3,17 @@ set -e
 set -x
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
-    for env in ci qa ; do
-        # always push to stage-beta
-        echo "PUSHING ${env}-beta"
-        rm -rf ./dist/.git
-        .travis/release.sh "${env}-beta"
+    # always push to stage-beta
+    echo "PUSHING qa-beta"
+    rm -rf ./dist/.git
+    .travis/release.sh "qa-beta"
 
-        # only push to stage-stable when enabled
-        [ -f .cloud-stage-cron.enabled ] || continue
-
-        echo "PUSHING ${env}-stable"
+    # only push to stage-stable when enabled
+    if [ -f .cloud-stage-cron.enabled ]; then
+        echo "PUSHING qa-stable"
         rm -rf ./dist/.git
-        .travis/release.sh "${env}-stable"
-    done
+        .travis/release.sh "qa-stable"
+    fi
 fi
 
 if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
