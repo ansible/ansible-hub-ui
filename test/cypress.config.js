708a57d08b308f2bd97fd4b157a64b9a0a08058d
Martin Hradil <mhradil@redhat.com>
2022-08-23 21:44:44 +0000

    Cypress: fix missing mocha-junit-reporter warning
    by removing the reporter config and letting cypress use the defaults
    
    Fixes..
    
        "mocha-junit-reporter" reporter not found
        Reporter not found! mocha-junit-reporter
    
    No-Issue

--- test/cypress.config.js
+++ test/cypress.config.js
@@ -3,10 +3,6 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   viewportWidth: 1200,
   viewportHeight: 800,
-  reporter: 'cypress-multi-reporters',
-  reporterOptions: {
-    configFile: 'reporter-config.json',
-  },
   e2e: {
     setupNodeEvents(on, _config) {
       return require('./cypress/plugins/console-logger').install(on);
