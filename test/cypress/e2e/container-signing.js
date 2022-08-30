0a8f46837468b84361d7c25256c21fbec72a593a
Milan Pospisil <arkanus@seznam.cz>
2022-08-30 15:59:03 +0200

    Remove EE list signing from test

--- test/cypress/e2e/container-signing.js
+++ test/cypress/e2e/container-signing.js
@@ -19,28 +19,8 @@ describe('Container Signing', () => {
       'https://registry.hub.docker.com/',
     );
 
-    /*cy.galaxykit(
-      'container create',
-      `remote1`,
-      'pulp/test-fixture-1',
-      `docker`,
-    );
-
-    cy.galaxykit(
-      'container create',
-      `remote2`,
-      'pulp/test-fixture-1',
-      `docker`,
-    );*/
-
     cy.addRemoteContainer({
       name: 'remote1',
-      upstream_name: 'pulp/test-fixture-1',
-      registry: 'docker',
-      exclude_tags: '*-source',
-    });
-    cy.addRemoteContainer({
-      name: 'remote2',
       upstream_name: 'pulp/test-fixture-1',
       registry: 'docker',
       exclude_tags: '*-source',
@@ -48,32 +28,19 @@ describe('Container Signing', () => {
 
     // prepare containers for signing - sync them
     cy.syncRemoteContainer('remote1');
-    cy.syncRemoteContainer('remote2');
-  });
-
-  it('can sign remote in list', () => {
-    cy.login();
-    cy.visit('/ui/containers');
-    cy.contains(getContainerRowQuery('remote1'), 'Unsigned', {
-      timeout: 10000,
-    });
-    getContainerMenu('remote1').click();
-    cy.contains('ul li a', 'Sign').click();
-    cy.contains('Signing started for container "remote1');
-    cy.contains(getContainerRowQuery('remote1'), 'Signed', { timeout: 30000 });
   });
 
   it('can sign remote in detail', () => {
     cy.login();
-    cy.visit('/ui/containers/remote2');
-    cy.contains('.column-section', 'remote2');
+    cy.visit('/ui/containers/remote1');
+    cy.contains('.column-section', 'remote1');
     cy.contains('.header-bottom', 'Unsigned', {
       timeout: 10000,
     });
 
     cy.get('button[aria-label="Actions"]').click();
     cy.contains('ul li a', 'Sign').click();
-    cy.contains('Signing started for container "remote2');
+    cy.contains('Signing started for container "remote1');
     cy.contains('.header-bottom', 'Signed', {
       timeout: 30000,
     });
