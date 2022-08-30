b875adacf464667e859518889d7743e2d48000f4
Milan Pospisil <arkanus@seznam.cz>
2022-08-30 16:54:02 +0200

    Test for remote

--- test/cypress/e2e/container-signing.js
+++ test/cypress/e2e/container-signing.js
@@ -1,17 +1,16 @@
 describe('Container Signing', () => {
-  function getContainerRowQuery(name) {
-    return `[data-cy="ExecutionEnvironmentList-row-${name}"]  `;
-  }
-
-  function getContainerMenu(name) {
-    return cy.get(getContainerRowQuery(name) + 'button[aria-label="Actions"]');
-  }
+  const user = 'EESignTestUser';
+  const password = 'MyPassword123';
 
   before(() => {
     cy.login();
 
     cy.deleteRegistries();
     cy.deleteContainers();
+    cy.deleteTestUsers();
+
+    // user without privilleges
+    cy.galaxykit('user create', user, password);
 
     cy.galaxykit(
       'registry create',
@@ -26,11 +25,23 @@ describe('Container Signing', () => {
       exclude_tags: '*-source',
     });
 
+    cy.addRemoteContainer({
+      name: 'remote2',
+      upstream_name: 'pulp/test-fixture-1',
+      registry: 'docker',
+      exclude_tags: '*-source',
+    });
+
+    cy.addLocalContainer('local1', 'pulp/test-fixture-1:manifest_a');
+
     // prepare containers for signing - sync them
     cy.syncRemoteContainer('remote1');
+    // remote 2 is not synced intentionaly - we need test that throws error
   });
 
-  it('can sign remote in detail', () => {
+  after(() => {});
+
+  it('can sign remote1', () => {
     cy.login();
     cy.visit('/ui/containers/remote1');
     cy.contains('.column-section', 'remote1');
@@ -45,4 +56,40 @@ describe('Container Signing', () => {
       timeout: 30000,
     });
   });
+
+  it('can not sign remote2', () => {
+    cy.login();
+    cy.visit('/ui/containers/remote2');
+    cy.contains('.column-section', 'remote2');
+    cy.contains('.header-bottom', 'Unsigned', {
+      timeout: 10000,
+    });
+
+    cy.get('button[aria-label="Actions"]').click();
+    cy.contains('ul li a', 'Sign').click();
+    cy.contains('Container must be synchronized with remote repository first.');
+  });
+
+  it('can sign local', () => {
+    cy.login();
+    cy.visit('/ui/containers/local1');
+    cy.contains('.column-section', 'local1');
+    cy.contains('.header-bottom', 'Unsigned', {
+      timeout: 10000,
+    });
+
+    cy.get('button[aria-label="Actions"]').click();
+    cy.contains('ul li a', 'Sign').click();
+    cy.contains('Signing started for container "local1');
+    cy.contains('.header-bottom', 'Signed', {
+      timeout: 30000,
+    });
+  });
+
+  /*it('cant see sign button when user has no rights', () => {
+    cy.login(user, password);
+    cy.visit('/ui/containers/local1');
+    cy.get('button[aria-label="Actions"]').click();
+
+  });*/
 });
