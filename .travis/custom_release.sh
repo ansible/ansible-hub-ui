2fe04ea08e89fb29504100b26a9bff9c637445e8
Martin Hradil <mhradil@redhat.com>
2022-09-01 16:45:25 +0200

    deploy-cloud, base config: add explicit deployment config to build stage-stable and stage-beta both from master (#2522)
    npm run deploy reads the current branch name (in cloud mode) and decides to build a beta version when the branch is master
    but since #2308, we're deploying to stage-stable as well
    leading to issues on stage-stable:
    
        Loading chunk 849 failed. (error: https://console.stage.redhat.com/beta/apps/automation-hub/js/849.1661953046888.a80f84b3e77abc4bde6d.js)
    
    adding a HUB_CLOUD_BETA env var to override the autodetection,
    and building with HUB_CLOUD_BETA=true for stage-beta, HUB_CLOUD_BETA=false for stage-stable,
    and no override for prod builds, as before.
    
    No-Issue

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -4,12 +4,14 @@ set -x
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
     # always push to stage-beta
+    HUB_CLOUD_BETA="true" npm run deploy
     echo "PUSHING qa-beta"
     rm -rf ./dist/.git
     .travis/release.sh "qa-beta"
 
     # only push to stage-stable when enabled
     if [ -f .cloud-stage-cron.enabled ]; then
+        HUB_CLOUD_BETA="false" npm run deploy
         echo "PUSHING qa-stable"
         rm -rf ./dist/.git
         .travis/release.sh "qa-stable"
@@ -17,6 +19,7 @@ if [ "${TRAVIS_BRANCH}" = "master" ]; then
 fi
 
 if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
+    npm run deploy
     echo "PUSHING ${TRAVIS_BRANCH}"
     rm -rf ./build/.git
     .travis/release.sh "${TRAVIS_BRANCH}"
