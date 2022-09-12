5cc120b0af08585373495a024c24fbd25a5fa113
Martin Hradil <mhradil@redhat.com>
2022-09-12 23:15:51 +0000

    syntax fixups, missing plugin support still

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,38 @@
-export default [
+const globals = require('globals');
+
+module.exports = [
   // .eslintrc extends
   'eslint:recommended',
-  require('eslint-plugin-react'),
-  require('@typescript-eslint/eslint-plugin/dist/configs/eslint-recommended'),
+  // require('eslint-plugin-react'), // TODO Unexpected key "deprecatedRules" found.
+  // require('@typescript-eslint/eslint-plugin/dist/configs/eslint-recommended'), // TODO Unexpected key "overrides" found.
   require('eslint-config-prettier'),
 
   // .eslintrc
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
-    env: {
-      "browser": true,
-      "node": true,
-      "es2021": true,
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        // more overrides below for tests
+        APPLICATION_NAME: "readonly",
+        NAMESPACE_TERM: "readonly",
+      },
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        "ecmaFeatures": {
+          "jsx": true,
+        },
+      },
     },
-    parser: "@typescript-eslint/parser",
     settings: {
       "react": {
         "version": "detect",
       },
     },
-    parserOptions: {
-      "ecmaFeatures": {
-        "jsx": true,
-      },
-    },
-    globals: {
-      // more overrides below for tests
-      APPLICATION_NAME: "readonly",
-      NAMESPACE_TERM: "readonly",
-    },
     rules: {
       "curly": ["error", "all"],
       "eol-last": ["error", "always"],
-      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+//TODO      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
 
       /// FIXME: Rules to add from previous config (see #860)
       // array-bracket-spacing camelcase comma-dangle comma-spacing comma-style curly dot-notation eol-last eqeqeq func-names indent key-spacing keyword-spacing linebreak-style max-len new-cap no-bitwise no-caller no-mixed-spaces-and-tabs no-multiple-empty-lines no-trailing-spaces no-undef no-unused-vars no-use-before-define no-var no-with object-curly-spacing object-shorthand one-var padding-line-between-statements quote-props quotes react/jsx-curly-spacing semi space-before-blocks space-in-parens space-infix-ops space-unary-ops vars-on-top wrap-iife yoda
@@ -42,6 +42,11 @@ export default [
   // config/.eslintrc
   {
     files: ['config/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
     rules: {
       "@typescript-eslint/no-var-requires": "off",
     },
@@ -50,6 +55,7 @@ export default [
   // test/.eslintrc
   {
     files: ['test/**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
     globals: {
       "Cypress": "readonly",
       "after": "readonly",
@@ -59,9 +65,9 @@ export default [
       "describe": "readonly",
       "expect": "readonly",
       "it": "readonly",
-    },
+    },},
     rules: {
-      "@typescript-eslint/no-var-requires": "off",
+//TODO      "@typescript-eslint/no-var-requires": "off",
     },
   },
 ];
