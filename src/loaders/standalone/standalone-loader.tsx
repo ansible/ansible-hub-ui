46d451900e710eb745e0623c81072ba6daa31658
Martin Hradil <mhradil@redhat.com>
2022-09-18 00:18:01 +0000

    standalone: only show Signature Keys menu item when display_signatures

--- src/loaders/standalone/standalone-loader.tsx
+++ src/loaders/standalone/standalone-loader.tsx
@@ -387,6 +387,7 @@ class App extends React.Component<RouteComponentProps, IState> {
       }),
       menuItem(t`Signature Keys`, {
         url: Paths.signatureKeys,
+        condition: ({ featureFlags }) => featureFlags.display_signatures,
       }),
       menuItem(t`Documentation`, {
         url: 'https://access.redhat.com/documentation/en-us/red_hat_ansible_automation_platform/',
