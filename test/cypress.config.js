6723a960fc9544ad482dbb6001bd2cccd0858801
MilanPospisil <arkanus@seznam.cz>
2022-10-18 09:59:59 +0200

    Rerun the failed cypress tests (#2683)
    * Rerun the failed cypress tests
    Issue: AAH-1964
    
    * WIP
    
    * Rerun failed cypress tests

--- test/cypress.config.js
+++ test/cypress.config.js
@@ -10,4 +10,12 @@ module.exports = defineConfig({
     baseUrl: 'http://localhost:8002',
     specPattern: 'cypress/e2e/**/*.js',
   },
+  retries: {
+    // Configure retry attempts for `cypress run`
+    // Default is 0
+    runMode: 2,
+    // Configure retry attempts for `cypress open`
+    // Default is 0
+    openMode: 0,
+  },
 });
