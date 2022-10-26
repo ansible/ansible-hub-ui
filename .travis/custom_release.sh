e99e36617dd2baf9c730e5b4f89f3d39b0509426
Martin Hradil <mhradil@redhat.com>
2022-10-26 12:45:15 +0000

    custom_release: build prod-beta with HUB_CLOUD_BETA=true
    No-Issue

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -2,25 +2,30 @@
 set -e
 set -x
 
+push () {
+    echo "PUSHING $1"
+    rm -rf dist/.git build/.git
+    .travis/release.sh "$1"
+}
+
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
     # always push to stage-beta
     HUB_CLOUD_BETA="true" npm run deploy
-    echo "PUSHING qa-beta"
-    rm -rf ./dist/.git
-    .travis/release.sh "qa-beta"
+    push "qa-beta"
 
     # only push to stage-stable when enabled
     if [ -f .cloud-stage-cron.enabled ]; then
         HUB_CLOUD_BETA="false" npm run deploy
-        echo "PUSHING qa-stable"
-        rm -rf ./dist/.git
-        .travis/release.sh "qa-stable"
+        push "qa-stable"
     fi
 fi
 
-if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
-    npm run deploy
-    echo "PUSHING ${TRAVIS_BRANCH}"
-    rm -rf ./build/.git
-    .travis/release.sh "${TRAVIS_BRANCH}"
+if [ "${TRAVIS_BRANCH}" = "prod-beta" ]; then
+    HUB_CLOUD_BETA="true" npm run deploy
+    push "prod-beta"
+fi
+
+if [ "${TRAVIS_BRANCH}" = "prod-stable" ]; then
+    HUB_CLOUD_BETA="false" npm run deploy
+    push "prod-stable"
 fi
