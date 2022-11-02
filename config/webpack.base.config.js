d83f85ecc7982d45d0ebe6fddfe3e2db8fd7ade7
Martin Hradil <mhradil@redhat.com>
2022-11-02 15:44:39 +0000

    webpack config - useFileHash: true

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -102,6 +102,11 @@ module.exports = (inputConfigs) => {
     // frontend-components-config 4.6.9+: keep HtmlWebpackPlugin for standalone
     useChromeTemplate: !isStandalone,
 
+    // frontend-components-config 4.6.25-29+: ensure hashed filenames
+    useFileHash: true,
+    liveReload: false,
+    _unstableHotReload: false,
+
     // insights dev
     ...(!isStandalone &&
       !isBuild && {
