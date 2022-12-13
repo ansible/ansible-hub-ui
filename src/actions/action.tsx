3bc92721626a67e98aa9287c1a7674abb5be3add
Martin Hradil <mhradil@redhat.com>
2022-12-13 01:28:44 +0000

    action: support .button too

--- src/actions/action.tsx
+++ src/actions/action.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
-import { DropdownItem } from '@patternfly/react-core';
+import { Button, DropdownItem } from '@patternfly/react-core';
 
 export const Action = ({ title, onClick }) => ({
   dropdownItem: (item) => (
     <DropdownItem key={title} onClick={() => onClick(item)}>
       {title}
     </DropdownItem>
+  ),
+  button: (item) => (
+    <Button key={title} onClick={() => onClick(item)}>
+      {title}
+    </Button>
   ),
 });
