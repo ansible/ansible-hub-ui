153c955420e1efe46e337f9b6c3658e86a808e7f
James Tanner <tanner.jc@gmail.com>
2022-12-13 18:56:02 -0500

    Use page_size for v1 instead of limit.
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- src/api/legacy.ts
+++ src/api/legacy.ts
@@ -1,5 +1,6 @@
 import { BaseAPI } from './base';
 import { LegacyRoleDetailType } from 'src/api';
+import { Constants } from 'src/constants';
 
 export class LegacyAPI extends BaseAPI {
   API_VERSION = 'v1';
@@ -8,6 +9,24 @@ export class LegacyAPI extends BaseAPI {
 
   constructor() {
     super(API_HOST + API_BASE_PATH);
+  }
+
+  public mapPageToOffset(p) {
+    // Need to copy the object to make sure we aren't accidentally
+    // setting page state
+    const params = { ...p };
+
+    const pageSize =
+      parseInt(params['page_size']) || Constants.DEFAULT_PAGE_SIZE;
+    const page = parseInt(params['page']) || 1;
+
+    delete params['page'];
+    delete params['page_size'];
+
+    params['offset'] = page * pageSize - pageSize;
+    params['page_size'] = pageSize;
+
+    return params;
   }
 
   get(apiPath: string) {
