c289ff2c14ad9b1c67f2adf4df428df6b2052714
Martin Hradil <mhradil@redhat.com>
2022-12-13 01:29:09 +0000

    CanContext: add hasPermission

--- src/permissions.ts
+++ src/permissions.ts
@@ -4,6 +4,7 @@ export type CanContext = (o: {
   featureFlags: FeatureFlagsType;
   settings: SettingsType;
   user: UserType;
+  hasPermission: (string) => boolean;
 }) => boolean;
 
 export const isLoggedIn: CanContext = ({ user }) => user && !user.is_anonymous;
