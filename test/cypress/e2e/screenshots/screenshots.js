935c27cc9caee87a74f0427e0f22e2b77c3a6faf
MilanPospisil <arkanus@seznam.cz>
2023-01-02 03:48:29 +0100

    Screenshots tests (#2742)
    * cypress: run against oci-env
    
    No-Issue
    
    drop cy.settings, use testdir/compose.env fragment
    
    debug insights galaxykit, reduce retries to 1
    
    update compose.env to match default settings
    
    require approval false for rest
    sign_after_approval tests were skipped and not working, removing
    approval_required - merge back into approval, both use same config
    remove compose.env entries that happen to be defaults, move sign_on_approval back to misc as same settings
    
    signing tests need require approval true
    
    insights galaxykit - more sleep
    
    to fix ocasional Failed to parse JSON response from API from galaxykit
    
    * Add screenshoots test
    
    Prettier
    
    comparing files
    
    prepare for github
    
    Create separate test matrix for screenshot
    
    delete screenshoots from branch
    
    Prepare test PR
    
    WIP
    
    WIP
    
    Compare one file
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    simplify, cleanup, allow other SHORT_BRANCHes, keep diffs
    
    No-Issue
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    * Corrent mistake in rebase
    
    WIP
    
    WIP
    
    y# This is a combination of 3 commits.
    
    PR review
    
    WIP
    
    WIP
    
    * Clear echoes, remove CAN_FAIL and force to fail on every change
    
    Change back to PR mode
    
    * Comment roles tests - need fake API
    
    * Delete unused UPLOAD_DIFF variable
    
    Co-authored-by: Martin Hradil <mhradil@redhat.com>

---
+++ test/cypress/e2e/screenshots/screenshots.js
@@ -0,0 +1,46 @@
+const { range } = require('lodash');
+
+describe('screenshots', () => {
+  before(() => {
+    cy.deleteNamespacesAndCollections();
+
+    // insert test data
+    cy.galaxykit('namespace create my_namespace');
+    range(5).forEach((i) => {
+      cy.galaxykit('-i collection upload my_namespace my_collection' + i);
+    });
+  });
+
+  beforeEach(() => {
+    cy.login();
+  });
+
+  after(() => {
+    cy.deleteNamespacesAndCollections();
+  });
+
+  it('takes screenshots', () => {
+    const screenshot = (path) => {
+      cy.visit(path);
+      cy.wait(3000);
+      cy.screenshot(path.replaceAll('/', '__'));
+      cy.wait(1000);
+    };
+
+    screenshot('/');
+    screenshot('/repo/published');
+    screenshot('/namespaces');
+    // TODO - problems - repositories are showing minutes, so text may quickly change and generate diff
+    screenshot('/repositories');
+    screenshot('/token');
+    screenshot('/approval-dashboard');
+    screenshot('/containers');
+    screenshot('/registries');
+    // screenshot('/tasks'); // TODO fake empty API response
+    // screenshot('/signature-keys'); // TODO fake empty API response
+    screenshot('/users');
+    screenshot('/group-list');
+
+    // screenshot('/roles');  // TODO fake empty API response
+  });
+});
