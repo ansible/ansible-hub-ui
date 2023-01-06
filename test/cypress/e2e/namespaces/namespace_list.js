00c434c08a5802745d8711d8e8d264c860b776d1
Milan Pospisil <arkanus@seznam.cz>
2023-01-06 14:47:19 +0100

    namespace_list test

--- test/cypress/e2e/namespaces/namespace_list.js
+++ test/cypress/e2e/namespaces/namespace_list.js
@@ -1,44 +1,42 @@
+const insightsLogin = Cypress.env('insightsLogin');
+
 describe('Namespaces Page Tests', () => {
   before(() => {
-    cy.deleteTestUsers();
-    cy.deleteTestGroups();
     cy.deleteNamespacesAndCollections();
-
-    cy.galaxykit('-i group create', 'testGroup1');
-    cy.galaxykit('-i group create', 'testGroup2');
-
-    cy.galaxykit('-i user create', 'testUser2', 'p@ssword1');
-    cy.galaxykit('user group add', 'testUser2', 'testGroup2');
-
     cy.galaxykit('-i namespace create', 'testns1');
     cy.galaxykit('-i namespace create', 'testns2');
-
-    //FIXME roles
-    //cy.galaxykit('namespace addgroup', 'testns1', 'testGroup1');
-    //cy.galaxykit('namespace addgroup', 'testns2', 'testGroup2');
   });
 
   after(() => {
-    cy.deleteTestUsers();
-    cy.deleteTestGroups();
     cy.deleteNamespacesAndCollections();
   });
 
-  it('can navigate to public namespace list', () => {
-    cy.login('testUser2', 'p@ssword1');
-    cy.menuGo('Collections > Namespaces');
-
+  it('can navigate to admin public namespace list', () => {
+    cy.login();
+    cy.goToNamespaces();
     cy.contains('testns2').should('exist');
     cy.contains('testns1').should('exist');
   });
 
-  it.skip('can navigate to personal namespace list', () => {
-    cy.login('testUser2', 'p@ssword1');
-    cy.menuGo('Collections > Namespaces');
+  if (!insightsLogin) {
+    it('can navigate to user public namespace list', () => {
+      cy.deleteTestUsers();
+      cy.deleteTestGroups();
 
-    cy.contains('My namespaces').click();
+      cy.galaxykit('-i group create', 'testGroup1');
+      cy.galaxykit('-i group create', 'testGroup2');
 
-    cy.contains('testns2').should('exist');
-    cy.contains('testns1').should('not.exist');
-  });
+      cy.galaxykit('-i user create', 'testUser2', 'p@ssword1');
+      cy.galaxykit('user group add', 'testUser2', 'testGroup2');
+
+      cy.login('testUser2', 'p@ssword1');
+      cy.menuGo('Collections > Namespaces');
+
+      cy.contains('testns2').should('exist');
+      cy.contains('testns1').should('exist');
+
+      cy.deleteTestUsers();
+      cy.deleteTestGroups();
+    });
+  }
 });
