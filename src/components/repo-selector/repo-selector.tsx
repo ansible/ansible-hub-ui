e3ef54146012f1177525cb4859461b36d29785df
Martin Hradil <mhradil@redhat.com>
2023-01-07 01:28:10 +0000

    RepoSelector: convert to functional

--- src/components/repo-selector/repo-selector.tsx
+++ src/components/repo-selector/repo-selector.tsx
@@ -1,6 +1,6 @@
 import { t } from '@lingui/macro';
 import { i18n } from '@lingui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import { redirect } from 'react-router-dom';
 import {
   Flex,
@@ -24,72 +24,65 @@ interface IProps {
   isDisabled?: boolean;
 }
 
-interface IState {
-  selectExpanded: boolean;
-}
+export const RepoSelector = ({
+  selectedRepo,
+  path,
+  pathParams,
+  isDisabled,
+}: IProps) => {
+  const [selectExpanded, setSelectExpanded] = useState<boolean>(false);
 
-export class RepoSelector extends React.Component<IProps, IState> {
-  constructor(props) {
-    super(props);
-
-    this.state = { selectExpanded: false };
-  }
-
-  render() {
-    const repoNames = Constants.REPOSITORYNAMES;
-
-    return (
-      <Flex>
-        <FlexItem>
-          <InputGroup>
-            <InputGroupText
-              style={{ paddingLeft: 0 }}
-              variant='plain'
-              className='hub-input-group-text-no-wrap'
-            >
-              {t`Filter by repository`}
-            </InputGroupText>
-            <Select
-              className='nav-select'
-              isDisabled={this.props.isDisabled}
-              isOpen={this.state.selectExpanded}
-              isPlain={false}
-              onSelect={(event: React.ChangeEvent<HTMLInputElement>) => {
-                const originalRepo = this.props.selectedRepo;
-                const newRepo = this.getRepoName(event.target.name);
-
-                this.setState({ selectExpanded: false });
-
-                if (newRepo !== originalRepo) {
-                  const path = formatPath(this.props.path, {
-                    ...this.props.pathParams,
-                    repo: event.target.name,
-                  });
-                  redirect(path);
-                }
-              }}
-              onToggle={(isExpanded) => {
-                this.setState({ selectExpanded: isExpanded });
-              }}
-              selections={this.getRepoName(this.props.selectedRepo)}
-              variant='single'
-            >
-              {Object.keys(repoNames).map((option) => (
-                <SelectOption
-                  name={option}
-                  key={option}
-                  value={i18n._(repoNames[option])}
-                />
-              ))}
-            </Select>
-          </InputGroup>
-        </FlexItem>
-      </Flex>
-    );
-  }
-
-  private getRepoName(repoName) {
+  const getRepoName = (repoName) => {
     const repo = Constants.REPOSITORYNAMES[repoName];
     return repo ? i18n._(repo) : repoName;
-  }
-}
+  };
+
+  const repoNames = Constants.REPOSITORYNAMES;
+
+  return (
+    <Flex>
+      <FlexItem>
+        <InputGroup>
+          <InputGroupText
+            style={{ paddingLeft: 0 }}
+            variant='plain'
+            className='hub-input-group-text-no-wrap'
+          >
+            {t`Filter by repository`}
+          </InputGroupText>
+          <Select
+            className='nav-select'
+            isDisabled={isDisabled}
+            isOpen={selectExpanded}
+            isPlain={false}
+            onSelect={(event: React.ChangeEvent<HTMLInputElement>) => {
+              const originalRepo = selectedRepo;
+              const newRepo = getRepoName(event.target.name);
+
+              setSelectExpanded(false);
+
+              if (newRepo !== originalRepo) {
+                const newPath = formatPath(path, {
+                  ...pathParams,
+                  repo: event.target.name,
+                });
+                redirect(newPath);
+              }
+            }}
+            onToggle={(isExpanded) => setSelectExpanded(isExpanded)}
+            selections={getRepoName(selectedRepo)}
+            variant='single'
+          >
+            {Object.keys(repoNames).map((option) => (
+              <SelectOption
+                name={option}
+                key={option}
+                value={i18n._(repoNames[option])}
+              />
+            ))}
+          </Select>
+        </InputGroup>
+      </FlexItem>
+    </Flex>
+  );
+};
