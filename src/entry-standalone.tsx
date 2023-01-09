5b3cca6efbeb6c567394c96c5de122b37055ab6f
Martin Hradil <mhradil@redhat.com>
2023-01-09 09:56:18 +0000

    redirect to base path when outside - react-router no longer does it by default

--- src/entry-standalone.tsx
+++ src/entry-standalone.tsx
@@ -8,6 +8,11 @@ import 'src/l10n';
 
 // Entrypoint for compiling the app to run in standalone mode
 
+if (!window.location.pathname.includes(UI_BASE_PATH)) {
+  // react-router v6 won't redirect to base path by default
+  window.history.pushState(null, null, UI_BASE_PATH);
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <Router basename={UI_BASE_PATH}>
