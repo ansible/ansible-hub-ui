2d0258e7dffd6fd7f9016473a21e2976ad44243d
Milan Pospisil <arkanus@seznam.cz>
2023-01-19 11:29:19 +0100

    Refactor collection-card

--- src/components/cards/collection-card.tsx
+++ src/components/cards/collection-card.tsx
@@ -27,107 +27,106 @@ interface IProps extends CollectionListType {
   menu?: React.ReactNode;
 }
 
-export class CollectionCard extends React.Component<IProps> {
-  MAX_DESCRIPTION_LENGTH = 60;
+export const CollectionCard = ({
+  name,
+  latest_version,
+  namespace,
+  className,
+  displaySignatures,
+  footer,
+  repo,
+  sign_state,
+  menu,
+}: IProps) => {
+  const MAX_DESCRIPTION_LENGTH = 60;
 
-  render() {
-    const {
-      name,
-      latest_version,
-      namespace,
-      className,
-      displaySignatures,
-      footer,
-      repo,
-      sign_state,
-      menu,
-    } = this.props;
+  const company = namespace.company || namespace.name;
+  const contentSummary = convertContentSummaryCounts(latest_version.metadata);
 
-    const company = namespace.company || namespace.name;
-    const contentSummary = convertContentSummaryCounts(latest_version.metadata);
+  return (
+    <Card className={cx('hub-c-card-collection-container ', className)}>
+      <CardHeader className='logo-row'>
+        <Logo
+          alt={t`${company} logo`}
+          fallbackToDefault
+          image={namespace.avatar_url}
+          size='40px'
+          unlockWidth
+          flexGrow
+        />
+        <TextContent>{getCertification(repo)}</TextContent>
+        {displaySignatures ? (
+          <SignatureBadge isCompact signState={sign_state} />
+        ) : null}
+        {menu}
+      </CardHeader>
+      <CardHeader>
+        <div className='name'>
+          <Link
+            to={formatPath(Paths.collectionByRepo, {
+              collection: name,
+              namespace: namespace.name,
+              repo: repo,
+            })}
+          >
+            {name}
+          </Link>
+        </div>
+        <div className='author'>
+          <TextContent>
+            <Text component={TextVariants.small}>
+              <Trans>Provided by {company}</Trans>
+            </Text>
+          </TextContent>
+        </div>
+      </CardHeader>
+      <CardBody>
+        <Tooltip content={<div>{latest_version.metadata.description}</div>}>
+          <div className='description'>
+            {getDescription(
+              latest_version.metadata.description,
+              MAX_DESCRIPTION_LENGTH,
+            )}
+          </div>
+        </Tooltip>
+      </CardBody>
+      <CardBody className='type-container'>
+        {Object.keys(contentSummary.contents).map((k) =>
+          renderTypeCount(k, contentSummary.contents[k]),
+        )}
+      </CardBody>
+      {footer && <CardFooter>{footer}</CardFooter>}
+    </Card>
+  );
+};
 
+function getCertification(repo) {
+  if (repo === Constants.CERTIFIED_REPO) {
     return (
-      <Card className={cx('hub-c-card-collection-container ', className)}>
-        <CardHeader className='logo-row'>
-          <Logo
-            alt={t`${company} logo`}
-            fallbackToDefault
-            image={namespace.avatar_url}
-            size='40px'
-            unlockWidth
-            flexGrow
-          />
-          <TextContent>{this.getCertification(repo)}</TextContent>
-          {displaySignatures ? (
-            <SignatureBadge isCompact signState={sign_state} />
-          ) : null}
-          {menu}
-        </CardHeader>
-        <CardHeader>
-          <div className='name'>
-            <Link
-              to={formatPath(Paths.collectionByRepo, {
-                collection: name,
-                namespace: namespace.name,
-                repo: repo,
-              })}
-            >
-              {name}
-            </Link>
-          </div>
-          <div className='author'>
-            <TextContent>
-              <Text component={TextVariants.small}>
-                <Trans>Provided by {company}</Trans>
-              </Text>
-            </TextContent>
-          </div>
-        </CardHeader>
-        <CardBody>
-          <Tooltip content={<div>{latest_version.metadata.description}</div>}>
-            <div className='description'>
-              {this.getDescription(latest_version.metadata.description)}
-            </div>
-          </Tooltip>
-        </CardBody>
-        <CardBody className='type-container'>
-          {Object.keys(contentSummary.contents).map((k) =>
-            this.renderTypeCount(k, contentSummary.contents[k]),
-          )}
-        </CardBody>
-        {footer && <CardFooter>{footer}</CardFooter>}
-      </Card>
+      <Text component={TextVariants.small}>
+        <Badge isRead>{t`Certified`}</Badge>
+      </Text>
     );
   }
 
-  private getCertification(repo) {
-    if (repo === Constants.CERTIFIED_REPO) {
-      return (
-        <Text component={TextVariants.small}>
-          <Badge isRead>{t`Certified`}</Badge>
-        </Text>
-      );
-    }
+  return null;
+}
 
-    return null;
+function getDescription(d: string, MAX_DESCRIPTION_LENGTH) {
+  if (!d) {
+    return '';
   }
+  if (d.length > MAX_DESCRIPTION_LENGTH) {
+    return d.slice(0, MAX_DESCRIPTION_LENGTH) + '...';
+  } else {
+    return d;
+  }
+}
 
-  private getDescription(d: string) {
-    if (!d) {
-      return '';
-    }
-    if (d.length > this.MAX_DESCRIPTION_LENGTH) {
-      return d.slice(0, this.MAX_DESCRIPTION_LENGTH) + '...';
-    } else {
-      return d;
-    }
-  }
-
-  private renderTypeCount(type, count) {
-    return (
-      <div key={type}>
-        <CollectionNumericLabel count={count} newline type={type} />
-      </div>
-    );
-  }
+function renderTypeCount(type, count) {
+  return (
+    <div key={type}>
+      <CollectionNumericLabel count={count} newline type={type} />
+    </div>
+  );
 }
