1077fed462015283eb333c7a7c8de874643af575
Milan Pospisil <arkanus@seznam.cz>
2023-01-19 11:41:30 +0100

    Refactor namespace-card

--- src/components/cards/namespace-card.tsx
+++ src/components/cards/namespace-card.tsx
@@ -22,47 +22,51 @@ interface IProps {
   namespaceURL?: string;
 }
 
-export class NamespaceCard extends React.Component<IProps> {
-  MAX_DESCRIPTION_LENGTH = 26;
-  render() {
-    const { avatar_url, name, company, namespaceURL } = this.props;
-    return (
-      <Card className='hub-c-card-ns-container'>
-        <CardHeader>
-          <CardHeaderMain>
-            <Logo
-              alt={t`${company} logo`}
-              fallbackToDefault
-              image={avatar_url}
-              size='40px'
-              unlockWidth
-            />
-          </CardHeaderMain>
-        </CardHeader>
-        <Tooltip content={company || name}>
-          <CardTitle>{this.getDescription(company || name)}</CardTitle>
-        </Tooltip>
-        <Tooltip content={name}>
-          <CardBody>{this.getDescription(name)}</CardBody>
-        </Tooltip>
+export const NamespaceCard = ({
+  avatar_url,
+  name,
+  company,
+  namespaceURL,
+}: IProps) => {
+  const MAX_DESCRIPTION_LENGTH = 26;
+  return (
+    <Card className='hub-c-card-ns-container'>
+      <CardHeader>
+        <CardHeaderMain>
+          <Logo
+            alt={t`${company} logo`}
+            fallbackToDefault
+            image={avatar_url}
+            size='40px'
+            unlockWidth
+          />
+        </CardHeaderMain>
+      </CardHeader>
+      <Tooltip content={company || name}>
+        <CardTitle>
+          {getDescription(company || name, MAX_DESCRIPTION_LENGTH)}
+        </CardTitle>
+      </Tooltip>
+      <Tooltip content={name}>
+        <CardBody>{getDescription(name, MAX_DESCRIPTION_LENGTH)}</CardBody>
+      </Tooltip>
 
-        {namespaceURL && (
-          <CardFooter>
-            <Link to={namespaceURL}>{t`View collections`}</Link>
-          </CardFooter>
-        )}
-      </Card>
-    );
+      {namespaceURL && (
+        <CardFooter>
+          <Link to={namespaceURL}>{t`View collections`}</Link>
+        </CardFooter>
+      )}
+    </Card>
+  );
+};
+
+function getDescription(d: string, MAX_DESCRIPTION_LENGTH) {
+  if (!d) {
+    return '';
   }
-
-  private getDescription(d: string) {
-    if (!d) {
-      return '';
-    }
-    if (d.length > this.MAX_DESCRIPTION_LENGTH) {
-      return d.slice(0, this.MAX_DESCRIPTION_LENGTH) + '...';
-    } else {
-      return d;
-    }
+  if (d.length > MAX_DESCRIPTION_LENGTH) {
+    return d.slice(0, MAX_DESCRIPTION_LENGTH) + '...';
+  } else {
+    return d;
   }
 }
