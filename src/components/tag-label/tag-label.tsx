4c5dcbbb88d2bd8fbf879e083bb4db1ce1062f7a
Martin Hradil <mhradil@redhat.com>
2023-01-24 01:36:35 +0000

    TagLabel({ tag })

--- src/components/tag-label/tag-label.tsx
+++ src/components/tag-label/tag-label.tsx
@@ -1,18 +1,13 @@
 import { Label } from '@patternfly/react-core';
 import { TagIcon } from '@patternfly/react-icons';
-import * as React from 'react';
+import React from 'react';
 
 interface IProps {
   tag: string;
 }
 
-export class TagLabel extends React.Component<IProps> {
-  render() {
-    const { tag } = this.props;
-    return (
-      <Label variant='outline' icon={<TagIcon />}>
-        {tag}
-      </Label>
-    );
-  }
-}
+export const TagLabel = ({ tag }: IProps) => (
+  <Label variant='outline' icon={<TagIcon />}>
+    {tag}
+  </Label>
+);
