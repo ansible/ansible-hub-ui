f1f8864865a7c3d74ecded369b22b4a878286a7b
Martin Hradil <mhradil@redhat.com>
2023-01-24 01:34:48 +0000

    LoginLink({ button, next })

--- src/components/shared/login-link.tsx
+++ src/components/shared/login-link.tsx
@@ -1,7 +1,7 @@
 import { t } from '@lingui/macro';
-import * as React from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { AppContext } from 'src/loaders/app-context';
+import { useContext } from 'src/loaders/app-context';
 import { Paths, formatPath } from 'src/paths';
 
 interface IProps {
@@ -9,26 +9,19 @@ interface IProps {
   next?: string;
 }
 
-export class LoginLink extends React.Component<IProps> {
-  static contextType = AppContext;
+export const LoginLink = ({ button, next }: IProps) => {
+  const { featureFlags } = useContext();
+  const className = button ? 'pf-c-button pf-m-primary' : '';
 
-  render() {
-    const { button, next } = this.props;
-    const { featureFlags } = this.context;
-    const className = button ? 'pf-c-button pf-m-primary' : '';
-
-    // NOTE: also update AuthHandler#render (src/loaders/standalone/routes.tsx) when changing this
-    if (featureFlags?.external_authentication && UI_EXTERNAL_LOGIN_URI) {
-      return (
-        <a className={className} href={UI_EXTERNAL_LOGIN_URI}>{t`Login`}</a>
-      );
-    } else {
-      return (
-        <Link
-          className={className}
-          to={formatPath(Paths.login, {}, { next })}
-        >{t`Login`}</Link>
-      );
-    }
+  // NOTE: also update AuthHandler#render (src/loaders/standalone/routes.tsx) when changing this
+  if (featureFlags?.external_authentication && UI_EXTERNAL_LOGIN_URI) {
+    return <a className={className} href={UI_EXTERNAL_LOGIN_URI}>{t`Login`}</a>;
+  } else {
+    return (
+      <Link
+        className={className}
+        to={formatPath(Paths.login, {}, { next })}
+      >{t`Login`}</Link>
+    );
   }
-}
+};
