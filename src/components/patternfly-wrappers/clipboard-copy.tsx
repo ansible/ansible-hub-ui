82d2b6690ec6f69fd4d7ad4948b4ab07f46f6527
Martin Hradil <mhradil@redhat.com>
2023-01-24 01:11:59 +0000

    ClipboardCopy({ children, ...props })

--- src/components/patternfly-wrappers/clipboard-copy.tsx
+++ src/components/patternfly-wrappers/clipboard-copy.tsx
@@ -1,23 +1,18 @@
 import { t } from '@lingui/macro';
 import { ClipboardCopy as PFClipboardCopy } from '@patternfly/react-core';
-import * as React from 'react';
+import React from 'react';
 
 interface IProps {
   children: string;
   [key: string]: string | number | boolean;
 }
 
-export class ClipboardCopy extends React.Component<IProps> {
-  render() {
-    const { children, ...props } = this.props;
-    return (
-      <PFClipboardCopy
-        hoverTip={t`Copy to clipboard`}
-        clickTip={t`Successfully copied to clipboard!`}
-        {...props}
-      >
-        {children}
-      </PFClipboardCopy>
-    );
-  }
-}
+export const ClipboardCopy = ({ children, ...props }: IProps) => (
+  <PFClipboardCopy
+    hoverTip={t`Copy to clipboard`}
+    clickTip={t`Successfully copied to clipboard!`}
+    {...props}
+  >
+    {children}
+  </PFClipboardCopy>
+);
