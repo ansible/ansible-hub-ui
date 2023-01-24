b8135cf43a3959ad3847eecfad98ba351738fb2d
Martin Hradil <mhradil@redhat.com>
2023-01-24 01:30:35 +0000

    Tabs({ tabs, params, updateParams, isDisabled, disabledTitle })

--- src/components/patternfly-wrappers/tabs.tsx
+++ src/components/patternfly-wrappers/tabs.tsx
@@ -1,5 +1,5 @@
 import { Tabs as PFTabs, Tab, TabTitleText } from '@patternfly/react-core';
-import * as React from 'react';
+import React from 'react';
 import { ParamHelper } from 'src/utilities/param-helper';
 
 export class TabsType {
@@ -22,51 +22,50 @@ interface IProps {
 }
 
 // FIXME: use LinkTabs, switch from ?tab to routes, rename to Tabs
-export class Tabs extends React.Component<IProps> {
-  render() {
-    const { tabs, params, updateParams, isDisabled, disabledTitle } =
-      this.props;
-    return (
-      <PFTabs
-        activeKey={this.getActiveTab()}
-        onSelect={(_, key) =>
-          !isDisabled &&
-          updateParams(
-            ParamHelper.setParam(params, 'tab', tabs[key].id.toLowerCase()),
-          )
-        }
-      >
-        {tabs.map((tab, i) => (
-          <Tab
-            key={i}
-            eventKey={i}
-            title={
-              <TabTitleText title={isDisabled ? disabledTitle : null}>
-                {tab.name}
-              </TabTitleText>
-            }
-            className={isDisabled ? 'disabled' : null}
-          />
-        ))}
-      </PFTabs>
-    );
-  }
-
-  private getActiveTab() {
-    const { params, tabs } = this.props;
-    if (params.tab) {
-      const i = tabs.findIndex(
-        (x) => x.id.toLowerCase() === params.tab.toLowerCase(),
-      );
-
-      // If tab is not found, default to the first tab.
-      if (i === -1) {
-        return 0;
-      } else {
-        return i;
-      }
-    } else {
-      return 0;
+export const Tabs = ({
+  tabs,
+  params,
+  updateParams,
+  isDisabled,
+  disabledTitle,
+}: IProps) => (
+  <PFTabs
+    activeKey={getActiveTab({ params, tabs })}
+    onSelect={(_, key) =>
+      !isDisabled &&
+      updateParams(
+        ParamHelper.setParam(params, 'tab', tabs[key].id.toLowerCase()),
+      )
     }
+  >
+    {tabs.map((tab, i) => (
+      <Tab
+        key={i}
+        eventKey={i}
+        title={
+          <TabTitleText title={isDisabled ? disabledTitle : null}>
+            {tab.name}
+          </TabTitleText>
+        }
+        className={isDisabled ? 'disabled' : null}
+      />
+    ))}
+  </PFTabs>
+);
+
+function getActiveTab({ params, tabs }) {
+  if (params.tab) {
+    const i = tabs.findIndex(
+      (x) => x.id.toLowerCase() === params.tab.toLowerCase(),
+    );
+
+    // If tab is not found, default to the first tab.
+    if (i === -1) {
+      return 0;
+    } else {
+      return i;
+    }
+  } else {
+    return 0;
   }
 }
