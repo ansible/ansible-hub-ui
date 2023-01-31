f3bbaf50c602f02c56923e19161c7eb2c9d646da
Milan Pospisil <arkanus@seznam.cz>
2023-01-31 11:53:40 +0100

    Extract config file - TODO

---
+++ i18next-scanner.config.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+module.exports = {
+    input: [
+        'src/**/*.{js,tsx}',
+    ],
+    output: './',
+    options: {
+        debug: true,
+        func: {
+            list: ['i18next.t', 'i18n.t', 't'],
+            extensions: ['.js', '.tsx']
+        },
+        trans: {
+            component: 'Trans',
+            i18nKey: 'i18nKey',
+            defaultsKey: 'defaults',
+            extensions: ['.js', '.tsx'],
+            fallbackKey: function(ns, value) {
+                return value;
+            },
+
+            // https://react.i18next.com/latest/trans-component#usage-with-simple-html-elements-like-less-than-br-greater-than-and-others-v10.4.0
+            supportBasicHtmlNodes: true, // Enables keeping the name of simple nodes (e.g. <br/>) in translations instead of indexed keys.
+            keepBasicHtmlNodesFor: ['br', 'strong', 'i', 'p'], // Which nodes are allowed to be kept in translations during defaultValue generation of <Trans>.
+
+            // https://github.com/acornjs/acorn/tree/master/acorn#interface
+            acorn: {
+                ecmaVersion: 2020,
+                sourceType: 'module', // defaults to 'module'
+            }
+        },
+        lngs: ['en'],
+        defaultLng: 'en',
+        defaultValue: '__STRING_NOT_TRANSLATED__',
+        resource: {
+            loadPath: 'locale/{{lng}}.json',
+            savePath: 'locale/{{lng}}.json',
+            jsonIndent: 2,
+            lineEnding: '\n'
+        },
+        nsSeparator: false, // namespace separator
+        keySeparator: false, // key separator
+        interpolation: {
+            prefix: '{{',
+            suffix: '}}'
+        },
+        metadata: {},
+        allowDynamicKeys: false,
+    },
+    transform: function customTransform(file, enc, done) {
+        "use strict";
+        const parser = this.parser;
+        const content = fs.readFileSync(file.path, enc);
+        let count = 0;
+
+        parser.parseFuncFromString(content, { list: ['i18next._', 'i18next.__'] }, (key, options) => {
+            parser.set(key, Object.assign({}, options, {
+                nsSeparator: false,
+                keySeparator: false
+            }));
+            ++count;
+        });
+
+        if (count > 0) {
+            console.log(`i18next-scanner: count=${chalk.cyan(count)}, file=${chalk.yellow(JSON.stringify(file.relative))}`);
+        }
+
+        done();
+    }
+};
