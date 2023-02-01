58cc0e5c7f23c147dfd2749adb6fccc27bc719cc
Milan Pospisil <arkanus@seznam.cz>
2023-02-01 12:58:40 +0100

    Class component test

--- src/entry-standalone.tsx
+++ src/entry-standalone.tsx
@@ -2,7 +2,7 @@ import { i18n } from '@lingui/core';
 import { I18nProvider } from '@lingui/react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Trans, useTranslation } from 'react-i18next';
+import { Trans, useTranslation, withTranslation } from 'react-i18next';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Constants } from 'src/constants';
 // constants must be imported after the translation library, so the translations will be loaded sooner than constants
@@ -15,18 +15,6 @@ if (!window.location.pathname.includes(UI_BASE_PATH)) {
   // react-router v6 won't redirect to base path by default
   window.history.pushState(null, null, UI_BASE_PATH);
 }
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Router basename={UI_BASE_PATH}>
-      <I18nProvider i18n={i18n}>
-        <HelloWorld />
-        <App />
-      </I18nProvider>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
 
 function HelloWorld() {
   const { t } = useTranslation();
@@ -67,3 +55,63 @@ function HelloWorld() {
     </div>
   );
 }
+
+// this base suffix is only needed here, in real scenerio, we will use something like this in component file:
+// export default withTranslation()(HelloWorldBase);
+
+class ClassHelloWorldBase extends React.Component {
+  render() {
+    const name = 'Ansible';
+    const componentName = 'Trans';
+    const t = this.props['t'];
+    return (
+      <div>
+        ClassHelloWorld component
+        -------------------------------------------------------------------------------------------------------
+        <h1>
+          {t(
+            'This.This is the hello world application: Test for simple translation.',
+          )}
+        </h1>
+        <div>
+          Non existing key - should be blank or left alone:{' '}
+          {t('non existing key - have to show this text untranslated')}
+        </div>
+        <Trans i18nKey='transTest'>
+          Hello <strong>{{ name }}</strong>: this is {{ componentName }} test.{' '}
+          <a href='/'>Empty link</a>
+        </Trans>
+        <div>This should show empty string after={t('empty translation')}</div>
+        <div>{t('Escaped code example.')}</div>
+        <div>
+          Orders:
+          {t('order', { count: 1, ordinal: true })},
+          {t('order', { count: 52, ordinal: true })},
+          {t('order', { count: 33, ordinal: true })},
+          {t('order', { count: 34, ordinal: true })},
+        </div>
+        <div>
+          The system contains {t('role', { count: 1, ordinal: true })}. The
+          system contains {t('role', { count: 12, ordinal: true })}. The system
+          contains {t('role', { count: 0, ordinal: true })}.
+        </div>
+        <div>Constants test: {Constants.STATIC_TRANSLATION_TEST}</div>
+      </div>
+    );
+  }
+}
+
+const ClassHelloWorld = withTranslation()(ClassHelloWorldBase);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router basename={UI_BASE_PATH}>
+      <I18nProvider i18n={i18n}>
+        <HelloWorld />
+        <ClassHelloWorld />
+        <App />
+      </I18nProvider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
