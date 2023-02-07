b95ca964befde66ed2396a5f45c4574c60b11d96
Martin Hradil <mhradil@redhat.com>
2023-02-07 23:19:22 +0000

    sort index.ts exports
        perl -i -npe 's/^export/import/' src/**/index.ts
        npm run prettier
        perl -i -npe 's/^import/export/' src/**/index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,23 @@
+export { ActiveUserAPI } from './active-user';
+export { ActivitiesAPI } from './activities';
+export { ApplicationInfoAPI } from './application-info';
 export { CollectionAPI } from './collection';
+export { CollectionVersionAPI } from './collection-version';
+export { ContainerDistributionAPI } from './container-distribution';
+export { ContainerTagAPI } from './container-tag';
+export { ControllerAPI } from './controller';
+export { DistributionAPI } from './distribution';
+export { ExecutionEnvironmentAPI } from './execution-environment';
+export { ExecutionEnvironmentNamespaceAPI } from './execution-environment-namespace';
+export { ExecutionEnvironmentRegistryAPI } from './execution-environment-registry';
+export { ExecutionEnvironmentRemoteAPI } from './execution-environment-remote';
+export { FeatureFlagsAPI } from './feature-flags';
+export { GenericPulpAPI } from './generic-pulp';
+export { GroupAPI } from './group';
+export { ImportAPI } from './import';
+export { MyNamespaceAPI } from './my-namespace';
 export { NamespaceAPI } from './namespace';
-export {
-  NamespaceType,
-  NamespaceListType,
-  NamespaceLinkType,
-} from './response-types/namespace';
+export { RemoteAPI } from './remotes';
 export {
   CollectionDetailType,
   CollectionListType,
@@ -18,40 +31,27 @@ export {
   PluginOption,
   ReturnedValue,
 } from './response-types/collection';
-export { ImportListType, ImportDetailType } from './response-types/import';
-export { PulpStatus } from './response-types/pulp';
-export { ImportAPI } from './import';
-export { ActiveUserAPI } from './active-user';
-export { UserType } from './response-types/user';
-export { CollectionVersionAPI } from './collection-version';
-export { MyNamespaceAPI } from './my-namespace';
-export { UserAPI } from './user';
-export { TaskAPI } from './task';
-export { GroupObjectPermissionType } from './response-types/permissions';
-export { GroupAPI } from './group';
-export { RemoteAPI } from './remotes';
-export { ApplicationInfoAPI } from './application-info';
-export { RemoteType } from './response-types/remote';
-export { DistributionAPI } from './distribution';
 export { DistributionType } from './response-types/distribution';
-export { ExecutionEnvironmentAPI } from './execution-environment';
-export { ExecutionEnvironmentRegistryAPI } from './execution-environment-registry';
 export {
-  ExecutionEnvironmentType,
   ContainerManifestType,
   ContainerRepositoryType,
+  ExecutionEnvironmentType,
 } from './response-types/execution-environment';
-export { ExecutionEnvironmentRemoteAPI } from './execution-environment-remote';
-export { WriteOnlyFieldType } from './response-types/write-only-field';
-export { ActivitiesAPI } from './activities';
-export { ContainerTagAPI } from './container-tag';
 export { FeatureFlagsType } from './response-types/feature-flags';
-export { FeatureFlagsAPI } from './feature-flags';
-export { ContainerDistributionAPI } from './container-distribution';
-export { ExecutionEnvironmentNamespaceAPI } from './execution-environment-namespace';
-export { ControllerAPI } from './controller';
-export { TaskManagementAPI } from './task-management';
-export { GenericPulpAPI } from './generic-pulp';
-export { SettingsAPI } from './settings';
+export { ImportDetailType, ImportListType } from './response-types/import';
+export {
+  NamespaceLinkType,
+  NamespaceListType,
+  NamespaceType,
+} from './response-types/namespace';
+export { GroupObjectPermissionType } from './response-types/permissions';
+export { PulpStatus } from './response-types/pulp';
+export { RemoteType } from './response-types/remote';
 export { SettingsType } from './response-types/settings';
+export { UserType } from './response-types/user';
+export { WriteOnlyFieldType } from './response-types/write-only-field';
+export { SettingsAPI } from './settings';
 export { SignCollectionAPI } from './sign-collections';
+export { TaskAPI } from './task';
+export { TaskManagementAPI } from './task-management';
+export { UserAPI } from './user';
--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,27 +1,27 @@
+export { canSign } from './can-sign';
 export { chipGroupProps } from './chip-group-props';
 export { convertContentSummaryCounts } from './content-summary';
-export { ParamHelper } from './param-helper';
-export { sanitizeDocsUrls } from './sanitize-docs-urls';
+export { errorMessage } from './fail-alerts';
+export { filterIsSet } from './filter-is-set';
+export { getContainersURL, getRepoUrl } from './get-repo-url';
+export { getHumanSize } from './get_human_size';
+export { lastSyncStatus, lastSynced } from './last-sync-task';
 export {
-  mapErrorMessages,
   ErrorMessagesType,
+  alertErrorsWithoutFields,
   isFieldValid,
   isFormValid,
-  alertErrorsWithoutFields,
+  mapErrorMessages,
 } from './map-error-messages';
-export { getRepoUrl, getContainersURL } from './get-repo-url';
+export { ParamHelper } from './param-helper';
+export { parsePulpIDFromURL } from './parse-pulp-id';
+export { sanitizeDocsUrls } from './sanitize-docs-urls';
+export { truncateSha } from './truncate_sha';
 export { twoWayMapper } from './two-way-mapper';
+export { validateURLHelper } from './validateURLHelper';
+export { waitForTask } from './wait-for-task';
 export {
   clearSetFieldsFromRequest,
   isFieldSet,
   isWriteOnly,
 } from './write-only-fields';
-export { filterIsSet } from './filter-is-set';
-export { truncateSha } from './truncate_sha';
-export { getHumanSize } from './get_human_size';
-export { parsePulpIDFromURL } from './parse-pulp-id';
-export { lastSynced, lastSyncStatus } from './last-sync-task';
-export { waitForTask } from './wait-for-task';
-export { errorMessage } from './fail-alerts';
-export { validateURLHelper } from './validateURLHelper';
-export { canSign } from './can-sign';
