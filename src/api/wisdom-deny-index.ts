3c990f4650aeb68fafdefa128002bdad041a4c48
MilanPospisil <arkanus@seznam.cz>
2023-02-22 12:37:45 +0100

    Remove and Add to deny index

--- src/api/wisdom-deny-index.ts
+++ src/api/wisdom-deny-index.ts
@@ -3,11 +3,25 @@ import { HubAPI } from './hub';
 export class API extends HubAPI {
   path = this.getUIPath('ai_deny_index/');
 
-  isInList(scope, reference) {
+  isInDenyIndex(scope, reference) {
     const params = { scope, reference };
-    return super
-      .list(params, this.path)
-      .then((response) => (response.data.count > 0 ? true : false));
+    return this.http
+      .get(this.path + `/?scope=${scope}&reference=${reference}`)
+      .then((response) => {
+        return response.data.count > 0 ? true : false;
+      });
+  }
+
+  removeFromDenyIndex(scope, reference) {
+    const params = { scope, reference };
+    const removePath = this.path + scope + '/' + reference + '/';
+    return this.http.delete(removePath);
+  }
+
+  addToDenyIndex(scope, reference) {
+    const params = { reference };
+    const addPath = this.path + scope + '/';
+    return this.http.post(addPath, params);
   }
 }
 
--- src/components/wisdom-namespace-modal/wisdom-namespace-modal.tsx
+++ src/components/wisdom-namespace-modal/wisdom-namespace-modal.tsx
@@ -1,7 +1,9 @@
 import { t } from '@lingui/macro';
-import { Modal, Spinner } from '@patternfly/react-core';
+import { Button, ButtonVariant, Modal, Spinner } from '@patternfly/react-core';
 import React, { useEffect, useState } from 'react';
 import { wisdomDenyIndexAPI } from 'src/api';
+import { AlertList, AlertType } from 'src/components';
+import { errorMessage } from 'src/utilities';
 
 interface IProps {
   scope: string;
@@ -10,33 +12,120 @@ interface IProps {
 }
 
 export const WisdomNamespaceModal = (props: IProps) => {
-  const [isInDenyList, setIsInDenyList] = useState(null);
+  const [isInDenyIndex, setIsInDenyIndex] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [alerts, setAlerts] = useState([]);
 
   useEffect(() => {
-    wisdomDenyIndexAPI.isInList(props.scope, props.reference).then((result) => {
-      setIsInDenyList(result);
-      setLoading(false);
-    });
+    wisdomDenyIndexAPI
+      .isInDenyIndex(props.scope, props.reference)
+      .then((result) => {
+        setIsInDenyIndex(result);
+        setLoading(false);
+      });
   }, []);
 
+  const addAlert = (alert: AlertType) => {
+    setAlerts([...alerts, alert]);
+  };
+
+  const closeAlert = () => {
+    setAlerts([]);
+  };
+
+  const removeFromDenyIndex = () => {
+    setLoading(true);
+    wisdomDenyIndexAPI
+      .removeFromDenyIndex(props.scope, props.reference)
+      .then((result) => {
+        setIsInDenyIndex(false);
+        setLoading(false);
+      })
+      .catch(({ response: { status, statusText } }) => {
+        addAlert({
+          title: t`Failed to add namespace to Ansible Wisdom.`,
+          variant: 'danger',
+          description: errorMessage(status, statusText),
+        });
+        setLoading(false);
+      });
+  };
+
+  const addToDenyIndex = () => {
+    setLoading(true);
+    wisdomDenyIndexAPI
+      .addToDenyIndex(props.scope, props.reference)
+      .then((result) => {
+        setIsInDenyIndex(true);
+        setLoading(false);
+      })
+      .catch(({ response: { status, statusText } }) => {
+        addAlert({
+          title: t`Failed to remove namespace from Ansible Wisdom.`,
+          variant: 'danger',
+          description: errorMessage(status, statusText),
+        });
+        setLoading(false);
+      });
+  };
+
+  let actions = [];
+
+  if (!loading) {
+    if (isInDenyIndex) {
+      actions.push(
+        <Button
+          key='remove'
+          onClick={removeFromDenyIndex}
+          variant={ButtonVariant.primary}
+        >
+          {t`Add to Ansible wisdom`}
+        </Button>,
+      );
+    } else {
+      actions.push(
+        <Button
+          key='add'
+          onClick={addToDenyIndex}
+          variant={ButtonVariant.primary}
+        >
+          {t`Remove from Ansible wisdom`}
+        </Button>,
+      );
+    }
+
+    actions.push(
+      <Button key='add' onClick={() => props.cancelAction()} variant='link'>
+        {t`Cancel`}
+      </Button>,
+    );
+  }
+
+  const namespace = props.reference;
   return (
     <Modal
-      actions={[]}
+      actions={actions}
       isOpen={true}
       onClose={props.cancelAction}
       title={t`Wisdom namespace modal`}
       titleIconVariant='warning'
       variant='small'
     >
-      Namespace {props.reference}
+      <AlertList alerts={alerts} closeAlert={() => closeAlert()} />
       {loading ? (
         <Spinner />
       ) : (
         <div>
-          {isInDenyList
-            ? t`The namespace will not be used by Ansible Wisdom`
-            : t`The namespace will be used by Ansible Wisdom`}
+          <div>
+            {!loading && isInDenyIndex
+              ? t`The namespace ${namespace} will not be used by Ansible Wisdom`
+              : t`The namespace ${namespace} will be used by Ansible Wisdom`}
+          </div>
+          <br />
+          <div>
+            Some information about Ansible Wisdom and why it is good to have
+            namespaces included in the project.
+          </div>
         </div>
       )}
     </Modal>
