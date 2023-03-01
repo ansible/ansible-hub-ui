25fba412f89516e3eb01007264185ff3a982e6a4
jerabekjiri <jerabekjirka@email.cz>
2023-03-01 17:58:43 +0100

    fix repo url and rebase
    Issue: AAH-1778

--- src/utilities/get-repo-url.ts
+++ src/utilities/get-repo-url.ts
@@ -1,12 +1,10 @@
 // Returns the API path for a specific repository
-export function getRepoUrl(distributionPath: string) {
+export function getRepoUrl() {
   // If the api is hosted on another URL, use API_HOST as the host part of the URL.
   // Otherwise use the host that the UI is served from
   const host = API_HOST ? API_HOST : window.location.origin;
 
-  return distributionPath
-    ? `${host}${API_BASE_PATH}content/${distributionPath}/`
-    : `${host}${API_BASE_PATH}`;
+  return `${host}${API_BASE_PATH}`;
 }
 
 // returns the server name for (protocol-less) container urls
