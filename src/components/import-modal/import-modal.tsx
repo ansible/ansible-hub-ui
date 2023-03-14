753e0867012cf63d00b2a2bfa91dd9ec69dd00e9
MilanPospisil <arkanus@seznam.cz>
2023-03-14 13:19:34 +0100

    Import-modal.

--- src/components/import-modal/import-modal.tsx
+++ src/components/import-modal/import-modal.tsx
@@ -2,7 +2,7 @@ import { t } from '@lingui/macro';
 import { Button, Modal } from '@patternfly/react-core';
 import { FolderOpenIcon, SpinnerIcon } from '@patternfly/react-icons';
 import axios from 'axios';
-import * as React from 'react';
+import React, { useState } from 'react';
 import {
   CollectionAPI,
   CollectionListType,
@@ -24,111 +24,36 @@ interface IProps {
   namespace: string;
 }
 
-interface IState {
-  file?: File;
-  errors?: string;
-  uploadProgress: number;
-  uploadStatus: Status;
-}
+export const ImportModal = (props: IProps) => {
+  const acceptedFileTypes = ['application/x-gzip', 'application/gzip'];
+  let cancelToken: ReturnType<typeof CollectionAPI.getCancelToken>;
+  const COLLECTION_NAME_REGEX = /[0-9a-z_]+-[0-9a-z_]+-[0-9A-Za-z.+-]+/;
 
-export class ImportModal extends React.Component<IProps, IState> {
-  acceptedFileTypes = ['application/x-gzip', 'application/gzip'];
-  cancelToken: ReturnType<typeof CollectionAPI.getCancelToken>;
-  COLLECTION_NAME_REGEX = /[0-9a-z_]+-[0-9a-z_]+-[0-9A-Za-z.+-]+/;
+  const [file, setFile] = useState<File>(undefined);
+  const [errors, setErrors] = useState<string>('');
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+  const [uploadStatus, setUploadStatus] = useState<Status>(Status.waiting);
 
-  constructor(props) {
-    super(props);
+  const { isOpen, collection } = props;
 
-    this.state = {
-      file: undefined,
-      errors: '',
-      uploadProgress: 0,
-      uploadStatus: Status.waiting,
-    };
-  }
-  render() {
-    const { isOpen, collection } = this.props;
-
-    const { file, errors, uploadProgress, uploadStatus } = this.state;
-    return (
-      <Modal
-        variant='small'
-        title={
-          collection ? t`New version of ${collection.name}` : t`New collection`
-        }
-        isOpen={isOpen}
-        onClose={() => this.handleClose()}
-        actions={[
-          <Button
-            key='confirm'
-            variant='primary'
-            onClick={() => this.saveFile()}
-            isDisabled={!this.canUpload()}
-            data-cy={'confirm-upload'}
-          >
-            {t`Upload`}
-          </Button>,
-          <Button
-            key='cancel'
-            variant='secondary'
-            onClick={() => this.handleClose()}
-          >
-            {t`Cancel`}
-          </Button>,
-        ]}
-      >
-        <div className='upload-collection'>
-          <form>
-            <input
-              disabled={uploadStatus !== Status.waiting}
-              className='upload-file'
-              type='file'
-              id='collection-widget'
-              onChange={(e) => this.handleFileUpload(e.target.files)}
-            />
-            <label className='upload-file-label' htmlFor='collection-widget'>
-              <div className='upload-box'>
-                <div className='upload-button'>{this.renderFileIcon()}</div>
-                <div className='upload-text'>
-                  {file != null ? file.name : t`Select file`}
-                  <div
-                    className='loading-bar'
-                    style={{
-                      width: uploadProgress * 100 + '%',
-                    }}
-                  />
-                </div>
-              </div>
-            </label>
-          </form>
-          {errors ? (
-            <span className='file-error-messages'>
-              <i className='pficon-error-circle-o' /> {errors}
-            </span>
-          ) : null}
-        </div>
-      </Modal>
-    );
-  }
-
-  private canUpload() {
-    if (this.state.errors) {
+  function canUpload() {
+    if (errors) {
       return false;
     }
 
-    if (this.state.uploadStatus !== Status.waiting) {
+    if (uploadStatus !== Status.waiting) {
       return false;
     }
 
-    if (!this.state.file) {
+    if (!file) {
       return false;
     }
 
     return true;
   }
 
-  private renderFileIcon() {
-    switch (this.state.uploadStatus) {
+  function renderFileIcon() {
+    switch (uploadStatus) {
       case Status.uploading:
         return <SpinnerIcon className='fa-spin'></SpinnerIcon>;
       default:
@@ -136,72 +61,64 @@ export class ImportModal extends React.Component<IProps, IState> {
     }
   }
 
-  private handleFileUpload(files) {
+  function handleFileUpload(files) {
     // Selects the artifact that will be uploaded and performs some basic
     // preliminary checks on it.
     const newCollection = files[0];
-    const { collection } = this.props;
+    const { collection } = props;
 
     if (files.length > 1) {
-      this.setState({
-        errors: t`Please select no more than one file.`,
-      });
-    } else if (!this.acceptedFileTypes.includes(newCollection.type)) {
-      this.setState({
-        errors: t`Invalid file format.`,
-        file: newCollection,
-        uploadProgress: 0,
-      });
-    } else if (!this.COLLECTION_NAME_REGEX.test(newCollection.name)) {
-      this.setState({
-        errors: t`Invalid file name. Collections must be formatted as 'namespace-collection_name-1.0.0'`,
-        file: newCollection,
-        uploadProgress: 0,
-      });
+      setErrors(t`Please select no more than one file.`);
+    } else if (!acceptedFileTypes.includes(newCollection.type)) {
+      setErrors(t`Invalid file format.`);
+      setFile(newCollection);
+      setUploadProgress(0);
+    } else if (!COLLECTION_NAME_REGEX.test(newCollection.name)) {
+      setErrors(
+        t`Invalid file name. Collections must be formatted as 'namespace-collection_name-1.0.0'`,
+      );
+      setFile(newCollection);
+      setUploadProgress(0);
     } else if (
       collection &&
       collection.name !== newCollection.name.split('-')[1]
     ) {
-      this.setState({
-        errors: t`The collection you have selected doesn't appear to match ${collection.name}`,
-        file: newCollection,
-        uploadProgress: 0,
-      });
-    } else if (this.props.namespace != newCollection.name.split('-')[0]) {
-      this.setState({
-        errors: t`The collection you have selected does not match this namespace.`,
-        file: newCollection,
-        uploadProgress: 0,
-      });
+      setErrors(
+        t`The collection you have selected doesn't appear to match ${collection.name}`,
+      );
+      setFile(newCollection);
+      setUploadProgress(0);
+    } else if (props.namespace != newCollection.name.split('-')[0]) {
+      setErrors(
+        t`The collection you have selected does not match this namespace.`,
+      );
+      setFile(newCollection);
+      setUploadProgress(0);
     } else {
-      this.setState({
-        errors: '',
-        file: newCollection,
-        uploadProgress: 0,
-      });
+      setErrors('');
+      setFile(newCollection);
+      setUploadProgress(0);
     }
   }
 
-  saveFile() {
-    this.setState({ uploadStatus: Status.uploading });
+  function saveFile() {
+    setUploadStatus(Status.uploading);
     const artifact = {
-      file: this.state.file,
+      file: file,
       sha256: '',
     } as CollectionUploadType;
 
-    this.cancelToken = CollectionAPI.getCancelToken();
+    cancelToken = CollectionAPI.getCancelToken();
 
     CollectionAPI.upload(
       artifact,
       (e) => {
-        this.setState({
-          uploadProgress: e.loaded / e.total,
-        });
+        setUploadProgress(e.loaded / e.total);
       },
-      this.cancelToken,
+      cancelToken,
     )
       .then((response) => {
-        this.props.onUploadSuccess(response);
+        props.onUploadSuccess(response);
       })
       .catch((errors) => {
         let errorMessage = '';
@@ -224,32 +141,81 @@ export class ImportModal extends React.Component<IProps, IState> {
             errorMessage = t`API error. Status code: ${errors.response.status}`;
           }
         }
-
-        this.setState({
-          uploadStatus: Status.waiting,
-          errors: errorMessage,
-        });
+        setUploadStatus(Status.waiting);
+        setErrors(errorMessage);
       })
       .finally(() => {
-        this.cancelToken = null;
+        cancelToken = null;
       });
   }
 
-  handleClose() {
+  function handleClose() {
     let msg = null;
-    if (this.cancelToken && this.state.uploadStatus === Status.uploading) {
+    if (cancelToken && uploadStatus === Status.uploading) {
       msg = t`Collection upload canceled`;
-      this.cancelToken.cancel(msg);
+      cancelToken.cancel(msg);
     }
 
-    this.setState(
-      {
-        file: undefined,
-        errors: '',
-        uploadProgress: 0,
-        uploadStatus: Status.waiting,
-      },
-      () => this.props.setOpen(false, msg),
-    );
+    setFile(undefined);
+    setErrors('');
+    setUploadProgress(0);
+    setUploadStatus(Status.waiting);
+    props.setOpen(false, msg);
   }
-}
+
+  return (
+    <Modal
+      variant='small'
+      title={
+        collection ? t`New version of ${collection.name}` : t`New collection`
+      }
+      isOpen={isOpen}
+      onClose={() => handleClose()}
+      actions={[
+        <Button
+          key='confirm'
+          variant='primary'
+          onClick={() => saveFile()}
+          isDisabled={!canUpload()}
+          data-cy={'confirm-upload'}
+        >
+          {t`Upload`}
+        </Button>,
+        <Button key='cancel' variant='secondary' onClick={() => handleClose()}>
+          {t`Cancel`}
+        </Button>,
+      ]}
+    >
+      <div className='upload-collection'>
+        <form>
+          <input
+            disabled={uploadStatus !== Status.waiting}
+            className='upload-file'
+            type='file'
+            id='collection-widget'
+            onChange={(e) => handleFileUpload(e.target.files)}
+          />
+          <label className='upload-file-label' htmlFor='collection-widget'>
+            <div className='upload-box'>
+              <div className='upload-button'>{renderFileIcon()}</div>
+              <div className='upload-text'>
+                {file != null ? file.name : t`Select file`}
+                <div
+                  className='loading-bar'
+                  style={{
+                    width: uploadProgress * 100 + '%',
+                  }}
+                />
+              </div>
+            </div>
+          </label>
+        </form>
+        {errors ? (
+          <span className='file-error-messages'>
+            <i className='pficon-error-circle-o' /> {errors}
+          </span>
+        ) : null}
+      </div>
+    </Modal>
+  );
+};
