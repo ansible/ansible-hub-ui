d310518b5bf0d232697cc39ea0397a4f494ce843
MilanPospisil <arkanus@seznam.cz>
2023-03-14 14:44:43 +0100

    loading components

--- src/components/loading/loading-page-spinner.tsx
+++ src/components/loading/loading-page-spinner.tsx
@@ -1,12 +1,10 @@
 import { Bullseye, Spinner } from '@patternfly/react-core';
 import * as React from 'react';
 
-export class LoadingPageSpinner extends React.Component {
-  render() {
-    return (
-      <Bullseye style={{ width: '100%', height: '100%' }}>
-        <Spinner />
-      </Bullseye>
-    );
-  }
-}
+export const LoadingPageSpinner = () => {
+  return (
+    <Bullseye style={{ width: '100%', height: '100%' }}>
+      <Spinner />
+    </Bullseye>
+  );
+};
--- src/components/loading/loading-with-header.tsx
+++ src/components/loading/loading-with-header.tsx
@@ -2,25 +2,23 @@ import { Skeleton, Title } from '@patternfly/react-core';
 import * as React from 'react';
 import { LoadingPageSpinner, Main } from 'src/components';
 
-export class LoadingPageWithHeader extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <section
-          className={
-            'pf-l-page-header pf-c-page-header pf-l-page__main-section pf-c-page__main-section pf-m-light'
-          }
-        >
-          <Title headingLevel='h1'>
-            <Skeleton></Skeleton>
-          </Title>
+export const LoadingPageWithHeader = () => {
+  return (
+    <React.Fragment>
+      <section
+        className={
+          'pf-l-page-header pf-c-page-header pf-l-page__main-section pf-c-page__main-section pf-m-light'
+        }
+      >
+        <Title headingLevel='h1'>
+          <Skeleton></Skeleton>
+        </Title>
+      </section>
+      <Main>
+        <section>
+          <LoadingPageSpinner></LoadingPageSpinner>
         </section>
-        <Main>
-          <section>
-            <LoadingPageSpinner></LoadingPageSpinner>
-          </section>
-        </Main>
-      </React.Fragment>
-    );
-  }
-}
+      </Main>
+    </React.Fragment>
+  );
+};
