49f6b5dcd49c6a866789e58e3d0c2743e45d29b5
Martin Hradil <mhradil@redhat.com>
2023-03-16 01:09:15 +0000

    context.queueAlert

--- src/loaders/app-context.ts
+++ src/loaders/app-context.ts
@@ -7,8 +7,9 @@ export interface IAppContextType {
   setUser: (user: UserType) => void;
   selectedRepo?: string;
   featureFlags: FeatureFlagsType;
-  alerts?: AlertType[];
-  setAlerts?: (alerts: AlertType[]) => void;
+  alerts: AlertType[];
+  setAlerts: (alerts: AlertType[]) => void;
+  queueAlert: (alert: AlertType) => void;
   settings: SettingsType;
   hasPermission: (name: string) => boolean;
 }
