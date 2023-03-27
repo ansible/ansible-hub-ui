caf8a981bb33982120367235833db8b9f5baaff7
jerabekjiri <jerabekjirka@email.cz>
2023-03-28 00:48:12 +0200

    improve collection distribution page
    Issue: AAH-767

--- src/containers/collection-detail/collection-distributions.tsx
+++ src/containers/collection-detail/collection-distributions.tsx
@@ -1,19 +1,29 @@
 import { t } from '@lingui/macro';
+import { Toolbar, ToolbarGroup, ToolbarItem } from '@patternfly/react-core';
 import React, { useEffect, useState } from 'react';
 import { AnsibleDistributionAPI } from 'src/api';
 import {
+  AppliedFilters,
   ClipboardCopy,
   CollectionHeader,
+  CompoundFilter,
   DateComponent,
+  EmptyStateFilter,
+  EmptyStateNoData,
+  LoadingPageSpinner,
   LoadingPageWithHeader,
   Main,
   Pagination,
   SortTable,
 } from 'src/components';
 import { Paths, formatPath, namespaceBreadcrumb } from 'src/paths';
-import { RouteProps, withRouter } from 'src/utilities';
-import { getRepoUrl } from 'src/utilities';
-import { ParamHelper } from 'src/utilities/param-helper';
+import {
+  ParamHelper,
+  RouteProps,
+  filterIsSet,
+  getRepoUrl,
+  withRouter,
+} from 'src/utilities';
 import { loadCollection } from './base';
 
 const CollectionDistributions = (props: RouteProps) => {
@@ -22,6 +32,11 @@ const CollectionDistributions = (props: RouteProps) => {
   const [collections, setCollections] = useState([]);
   const [collection, setCollection] = useState(null);
   const [content, setContent] = useState(null);
+  const [inputText, setInputText] = useState('');
+
+  const [distributions, setDistributions] = useState(null);
+  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   const [params, setParams] = useState(
     Object.keys(routeParams).length
@@ -30,9 +45,6 @@ const CollectionDistributions = (props: RouteProps) => {
           sort: '-pulp_created',
         },
   );
-  const [distributions, setDistributions] = useState(null);
-  const [count, setCount] = useState(0);
-
   const loadCollections = (forceReload) => {
     loadCollection({
       forceReload,
@@ -50,6 +62,7 @@ const CollectionDistributions = (props: RouteProps) => {
   };
 
   const loadDistributions = async (repositoryHref: string) => {
+    setLoading(true);
     const distroList = await AnsibleDistributionAPI.list({
       repository: repositoryHref,
       ...ParamHelper.getReduced(params, ['version']),
@@ -57,6 +70,7 @@ const CollectionDistributions = (props: RouteProps) => {
 
     setDistributions(distroList.data.results);
     setCount(distroList.data.count);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -67,7 +81,7 @@ const CollectionDistributions = (props: RouteProps) => {
     loadCollections(false);
   }, [params]);
 
-  if (!collection || !content || collections.length <= 0 || !distributions) {
+  if (!collection || !content || collections.length <= 0) {
     return <LoadingPageWithHeader></LoadingPageWithHeader>;
   }
 
@@ -102,35 +116,84 @@ const CollectionDistributions = (props: RouteProps) => {
       'token=<put your token here>',
     ].join('\n');
 
-  const sortTableOptions = {
-    headers: [
-      {
-        title: t`Name`,
-        type: 'alpha',
-        id: 'name',
-      },
-      {
-        title: t`Base path`,
-        type: 'alpha',
-        id: 'base_path',
-      },
-      {
-        title: t`Created`,
-        type: 'alpha',
-        id: 'pulp_created',
-      },
-      {
-        title: t`CLI configuration`,
-        type: 'none',
-        id: '',
-      },
-    ],
-  };
-
   const updateParamsMixin = (params) => {
     props.navigate({
       search: '?' + ParamHelper.getQueryString(params || []),
     });
+
+    setParams(params);
+  };
+
+  const renderTable = (distributions, params) => {
+    if (distributions.length === 0) {
+      return filterIsSet(params, [
+        'name__icontains',
+        'base_path__icontains',
+      ]) ? (
+        <EmptyStateFilter />
+      ) : (
+        <EmptyStateNoData
+          title={t`No distributions yet`}
+          description={t`Collection doesn't have any distribution assigned.`}
+        />
+      );
+    }
+
+    const sortTableOptions = {
+      headers: [
+        {
+          title: t`Name`,
+          type: 'alpha',
+          id: 'name',
+        },
+        {
+          title: t`Base path`,
+          type: 'alpha',
+          id: 'base_path',
+        },
+        {
+          title: t`Created`,
+          type: 'alpha',
+          id: 'pulp_created',
+        },
+        {
+          title: t`CLI configuration`,
+          type: 'none',
+          id: '',
+        },
+      ],
+    };
+
+    return (
+      <table
+        aria-label={t`Collection distributions`}
+        className='hub-c-table-content pf-c-table'
+      >
+        <SortTable
+          options={sortTableOptions}
+          params={params}
+          updateParams={(params) => {
+            updateParamsMixin(params);
+          }}
+        />
+        <tbody>
+          {distributions.map((distribution, i) => (
+            <tr key={i}>
+              <td>{distribution.name}</td>
+              <td>{distribution.base_path}</td>
+              <td>
+                <DateComponent date={distribution.pulp_created} />
+              </td>
+              <td>
+                <ClipboardCopy isCode isReadOnly variant={'expansion'} key={i}>
+                  {cliConfig(distribution)}
+                </ClipboardCopy>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
   };
 
   return (
@@ -145,53 +208,71 @@ const CollectionDistributions = (props: RouteProps) => {
           updateParamsMixin(
             ParamHelper.setParam(params, 'version', params.version),
           );
-          setParams(params);
         }}
         breadcrumbs={breadcrumbs}
         activeTab='distributions'
       />
       <Main>
         <section className='body'>
-          <table
-            aria-label={t`Collection distributions`}
-            className='hub-c-table-content pf-c-table'
-          >
-            <SortTable
-              options={sortTableOptions}
-              params={params}
-              updateParams={(params) => {
-                updateParamsMixin(params);
-                setParams(params);
-              }}
-            />
-            <tbody>
-              {distributions.map((distribution, i) => (
-                <tr key={i}>
-                  <td>{distribution.name}</td>
-                  <td>{distribution.base_path}</td>
-                  <td>
-                    <DateComponent date={distribution.pulp_created} />
-                  </td>
-                  <td>
-                    <ClipboardCopy
-                      isCode
-                      isReadOnly
-                      variant={'expansion'}
-                      key={i}
-                    >
-                      {cliConfig(distribution)}
-                    </ClipboardCopy>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <div className='toolbar hub-toolbar'>
+            <Toolbar>
+              <ToolbarGroup>
+                <ToolbarItem>
+                  <CompoundFilter
+                    inputText={inputText}
+                    onChange={(text) => {
+                      setInputText(text);
+                    }}
+                    updateParams={(p) => {
+                      updateParamsMixin(p);
+                    }}
+                    params={params}
+                    filterConfig={[
+                      {
+                        id: 'name__icontains',
+                        title: t`Name`,
+                      },
+                      {
+                        id: 'base_path__icontains',
+                        title: t`Base path`,
+                      },
+                    ]}
+                  />
+                </ToolbarItem>
+              </ToolbarGroup>
+            </Toolbar>
 
+            <Pagination
+              params={params}
+              updateParams={(p) => {
+                updateParamsMixin(p);
+              }}
+              count={count}
+              isTop
+            />
+          </div>
+
+          <AppliedFilters
+            updateParams={(p) => {
+              updateParamsMixin(p);
+              setInputText('');
+            }}
+            params={params}
+            ignoredParams={['page_size', 'page', 'sort']}
+            niceNames={{
+              base_path__icontains: t`Base path`,
+              name__icontains: t`Name`,
+            }}
+          />
+          {loading ? (
+            <LoadingPageSpinner />
+          ) : (
+            renderTable(distributions, params)
+          )}
           <Pagination
             params={params}
             updateParams={(p) => {
               updateParamsMixin(p);
-              setParams(p);
             }}
             count={count}
           />
