f2907c3417cc0ff9efe78fe82d7298a9195a86d1
Martin Hradil <mhradil@redhat.com>
2023-04-03 13:32:29 +0000

    collection add picker

--- src/actions/ansible-repository-collection-version-add.tsx
+++ src/actions/ansible-repository-collection-version-add.tsx
@@ -1,18 +1,19 @@
 import { t } from '@lingui/macro';
-import { Button, Modal } from '@patternfly/react-core';
+import { Button, Modal, Radio } from '@patternfly/react-core';
 import React, { useState } from 'react';
-import { AnsibleRepositoryAPI } from 'src/api';
-import { AlertList, closeAlert } from 'src/components';
+import {
+  AnsibleRepositoryAPI,
+  CollectionVersionAPI,
+  CollectionVersionSearch,
+} from 'src/api';
+import { AlertList, AlertType, DetailList, closeAlert } from 'src/components';
 import { canEditAnsibleRepository } from 'src/permissions';
 import { handleHttpError, parsePulpIDFromURL, taskAlert } from 'src/utilities';
 import { Action } from './action';
 
 const add = (
-  {
-    collection: { namespace, name, version, pulp_href: collectionVersionHref },
-    repositoryHref,
-    repositoryName,
-  },
+  { repositoryHref, repositoryName },
+  { namespace, name, version, pulp_href: collectionVersionHref },
   { addAlert, setState, query },
 ) => {
   const pulpId = parsePulpIDFromURL(repositoryHref);
@@ -25,7 +26,7 @@ const add = (
         ),
       );
       setState({ addCollectionVersionModal: null });
-      query();
+      query({});
     })
     .catch(
       handleHttpError(
@@ -40,18 +41,66 @@ const AddCollectionVersionModal = ({
   addAction,
   closeAction,
 }: {
-  addAction: () => void;
+  addAction: (selected) => void;
   closeAction: () => void;
 }) => {
   const [alerts, setAlerts] = useState([]);
   const [selected, setSelected] = useState(null);
+
+  const addAlert = (alert: AlertType) => {
+    setAlerts([...alerts, alert]);
+  };
+
+  const query = ({ params }) => {
+    const newParams = { ...params };
+    newParams.ordering = newParams.sort;
+    delete newParams.sort;
+
+    return CollectionVersionAPI.list({
+      ...newParams,
+    }).then(
+      ({
+        data: {
+          meta: { count },
+          data: results,
+        },
+      }) => ({
+        data: { count, results },
+      }),
+    );
+  };
+
+  const [modalState, setModalState] = useState({});
+
+  const renderTableRow = (item: CollectionVersionSearch, index: number) => {
+    const {
+      collection_version: { name, namespace, version, description },
+    } = item;
+
+    return (
+      <tr onClick={() => setSelected(item)} key={index}>
+        <td>
+          <Radio
+            aria-label={`${namespace}.${name} v${version}`}
+            id={`collection-${index}`}
+            isChecked={selected === item}
+            name={`collection-${index}`}
+          />
+        </td>
+        <td>
+          {namespace}.{name} v{version}
+        </td>
+        <td>{description}</td>
+      </tr>
+    );
+  };
 
   return (
     <Modal
       actions={[
         <Button
           key='confirm'
-          onClick={addAction}
+          onClick={() => addAction(selected)}
           variant='primary'
           isDisabled={!selected}
         >
@@ -66,7 +115,54 @@ const AddCollectionVersionModal = ({
       title={t`Select a collection`}
       variant='large'
     >
-      <section className='modal-body' data-cy='modal-body'></section>
+      <section className='modal-body' data-cy='modal-body'>
+        <DetailList<CollectionVersionSearch>
+          actionContext={{
+            addAlert,
+            state: modalState,
+            setState: setModalState,
+            query,
+            hasPermission: () => {
+              throw 'unused';
+            },
+          }}
+          defaultPageSize={10}
+          defaultSort={'name'}
+          errorTitle={t`Collection versions could not be displayed.`}
+          filterConfig={[
+            {
+              id: 'keywords',
+              title: t`Keywords`,
+            },
+            {
+              id: 'namespace',
+              title: t`Namespace`,
+            },
+          ]}
+          noDataDescription={t`Collection versions will appear once a collection is uploaded.`}
+          noDataTitle={t`No collection versions yet`}
+          query={query}
+          renderTableRow={renderTableRow}
+          sortHeaders={[
+            {
+              title: '',
+              type: 'none',
+              id: 'radio',
+            },
+            {
+              title: t`Collection`,
+              type: 'none',
+              id: 'col1',
+            },
+            {
+              title: t`Description`,
+              type: 'none',
+              id: 'col2',
+            },
+          ]}
+          title={t`Collection versions`}
+        />
+      </section>
 
       <AlertList
         alerts={alerts}
@@ -82,8 +178,8 @@ export const ansibleRepositoryCollectionVersionAddAction = Action({
   modal: ({ addAlert, state, setState, query }) =>
     state.addCollectionVersionModal ? (
       <AddCollectionVersionModal
-        addAction={() =>
-          add(state.addCollectionVersionModal, {
+        addAction={(collection) =>
+          add(state.addCollectionVersionModal, collection.collection_version, {
             addAlert,
             setState,
             query,
