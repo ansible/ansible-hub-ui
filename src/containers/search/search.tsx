686ef1f5f08385730c3cb29a05c3811c5bc9a824
Martin Hradil <mhradil@redhat.com>
2023-04-17 15:00:05 +0200

    Restyle collection list item controls to keep alignment despite varying repository name lengths (#3603)
    Issue: AAH-2303

--- src/containers/search/search.tsx
+++ src/containers/search/search.tsx
@@ -398,22 +398,28 @@ class Search extends React.Component<RouteProps, IState> {
 
     const displayMenu = menuItems.length > 0;
 
-    return (
-      <React.Fragment>
-        {list && hasPermission('galaxy.upload_to_namespace') && (
+    if (list) {
+      return {
+        uploadButton: hasPermission('galaxy.upload_to_namespace') ? (
           <Button
             onClick={() => this.checkUploadPrivilleges(collection)}
             variant='secondary'
           >
             {t`Upload new version`}
           </Button>
+        ) : null,
+        dropdownMenu: displayMenu ? (
+          <StatefulDropdown items={menuItems} ariaLabel='collection-kebab' />
+        ) : null,
+      };
+    }
+
+    return (
+      <span className={cx(!displayMenu && 'hidden-menu-space')}>
+        {displayMenu && (
+          <StatefulDropdown items={menuItems} ariaLabel='collection-kebab' />
         )}
-        <span className={cx(!displayMenu && 'hidden-menu-space')}>
-          {displayMenu && (
-            <StatefulDropdown items={menuItems} ariaLabel='collection-kebab' />
-          )}
-        </span>
-      </React.Fragment>
+      </span>
     );
   }
 
@@ -495,11 +501,7 @@ class Search extends React.Component<RouteProps, IState> {
       (el) => el.name === name && el.namespace === namespace,
     );
 
-    if (synclist.policy === 'include') {
-      return !(found === undefined);
-    } else {
-      return found === undefined;
-    }
+    return synclist.policy === 'include' ? !!found : !found;
   }
 
   private renderList(collections) {
@@ -509,19 +511,15 @@ class Search extends React.Component<RouteProps, IState> {
           <DataList className='data-list' aria-label={t`List of Collections`}>
             {collections.map((c, i) => (
               <CollectionListItem
-                showNamespace={true}
                 key={i}
-                {...c}
-                controls={
-                  <>
-                    {this.renderSyncToogle(
-                      c.collection_version.name,
-                      c.collection_version.namespace,
-                    )}
-                    {this.renderMenu(true, c)}
-                  </>
-                }
+                collection={c}
                 displaySignatures={this.context.featureFlags.display_signatures}
+                showNamespace
+                synclistSwitch={this.renderSyncToogle(
+                  c.collection_version.name,
+                  c.collection_version.namespace,
+                )}
+                {...this.renderMenu(true, c)}
               />
             ))}
           </DataList>
