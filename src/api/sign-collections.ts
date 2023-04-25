6f17d16ad3e7f0f6e30726cebdce9757151d80a5
Martin Hradil <mhradil@redhat.com>
2023-04-25 23:45:21 +0000

    SignCollectionsAPI - support signing by repository name in addition to pulp_href

--- src/api/sign-collections.ts
+++ src/api/sign-collections.ts
@@ -1,13 +1,15 @@
+import { t } from '@lingui/macro';
 import {
   AnsibleDistributionAPI,
+  AnsibleRepositoryAPI,
   CollectionVersionSearch,
-  findDistroBasePathByRepo,
 } from 'src/api';
 import { HubAPI } from './hub';
 
 interface SignNamespace {
   signing_service?: string;
-  repository: CollectionVersionSearch['repository'];
+  repository?: CollectionVersionSearch['repository'];
+  repository_name?: string;
   namespace: string;
 }
 
@@ -24,19 +26,26 @@ type SignProps = SignNamespace | SignCollection | SignVersion;
 class API extends HubAPI {
   apiPath = this.getUIPath('collection_signing/');
 
-  async sign(data: SignProps) {
-    const { repository, ...args } = data;
-    const distros = await AnsibleDistributionAPI.list({
-      repository: repository.pulp_href,
-    });
+  async sign({ repository, repository_name, ...args }: SignProps) {
+    if (!repository && repository_name) {
+      repository = (await AnsibleRepositoryAPI.list({ name: repository_name }))
+        ?.data?.results?.[0];
+    }
 
-    const distroBasePath = findDistroBasePathByRepo(
-      distros.data.results,
-      repository,
-    );
+    const distribution = (
+      await AnsibleDistributionAPI.list({
+        repository: repository?.pulp_href,
+      })
+    )?.data?.results?.[0];
+
+    if (!distribution) {
+      return Promise.reject({
+        response: { status: t`Failed to find a distribution` },
+      });
+    }
 
     const updatedData = {
-      distro_base_path: distroBasePath,
+      distro_base_path: distribution.base_path,
       ...args,
     };
 
