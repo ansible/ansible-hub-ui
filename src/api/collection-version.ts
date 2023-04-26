c27e71bad51ee673a0de64174934fdd0a3fe9483
Martin Hradil <mhradil@redhat.com>
2023-04-26 16:37:42 +0000

    Fix approve/reject when repository name is different from distribution base_path
    when calling the collection version copy/move API, the endpoint accepts source base path and destination base path,
    those are usually identical to repository name, but not always .. any repo with spaces in the name will have a different base path
    
    This ensures we call the endpoint with actual distribution base paths, not repository names.
    
    No-Issue

--- src/api/collection-version.ts
+++ src/api/collection-version.ts
@@ -4,25 +4,25 @@ import { HubAPI } from './hub';
 export class API extends HubAPI {
   apiPath = 'v3/plugin/ansible/search/collection-versions/';
 
-  setRepository(
+  move(
     namespace: string,
     name: string,
     version: string,
-    originalRepo: string,
-    destinationRepo: string,
+    source_base_path: string,
+    destination_base_path: string,
   ) {
-    const path = `v3/collections/${namespace}/${name}/versions/${version}/move/${originalRepo}/${destinationRepo}/`;
+    const path = `v3/collections/${namespace}/${name}/versions/${version}/move/${source_base_path}/${destination_base_path}/`;
     return this.create({}, path);
   }
 
-  copyToRepository(
+  copy(
     namespace: string,
     name: string,
     version: string,
-    originalRepo: string,
-    destinationRepo: string,
+    source_base_path: string,
+    destination_base_path: string,
   ) {
-    const path = `v3/collections/${namespace}/${name}/versions/${version}/copy/${originalRepo}/${destinationRepo}/`;
+    const path = `v3/collections/${namespace}/${name}/versions/${version}/copy/${source_base_path}/${destination_base_path}/`;
     return this.create({}, path);
   }
 
