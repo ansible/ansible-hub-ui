5d7c18304af587ad2899ef01fe723a46a268564a
jctanner <tanner.jc@gmail.com>
2023-05-05 10:10:15 -0400

    Fix getRepoUrl so that it takes a parameter and returns the correct url. (#3691)
    * Fix getRepoUrl so that it takes a parameter and returns the correct url.
    * Real equality.
    * Fix trailing backslash.
    
    No-Issue
    
    Signed-off-by: James Tanner <tanner.jc@gmail.com>

--- src/utilities/get-repo-url.ts
+++ src/utilities/get-repo-url.ts
@@ -1,10 +1,13 @@
 // Returns the API path for a specific repository
-export function getRepoUrl() {
+export function getRepoUrl(reponame) {
   // If the api is hosted on another URL, use API_HOST as the host part of the URL.
   // Otherwise use the host that the UI is served from
   const host = API_HOST ? API_HOST : window.location.origin;
 
-  return `${host}${API_BASE_PATH}`;
+  if (reponame === 'published') {
+    return `${host}${API_BASE_PATH}`;
+  }
+  return `${host}${API_BASE_PATH}content/${reponame}/`;
 }
 
 // returns the server name for (protocol-less) container urls
