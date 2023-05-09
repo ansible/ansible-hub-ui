5280d60de5e2ae8c159772dca1fc469041aac2b7
Shaiah Emigh-Doyle <semighdo@redhat.com>
2023-05-09 13:25:22 -0400

    use new namespace api
    Issue: AAH-2190

--- src/api/namespace.ts
+++ src/api/namespace.ts
@@ -2,8 +2,7 @@ import { HubAPI } from './hub';
 
 class API extends HubAPI {
   // apiPath = this.getUIPath('namespaces/');
-  apiPath =
-    'http://localhost:8002/api/automation-hub/pulp/api/v3/content/ansible/namespaces/';
+  apiPath = '/pulp/api/v3/content/ansible/namespaces/';
 
   get(id: string, params = {}) {
     return this.http.get(this.apiPath + id + '/', { params });
