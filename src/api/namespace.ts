2508cc9623b8c3321d0c52cc46063285f7ac4594
Shaiah Emigh-Doyle <semighdo@redhat.com>
2023-05-09 10:44:42 -0400

    use v3/namespace
    Issue: AAH-2190

--- src/api/namespace.ts
+++ src/api/namespace.ts
@@ -1,14 +1,12 @@
 import { HubAPI } from './hub';
 
 class API extends HubAPI {
-  apiPath = this.getUIPath('namespaces/');
+  // apiPath = this.getUIPath('namespaces/');
+  apiPath =
+    'http://localhost:8002/api/automation-hub/pulp/api/v3/content/ansible/namespaces/';
 
   get(id: string, params = {}) {
-    // return this.http.get(this.apiPath + id + '/', { params });
-    return this.http.get(
-      'http://localhost:8002/api/automation-hub/pulp/api/v3/content/ansible/namespaces/',
-      { params },
-    );
+    return this.http.get(this.apiPath + id + '/', { params });
   }
 }
 
