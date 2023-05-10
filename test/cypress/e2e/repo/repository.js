44c63affe027f6181b95e12d36bca0bfa452f8c1
MilanPospisil <arkanus@seznam.cz>
2023-05-10 13:32:20 +0200

    Add repo

--- test/cypress/e2e/repo/repository.js
+++ test/cypress/e2e/repo/repository.js
@@ -7,9 +7,30 @@ describe('Repository', () => {
   });
 
   beforeEach(() => {
-    cy.visit(uiPrefix);
     cy.login();
   });
 
-  it('checks for empty state', () => {});
+  it('Create repository', () => {
+    cy.visit(uiPrefix + 'ansible/repositories');
+    cy.contains('Repositories');
+    cy.contains('button', 'Add repository').click();
+    cy.contains('Add new repository');
+    cy.get('[data-cy="Page-AnsibleRepositoryEdit"] input[id="name"]').type(
+      'repo1Test',
+    );
+    cy.get(
+      '[data-cy="Page-AnsibleRepositoryEdit"] input[id="description"]',
+    ).type('repo1Test description');
+
+    cy.get('[data-cy="pipeline"] button').click();
+    cy.contains('[data-cy="pipeline"]', 'Staging');
+    cy.contains('[data-cy="pipeline"]', 'Approved');
+    cy.contains('[data-cy="pipeline"]', 'None');
+
+    cy.contains('[data-cy="pipeline"] button', 'Approved').click();
+    cy.contains(
+      '[data-cy="Page-AnsibleRepositoryEdit"] button',
+      'Save',
+    ).click();
+  });
 });
