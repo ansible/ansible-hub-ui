90bbc8745024b6ed58d2d6fbe6a5f748d5de5fb1
Shaiah Emigh-Doyle <semighdo@redhat.com>
2023-05-10 11:26:57 -0400

    fix api endpoint
    Issue: AAH-2190

--- src/api/namespace.ts
+++ src/api/namespace.ts
@@ -2,7 +2,7 @@ import { HubAPI } from './hub';
 
 class API extends HubAPI {
   // apiPath = this.getUIPath('namespaces/');
-  apiPath = '/pulp/api/v3/content/ansible/namespaces/';
+  apiPath = '/pulp/api/v3/pulp_ansible/namespaces/';
 
   get(id: string, params = {}) {
     return this.http.get(this.apiPath + id + '/', { params });
