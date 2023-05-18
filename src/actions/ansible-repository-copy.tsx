7f74e88f933ab83dc0392050ab611289f8ce1d1b
Martin Hradil <mhradil@redhat.com>
2023-05-18 23:07:38 +0000

    Copy CLI configuration - align with other implementation, use distro base_path
    when item.distributions is missing, load distribution, and either copy or alert no distribution
    when item.distributions is available, use disabled not alert
    
    (detail view will have distributions available, list won't)

--- src/actions/ansible-repository-copy.tsx
+++ src/actions/ansible-repository-copy.tsx
@@ -1,25 +1,60 @@
 import { t } from '@lingui/macro';
 import React from 'react';
-import { getRepoUrl } from 'src/utilities';
+import { AnsibleDistributionAPI } from 'src/api';
+import { getRepoURL } from 'src/utilities';
 import { Action } from './action';
 
 export const ansibleRepositoryCopyAction = Action({
   title: t`Copy CLI configuration`,
-  onClick: (item, { addAlert }) => {
+  onClick: async (item, { addAlert }) => {
+    let distribution = null;
+    if (!item.distributions) {
+      addAlert({
+        id: 'copy-cli-config',
+        title: t`Loading distribution...`,
+        variant: 'info',
+      });
+
+      distribution = (
+        await AnsibleDistributionAPI.list({
+          repository: item.pulp_href,
+        })
+      )?.data?.results?.[0];
+    } else {
+      distribution = item.distributions?.[0];
+    }
+
+    if (!distribution) {
+      addAlert({
+        id: 'copy-cli-config',
+        title: t`There are no distributions associated with this repository.`,
+        variant: 'danger',
+      });
+      return;
+    }
+
     const cliConfig = [
       '[galaxy]',
-      `server_list = ${item.name}_repo`,
+      `server_list = ${distribution.base_path}`,
       '',
-      `[galaxy_server.${item.name}_repo]`,
-      `url=${getRepoUrl(item.name)}`,
+      `[galaxy_server.${distribution.base_path}]`,
+      `url=${getRepoURL(distribution.base_path)}`,
       'token=<put your token here>',
     ].join('\n');
 
     navigator.clipboard.writeText(cliConfig);
     addAlert({
+      description: <pre>{cliConfig}</pre>,
+      id: 'copy-cli-config',
       title: t`Successfully copied to clipboard`,
       variant: 'success',
-      description: <pre>{cliConfig}</pre>,
     });
+  },
+  disabled: ({ distributions }) => {
+    if (distributions && !distributions.length) {
+      return t`There are no distributions associated with this repository.`;
+    }
+
+    return null;
   },
 });
