8de7832e269b225d87ff01c04d6872e7e078b097
Martin Hradil <mhradil@redhat.com>
2023-06-02 02:00:39 +0200

    Repository sync - modal to set mirror, optimize (#3786)
    * Repository sync - modal to set mirror, optimize
    
    Issue: AAH-2301
    
    * tests using repo sync - confirm modal

--- test/cypress/e2e/misc/task_list.js
+++ test/cypress/e2e/misc/task_list.js
@@ -16,6 +16,7 @@ describe('Task table contains correct headers and filter', () => {
 
     cy.get('[aria-label="Actions"]').eq(1).click();
     cy.get('tr').eq(2).contains('Sync').click();
+    cy.get('.pf-c-modal-box__footer .pf-m-primary').contains('Sync').click();
 
     cy.get('.pf-c-alert.pf-m-info');
   });
