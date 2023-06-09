1c897b9bd725f5f17a4d7f6b713a061a999ecff8
MilanPospisil <arkanus@seznam.cz>
2023-06-09 11:21:55 +0200

    Fix copy collection version test

--- test/cypress/e2e/collections/collection.js
+++ test/cypress/e2e/collections/collection.js
@@ -51,6 +51,38 @@ describe('collection tests', () => {
     );
   });
 
+  it('should copy collection version to validated repository', () => {
+    cy.deleteNamespacesAndCollections();
+    const rand = Math.floor(Math.random() * 9999999);
+    const namespace = `foo_${rand}`;
+    const collection = `bar_${rand}`;
+    cy.galaxykit(`-i collection upload ${namespace} ${collection}`);
+    cy.visit(`${uiPrefix}repo/published/${namespace}/${collection}`);
+
+    cy.get('[data-cy="kebab-toggle"]').click();
+    cy.get(
+      '[data-cy="copy-collection-version-to-repository-dropdown"]',
+    ).click();
+
+    cy.contains('Select repositories');
+    cy.get(
+      '[data-cy="ApproveModal-CheckboxRow-row-published"] .pf-c-table__check input',
+    ).should('be.disabled');
+
+    cy.get("[aria-label='name__icontains']").type('validate{enter}');
+    cy.get(
+      "[data-cy='ApproveModal-CheckboxRow-row-validated'] .pf-c-table__check input",
+    ).check();
+
+    cy.get('.pf-m-primary').contains('Select').click();
+
+    cy.get('[data-cy="AlertList"]').contains(
+      `Started adding ${namespace}.${collection} v1.0.0 from "published" to repository "validated".`,
+    );
+    cy.get('[data-cy="AlertList"]').contains('detail page').click();
+    cy.contains('Completed');
+  });
+
   it('deletes an collection from repository', () => {
     cy.deleteNamespacesAndCollections();
     cy.deleteRepositories();
