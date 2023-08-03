eae653d252a1c27c85086f85801b096068a7b47c
Martin Hradil <mhradil@redhat.com>
2023-08-03 16:01:14 +0000

    sort index.ts
    s/^export/import/; npm run prettier; s/^import/export/;

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,18 +1,44 @@
+export { ActiveUserAPI } from './active-user';
+export { ActivitiesAPI } from './activities';
+export { AnsibleDistributionAPI } from './ansible-distribution';
+export { AnsibleRemoteAPI } from './ansible-remote';
+export { AnsibleRepositoryAPI } from './ansible-repository';
+export { ApplicationInfoAPI } from './application-info';
+export { CertificateUploadAPI } from './certificate-upload';
 export { CollectionAPI } from './collection';
+export { CollectionVersionAPI } from './collection-version';
+export { ContainerDistributionAPI } from './container-distribution';
+export { ContainerTagAPI } from './container-tag';
+export { ControllerAPI } from './controller';
+export { ExecutionEnvironmentAPI } from './execution-environment';
+export { ExecutionEnvironmentNamespaceAPI } from './execution-environment-namespace';
+export { ExecutionEnvironmentRegistryAPI } from './execution-environment-registry';
+export { ExecutionEnvironmentRemoteAPI } from './execution-environment-remote';
+export { FeatureFlagsAPI } from './feature-flags';
+export { GenericPulpAPI } from './generic-pulp';
+export { GroupAPI } from './group';
+export { GroupRoleAPI } from './group-role';
+export { ImportAPI } from './import';
+export { LegacyNamespaceAPI } from './legacy-namespace';
+export { LegacyRoleAPI } from './legacy-role';
+export { MyNamespaceAPI } from './my-namespace';
+export { MySyncListAPI } from './my-synclist';
 export { NamespaceAPI } from './namespace';
+export { PulpAPI } from './pulp';
+export { Repositories } from './repositories';
+export { AnsibleRemoteType } from './response-types/ansible-remote';
 export {
-  NamespaceType,
-  NamespaceListType,
-  NamespaceLinkType,
-} from './response-types/namespace';
+  AnsibleRepositoryType,
+  AnsibleRepositoryVersionType,
+} from './response-types/ansible-repository';
 export {
   CollectionDetailType,
   CollectionListType,
   CollectionUploadType,
   CollectionUsedByDependencies,
   CollectionVersion,
-  CollectionVersionSearch,
   CollectionVersionContentType,
+  CollectionVersionSearch,
   ContentSummaryType,
   DocsBlobType,
   PluginContentType,
@@ -20,72 +46,46 @@ export {
   PluginOption,
   ReturnedValue,
 } from './response-types/collection';
-export { ImportListType, ImportDetailType } from './response-types/import';
-export { PulpStatus } from './response-types/pulp';
-export { ImportAPI } from './import';
-export { ActiveUserAPI } from './active-user';
 export {
-  GroupType,
-  ModelPermissionsType,
-  UserType,
-} from './response-types/user';
-export { CollectionVersionAPI } from './collection-version';
-export { MyNamespaceAPI } from './my-namespace';
-export { UserAPI } from './user';
-export { MySyncListAPI } from './my-synclist';
-export { SyncListType } from './response-types/synclists';
-export { TaskAPI } from './task';
-export { TaskType } from './response-types/task';
-export { GroupObjectPermissionType } from './response-types/permissions';
-export { GroupAPI } from './group';
-export { RoleAPI } from './role';
-export { ApplicationInfoAPI } from './application-info';
-export { RemoteType } from './response-types/remote';
-export { ExecutionEnvironmentAPI } from './execution-environment';
-export { ExecutionEnvironmentRegistryAPI } from './execution-environment-registry';
-export {
-  ExecutionEnvironmentType,
   ContainerManifestType,
   ContainerRepositoryType,
+  ExecutionEnvironmentType,
 } from './response-types/execution-environment';
-export { ExecutionEnvironmentRemoteAPI } from './execution-environment-remote';
-export { WriteOnlyFieldType } from './response-types/write-only-field';
-export { ActivitiesAPI } from './activities';
-export { ContainerTagAPI } from './container-tag';
 export { FeatureFlagsType } from './response-types/feature-flags';
-export { FeatureFlagsAPI } from './feature-flags';
-export { ContainerDistributionAPI } from './container-distribution';
-export { ExecutionEnvironmentNamespaceAPI } from './execution-environment-namespace';
-export { ControllerAPI } from './controller';
-export { TaskManagementAPI } from './task-management';
-export { GroupRoleAPI } from './group-role';
-export { GenericPulpAPI } from './generic-pulp';
-export { PulpAPI } from './pulp';
-export { SettingsAPI } from './settings';
-export { SettingsType } from './response-types/settings';
-export { SignCollectionAPI } from './sign-collections';
-export { SigningServiceAPI, SigningServiceType } from './signing-service';
-export { RoleType, GroupRoleType } from './response-types/role';
-export { CertificateUploadAPI } from './certificate-upload';
-export { Repositories } from './repositories';
-export { AnsibleDistributionAPI } from './ansible-distribution';
-export { AnsibleRemoteAPI } from './ansible-remote';
-export { AnsibleRemoteType } from './response-types/ansible-remote';
-export { AnsibleRepositoryAPI } from './ansible-repository';
+export { ImportDetailType, ImportListType } from './response-types/import';
 export {
-  AnsibleRepositoryType,
-  AnsibleRepositoryVersionType,
-} from './response-types/ansible-repository';
-export { SignContainersAPI } from './sign-containers';
+  LegacyNamespaceDetailType,
+  LegacyNamespaceListType,
+} from './response-types/legacy-namespace';
 export {
   LegacyRoleDetailType,
   LegacyRoleListType,
   LegacyRoleVersionDetailType,
 } from './response-types/legacy-role';
 export {
-  LegacyNamespaceDetailType,
-  LegacyNamespaceListType,
-} from './response-types/legacy-namespace';
-export { LegacyRoleAPI } from './legacy-role';
-export { LegacyNamespaceAPI } from './legacy-namespace';
+  NamespaceLinkType,
+  NamespaceListType,
+  NamespaceType,
+} from './response-types/namespace';
+export { GroupObjectPermissionType } from './response-types/permissions';
+export { PulpStatus } from './response-types/pulp';
+export { RemoteType } from './response-types/remote';
+export { GroupRoleType, RoleType } from './response-types/role';
+export { SettingsType } from './response-types/settings';
+export { SyncListType } from './response-types/synclists';
+export { TaskType } from './response-types/task';
+export {
+  GroupType,
+  ModelPermissionsType,
+  UserType,
+} from './response-types/user';
+export { WriteOnlyFieldType } from './response-types/write-only-field';
+export { RoleAPI } from './role';
+export { SettingsAPI } from './settings';
+export { SignCollectionAPI } from './sign-collections';
+export { SignContainersAPI } from './sign-containers';
+export { SigningServiceAPI, SigningServiceType } from './signing-service';
+export { TaskAPI } from './task';
+export { TaskManagementAPI } from './task-management';
+export { UserAPI } from './user';
 export { WisdomDenyIndexAPI } from './wisdom-deny-index';
