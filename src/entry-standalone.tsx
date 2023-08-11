4e7783145ed9997d49ffc5f04ca5d396a811895e
Martin Hradil <mhradil@redhat.com>
2023-08-11 00:11:07 +0000

    simplify standalone redirect

--- src/entry-standalone.tsx
+++ src/entry-standalone.tsx
@@ -6,27 +6,20 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import 'src/l10n';
 import App from './loaders/standalone/loader';
 
-// support common original galaxy urls
-// return falsy for default, or an url starting with a slash
-function redirect(from) {
-  console.debug('pathname outside base path', from);
-
-  // match /namespace/name & /namespace/name/
-  if (from.match(/^\/(\w+)\/(\w+)\/?$/)) {
-    return '/dispatch/?pathname=' + encodeURIComponent(from);
-  }
-}
-
 // Entrypoint for compiling the app to run in standalone mode
 
-// react-router v6 won't redirect to base path by default
-if (!window.location.pathname.includes(UI_BASE_PATH)) {
-  window.history.pushState(
-    null,
-    null,
-    UI_BASE_PATH.replace(/\/$/, '') +
-      (redirect(window.location.pathname) || '/'),
-  );
+if (!window.location.pathname.startsWith(UI_BASE_PATH)) {
+  // react-router v6 won't redirect to base path by default
+  // also support old-galaxy /namespace/name/ urls
+  const originalPath = window.location.pathname;
+  const newPath = originalPath.match(/^\/(\w+)\/(\w+)\/?$/)
+    ? UI_BASE_PATH.replace(
+        /\/$/,
+        '/dispatch/?pathname=' + encodeURIComponent(originalPath),
+      )
+    : UI_BASE_PATH;
+
+  window.history.pushState(null, null, newPath);
 }
 
 ReactDOM.render(
