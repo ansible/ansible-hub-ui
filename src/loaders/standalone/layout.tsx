b7d4b566c2f3347a537ddbbe7e404e7480776d90
Martin Hradil <mhradil@redhat.com>
2023-08-21 02:24:41 +0200

    Add banner linking discussions on beta galaxy (#4128)
    * Add banner linking discussions on beta galaxy
    
    Issue: AAH-1893
    
    * use Banner
    
    * add feature flag check
    
    using ai_deny_index to mean community

--- src/loaders/standalone/layout.tsx
+++ src/loaders/standalone/layout.tsx
@@ -1,5 +1,6 @@
 import { Trans, t } from '@lingui/macro';
 import {
+  Banner,
   DropdownItem,
   DropdownSeparator,
   Page,
@@ -168,6 +169,22 @@ export const StandaloneLayout = ({
 
   return (
     <Page isManagedSidebar={true} header={Header} sidebar={Sidebar}>
+      {featureFlags?.ai_deny_index ? (
+        <Banner>
+          <Trans>
+            Thanks for trying out the new and improved Beta Galaxy, please share
+            your feedback on{' '}
+            <a
+              href='https://github.com/ansible/galaxy_ng/discussions'
+              target='_blank'
+              rel='noreferrer'
+            >
+              github.com/ansible/galaxy_ng/discussions
+            </a>
+            .
+          </Trans>
+        </Banner>
+      ) : null}
       {children}
       {aboutModalVisible && aboutModal}
     </Page>
