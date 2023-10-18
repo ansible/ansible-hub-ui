1609f5f0874b0b84280eb9057fe65043cfcee250
Martin Hradil <mhradil@redhat.com>
2023-10-18 16:18:39 +0200

    insights: run gettext extract&compile before build-insights (#4421)
    No-Issue

--- ci.sh
+++ ci.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
+set -e
 
-# setNpmOrYarn
-USES_NPM=true
-
-# install dependencies
 npm ci
-npm i -g npm-run-all
 
-# build
-if [ $IS_PR = true ]; then
-  npm-run-all build-insights 'lint:!(po|yaml)'
-else
-  npm run deploy
-fi
+npm run gettext:extract
+npm run gettext:compile
+npm run build-insights
 
 # do not use dev dockerfile
-rm $APP_ROOT/Dockerfile
-rm $APP_ROOT/.dockerignore
+rm "$APP_ROOT"/Dockerfile "$APP_ROOT"/.dockerignore || true
