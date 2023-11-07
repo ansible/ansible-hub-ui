e75d327de862f837abc627a03e5e559d12e66cdb
Martin Hradil <mhradil@redhat.com>
2023-11-07 00:52:28 +0000

    list-item-actions: functional, fix import

--- src/components/list-item-actions/list-item-actions.tsx
+++ src/components/list-item-actions/list-item-actions.tsx
@@ -1,38 +1,37 @@
 import { List } from '@patternfly/react-core';
 import React from 'react';
-import { StatefulDropdown } from '../patternfly-wrappers/stateful-dropdown';
+import { StatefulDropdown } from '../shared/stateful-dropdown';
 
 interface IProps {
   kebabItems?: React.ReactNode[];
   buttons?: React.ReactNode[];
 }
-export class ListItemActions extends React.Component<IProps> {
-  render() {
-    const buttons = this.props.buttons?.filter(Boolean);
-    const kebabItems = this.props.kebabItems?.filter(Boolean);
-    const anyButtons = buttons?.length;
-    const anyKebab = kebabItems?.length;
 
-    return (
-      <td
-        style={{
-          paddingRight: anyKebab ? '0px' : '16px',
-          textAlign: 'right',
-          display: 'flex',
-          justifyContent: 'flex-end',
-        }}
-      >
-        {anyButtons ? (
-          <>
-            <List>{buttons}</List>{' '}
-          </>
-        ) : null}
-        {anyKebab ? (
-          <div data-cy='kebab-toggle'>
-            <StatefulDropdown items={kebabItems} />{' '}
-          </div>
-        ) : null}
-      </td>
-    );
-  }
+export function ListItemActions(props: IProps) {
+  const buttons = props.buttons?.filter(Boolean);
+  const kebabItems = props.kebabItems?.filter(Boolean);
+  const anyButtons = buttons?.length;
+  const anyKebab = kebabItems?.length;
+
+  return (
+    <td
+      style={{
+        paddingRight: anyKebab ? '0px' : '16px',
+        textAlign: 'right',
+        display: 'flex',
+        justifyContent: 'flex-end',
+      }}
+    >
+      {anyButtons ? (
+        <>
+          <List>{buttons}</List>{' '}
+        </>
+      ) : null}
+      {anyKebab ? (
+        <div data-cy='kebab-toggle'>
+          <StatefulDropdown items={kebabItems} />{' '}
+        </div>
+      ) : null}
+    </td>
+  );
 }
