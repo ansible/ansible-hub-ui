1865c8908c56320f00bb9eb2261ad63fd4dacd52
Martin Hradil <mhradil@redhat.com>
2023-11-23 16:53:50 +0100

    title - set "Ansible Galaxy" for community (#4578)
    * title - set Ansible Galaxy for community
    
    * No-Issue

--- config/community.prod.webpack.config.js
+++ config/community.prod.webpack.config.js
@@ -3,14 +3,15 @@ const webpackBase = require('./webpack.base.config');
 
 // Compile configuration for stnadalone mode
 module.exports = webpackBase({
-  API_HOST: '',
   API_BASE_PATH: '/api/',
-  UI_BASE_PATH: '/ui/',
+  API_HOST: '',
+  APPLICATION_NAME: 'Ansible Galaxy',
   DEPLOYMENT_MODE: 'standalone',
   IS_COMMUNITY: true,
   NAMESPACE_TERM: 'namespaces',
-  UI_USE_HTTPS: false,
+  UI_BASE_PATH: '/ui/',
   UI_DEBUG: false,
   UI_EXTERNAL_LOGIN_URI: '/login/github/',
+  UI_USE_HTTPS: false,
   WEBPACK_PUBLIC_PATH: '/',
 });
