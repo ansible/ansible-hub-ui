fd660871da915feaeb7e83593afa78156db6b9cf
Martin Hradil <mhradil@redhat.com>
2023-11-28 04:53:37 +0100

    [stable-4.8] Drop @redhat-cloud-services/frontend-components-config (#4619)
    * Bump @redhat-cloud-services/frontend-components-config to 6.0.5
    
    add babel-loader since f-c-c 6 no longer depends on it,
    
    No-Issue
    
    * webpack - drop shared config, redo locally, clean up
    
    fcc 6 seems to insist on pf5 and apparently also fec.config.js
    
    makes sense for master, but simplifying for stable branches
    
    * lodash

--- config/standalone.prod.webpack.config.js
+++ config/standalone.prod.webpack.config.js
@@ -7,7 +7,6 @@ module.exports = webpackBase({
   API_BASE_PATH: '/api/galaxy/',
   UI_BASE_PATH: '/ui/',
   UI_USE_HTTPS: false,
-  UI_DEBUG: false,
   UI_EXTERNAL_LOGIN_URI: '/login',
   WEBPACK_PUBLIC_PATH: '/static/galaxy_ng/',
 });
