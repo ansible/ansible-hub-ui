6e51890d5dcbd1cef2e89cbd86cd8a211ea359e2
Martin Hradil <mhradil@redhat.com>
2023-12-01 02:41:49 +0000

    aargh, is it a timing bug?

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -41,6 +41,7 @@ Cypress.Commands.add('assertTitle', {}, (title) => {
 });
 
 Cypress.Commands.add('openHeaderKebab', {}, () => {
+  cy.wait(500);
   cy.scrollTo(0, 0, { ensureScrollable: false });
   cy.get('[data-cy="kebab-toggle"] [aria-label="Actions"]').click();
 });
