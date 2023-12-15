1ef2601c2f228c6672e41893fbdc5c131e11c4a5
Martin Hradil <mhradil@redhat.com>
2023-12-15 21:14:44 +0000

    Fix `cy.get()` could not find a registered alias for: `@taskStatus.last`.
    No-Issue

--- test/cypress/e2e/collections/collection.js
+++ test/cypress/e2e/collections/collection.js
@@ -1,16 +1,3 @@
-const waitForTaskToFinish = (task, maxRequests, level = 0) => {
-  if (level === maxRequests) {
-    throw `Maximum requests exceeded.`;
-  }
-
-  cy.wait(task).then(({ response }) => {
-    if (response.body.state !== 'completed') {
-      cy.wait(1000);
-      waitForTaskToFinish(task, maxRequests, level + 1);
-    }
-  });
-};
-
 describe('collection tests', () => {
   before(() => {
     cy.deleteNamespacesAndCollections();
@@ -22,30 +9,15 @@ describe('collection tests', () => {
 
   it('deletes an entire collection', () => {
     cy.galaxykit('-i collection upload test_namespace test_collection');
+    cy.galaxykit('task wait all');
+
     cy.visit('/ui/repo/published/test_namespace/test_collection');
 
     cy.get('[data-cy=kebab-toggle]').click();
     cy.get('[data-cy=delete-collection-dropdown]').click();
     cy.get('input[id=delete_confirm]').click();
-
-    cy.intercept(
-      'DELETE',
-      Cypress.env('prefix') +
-        'v3/plugin/ansible/content/published/collections/index/test_namespace/test_collection',
-    ).as('deleteCollection');
-    cy.intercept('GET', Cypress.env('prefix') + '/v3/tasks/*').as('taskStatus');
-
     cy.get('button').contains('Delete').click();
-
-    cy.wait('@deleteCollection').its('response.statusCode').should('eq', 202);
-
-    waitForTaskToFinish('@taskStatus', 10);
-    cy.get('@taskStatus.last').then(() => {
-      cy.get('h4[class=pf-c-alert__title]').should(
-        'have.text',
-        'Success alert:Collection "test_collection" has been successfully deleted.',
-      );
-    });
+    cy.contains('No collections yet', { timeout: 10000 });
   });
 
   it('deletes a collection version', () => {
