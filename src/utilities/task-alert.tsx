693319d16b9116790522eb89936c683b85cd9d63
Martin Hradil <mhradil@redhat.com>
2023-12-17 03:00:54 +0000

    taskAlert - handle unknown task

--- src/utilities/task-alert.tsx
+++ src/utilities/task-alert.tsx
@@ -1,10 +1,13 @@
 import { Trans } from '@lingui/macro';
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { Link } from 'react-router-dom';
 import { Paths, formatPath } from 'src/paths';
 import { parsePulpIDFromURL } from './parse-pulp-id';
 
 type VariantType = 'default' | 'success' | 'danger' | 'warning' | 'info';
+
+const MaybeLink = ({ to, children }: { to: string; children: ReactNode }) =>
+  to ? <Link to={to}>{children}</Link> : <>{children}</>;
 
 // task can be { task: (pulp_href) } or "(pulp_href)" or "(uuid)"
 export const taskAlert = (task, title, variant: VariantType = 'info') => ({
@@ -14,13 +17,13 @@ export const taskAlert = (task, title, variant: VariantType = 'info') => ({
     <span>
       <Trans>
         See the task management{' '}
-        <Link
+        <MaybeLink
           to={formatPath(Paths.taskDetail, {
             task: parsePulpIDFromURL(task?.task || task),
           })}
         >
           detail page{' '}
-        </Link>
+        </MaybeLink>
         for the status of this task.
       </Trans>
     </span>
