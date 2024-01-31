95856f8d7819dea2f885a2dcbcba7205a78a92f6
Daniel Rodowicz <drodowic@redhat.com>
2024-01-31 10:30:45 -0500

    support preview and stable assets in frontend container build
    Issue: AAH-3039

--- ci.sh
+++ ci.sh
@@ -5,7 +5,22 @@ npm ci
 
 npm run gettext:extract
 npm run gettext:compile
-npm run build-insights
+
+if [ "$IS_PR" == true ]; then
+    npm run build-insights
+else
+    export HUB_CLOUD_BETA=false
+    npm run build-insights
+    mv ${DIST_FOLDER} stable
+
+    export HUB_CLOUD_BETA=true
+    npm run build-insights
+    mv ${DIST_FOLDER} preview
+    
+    mkdir -p ${DIST_FOLDER}
+    mv stable ${DIST_FOLDER}/stable
+    mv preview ${DIST_FOLDER}/preview
+fi
 
 # do not use dev dockerfile
 rm "$APP_ROOT"/Dockerfile "$APP_ROOT"/.dockerignore || true
