98dfe63bc00665364779e70af07028fec429950e
Martin Hradil <mhradil@redhat.com>
2024-03-27 14:06:59 +0000

    functional Typeahead

--- src/components/typeahead.tsx
+++ src/components/typeahead.tsx
@@ -4,118 +4,74 @@ import {
   SelectOption,
   SelectVariant,
 } from '@patternfly/react-core/deprecated';
-import React, { type CSSProperties, Component, type ReactElement } from 'react';
+import React, { type CSSProperties, type ReactElement, useState } from 'react';
 
 interface IProps {
-  results: { name: string; id: number | string }[];
-  loadResults: (filter: string) => void;
-  onSelect: (event, selection, isPlaceholder) => void;
-
-  selections?: { name: string; id: number | string }[];
-  multiple?: boolean;
-  placeholderText?: string;
-  onClear?: () => void;
-
   isDisabled?: boolean;
-  endLink?: {
-    href: string;
-    name: string;
-  };
+  loadResults: (filter: string) => void;
   menuAppendTo?: 'parent' | 'inline';
-  toggleIcon?: ReactElement;
+  multiple?: boolean;
+  onClear?: () => void;
+  onSelect: (event, selection, isPlaceholder) => void;
+  placeholderText?: string;
+  results: { name: string; id: number | string }[];
+  selections?: { name: string; id: number | string }[];
   style?: CSSProperties;
+  toggleIcon?: ReactElement;
 }
 
-interface IState {
-  isOpen: boolean;
-}
+export const Typeahead = ({
+  isDisabled,
+  loadResults,
+  menuAppendTo,
+  multiple,
+  onClear,
+  onSelect,
+  placeholderText,
+  results,
+  selections,
+  style,
+  toggleIcon,
+}: IProps) => {
+  const [isOpen, setOpen] = useState(false);
+  const selected = selections?.map((group) => group.name) || null;
 
-export class Typeahead extends Component<IProps, IState> {
-  constructor(props) {
-    super(props);
+  return (
+    <Select
+      hasInlineFilter
+      isDisabled={isDisabled}
+      isOpen={isOpen}
+      menuAppendTo={menuAppendTo}
+      onClear={() => {
+        loadResults('');
+        onClear?.();
+      }}
+      onFilter={(_e, value) => {
+        loadResults(value);
+      }}
+      onSelect={(event, selection, isPlaceholder) => {
+        onSelect(event, selection, isPlaceholder);
 
-    this.state = {
-      isOpen: false,
-    };
-  }
-
-  render() {
-    let selected = null;
-    if (this.props.selections) {
-      selected = this.props.selections.map((group) => group.name);
-    }
-
-    const { isOpen } = this.state;
-    const variant = this.props.multiple
-      ? SelectVariant.typeaheadMulti
-      : SelectVariant.typeahead;
-
-    return (
-      <Select
-        menuAppendTo={this.props.menuAppendTo}
-        onClear={this.onClear}
-        onSelect={this.onSelect}
-        onToggle={(_event, isOpen) => this.onToggle(isOpen)}
-        variant={variant}
-        selections={selected}
-        isOpen={isOpen}
-        hasInlineFilter
-        onFilter={this.onFilter}
-        placeholderText={this.props.placeholderText}
-        isDisabled={this.props.isDisabled}
-        toggleIcon={this.props.toggleIcon}
-        style={this.props.style}
-      >
-        {this.getOptions()}
-      </Select>
-    );
-  }
-
-  private onClear = () => {
-    this.props.loadResults('');
-    if (this.props.onClear) {
-      this.props.onClear();
-    }
-  };
-
-  private getOptions() {
-    const options = this.props.results.map(({ id, name }) => (
-      <SelectOption key={id} value={name} />
-    ));
-
-    if (options.length === 0) {
-      options.push(
-        <SelectOption key={'not_found'} value={t`Not found`} isDisabled />,
-      );
-    }
-
-    return options;
-  }
-
-  private onFilter = (evt) => {
-    if (evt !== null) {
-      const textInput = evt.target.value;
-      this.props.loadResults(textInput);
-    }
-    return this.getOptions();
-  };
-
-  private onToggle = (isOpen) => {
-    this.setState({
-      isOpen,
-    });
-  };
-
-  private onSelect = (event, selection, isPlaceholder) => {
-    this.props.onSelect(event, selection, isPlaceholder);
-
-    if (!this.props.multiple) {
-      this.setState(
-        {
-          isOpen: false,
-        },
-        () => this.props.loadResults(''),
-      );
-    }
-  };
-}
+        if (!multiple) {
+          setOpen(false);
+          loadResults('');
+        }
+      }}
+      onToggle={(_e, isOpen) => setOpen(isOpen)}
+      placeholderText={placeholderText}
+      selections={selected}
+      style={style}
+      toggleIcon={toggleIcon}
+      variant={
+        multiple ? SelectVariant.typeaheadMulti : SelectVariant.typeahead
+      }
+    >
+      {results.map(({ id, name }) => (
+        <SelectOption key={id} value={name} />
+      ))}
+      {results.length === 0 ? (
+        <SelectOption key={'not_found'} value={t`Not found`} isDisabled />
+      ) : null}
+    </Select>
+  );
+};
