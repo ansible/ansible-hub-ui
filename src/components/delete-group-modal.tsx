b7abb9c58b9d66104628703080345d4177d97f11
Martin Hradil <mhradil@redhat.com>
2024-05-10 21:31:56 +0000

    functional DeleteGroupModal

--- src/components/delete-group-modal.tsx
+++ src/components/delete-group-modal.tsx
@@ -1,78 +1,76 @@
 import { Trans, t } from '@lingui/macro';
 import { List, ListItem } from '@patternfly/react-core';
-import React, { Component } from 'react';
+import React from 'react';
 import { type UserType } from 'src/api';
 import { Alert, DeleteModal, Spinner } from 'src/components';
 
 interface IProps {
-  count?: number;
+  canViewUsers?: boolean;
   cancelAction: () => void;
+  count?: number;
   deleteAction: () => void;
   name: string;
   users?: UserType[];
-  canViewUsers?: boolean;
 }
 
-export class DeleteGroupModal extends Component<IProps> {
-  render() {
-    const { cancelAction, count, deleteAction, name, users, canViewUsers } =
-      this.props;
+export const DeleteGroupModal = ({
+  canViewUsers,
+  cancelAction,
+  count,
+  deleteAction,
+  name,
+  users,
+}: IProps) => (
+  <DeleteModal
+    cancelAction={cancelAction}
+    deleteAction={deleteAction}
+    title={t`Delete group?`}
+  >
+    <Trans>
+      <b>{name}</b> will be permanently deleted.
+    </Trans>
+    <p>&nbsp;</p>
+    <div>
+      {users && count > 10 && (
+        <p>
+          <Trans>Deleting this group will affect {count} users.</Trans>
+        </p>
+      )}
+      {users && count > 0 && count <= 10 && (
+        <>
+          <p>
+            <Trans>These users will lose access to the group content:</Trans>
+          </p>
+          <List>
+            {users.map((u) => (
+              <ListItem key={u.username}>
+                <b>{u.username}</b>
+              </ListItem>
+            ))}
+          </List>
+        </>
+      )}
 
-    return (
-      <DeleteModal
-        cancelAction={cancelAction}
-        deleteAction={deleteAction}
-        title={t`Delete group?`}
-      >
-        <Trans>
-          <b>{name}</b> will be permanently deleted.
-        </Trans>
-        <p>&nbsp;</p>
-        <div>
-          {users && count > 10 && (
+      {canViewUsers ? (
+        <>
+          {users && !count && <p>{t`No users will be affected.`}</p>}
+          {!users && (
             <p>
-              <Trans>Deleting this group will affect {count} users.</Trans>
+              <Trans>
+                Checking for affected users... <Spinner size='sm' />
+              </Trans>
             </p>
           )}
-          {users && count > 0 && count <= 10 && (
-            <>
-              <p>
-                <Trans>
-                  These users will lose access to the group content:
-                </Trans>
-              </p>
-              <List>
-                {users.map((u) => (
-                  <ListItem key={u.username}>
-                    <b>{u.username}</b>
-                  </ListItem>
-                ))}
-              </List>
-            </>
-          )}
-
-          {canViewUsers ? (
-            <>
-              {users && !count && <p>{t`No users will be affected.`}</p>}
-              {!users && (
-                <p>
-                  <Trans>
-                    Checking for affected users... <Spinner size='sm' />
-                  </Trans>
-                </p>
-              )}
-            </>
-          ) : (
-            <Alert
-              title={t`This group can include users`}
-              variant='warning'
-              isInline
-            >
-              <Trans>You don&apos;t have permission to display users.</Trans>
-            </Alert>
-          )}
-        </div>
-      </DeleteModal>
-    );
-  }
-}
+        </>
+      ) : (
+        <Alert
+          title={t`This group can include users`}
+          variant='warning'
+          isInline
+        >
+          <Trans>You don&apos;t have permission to display users.</Trans>
+        </Alert>
+      )}
+    </div>
+  </DeleteModal>
+);
