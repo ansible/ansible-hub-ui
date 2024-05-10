816473933372271eaef0b2d8efb9a208e986c758
Martin Hradil <mhradil@redhat.com>
2024-05-10 21:31:56 +0000

    functional PartnerHeader

--- src/components/partner-header.tsx
+++ src/components/partner-header.tsx
@@ -1,5 +1,5 @@
 import { t } from '@lingui/macro';
-import React, { Component, type ReactNode } from 'react';
+import React, { type ReactNode } from 'react';
 import { type NamespaceType } from 'src/api';
 import {
   BaseHeader,
@@ -13,71 +13,72 @@ import {
 import { namespaceTitle } from 'src/utilities';
 
 interface IProps {
-  namespace: NamespaceType;
-  tabs: LinkTabsProps['tabs'];
   breadcrumbs: BreadcrumbType[];
-
-  pageControls?: ReactNode;
   filters?: ReactNode;
+  namespace: NamespaceType;
+  pageControls?: ReactNode;
+  tabs: LinkTabsProps['tabs'];
 }
 
-export class PartnerHeader extends Component<IProps> {
-  render() {
-    const { breadcrumbs, filters, namespace, pageControls, tabs } = this.props;
+export const PartnerHeader = ({
+  breadcrumbs,
+  filters,
+  namespace,
+  pageControls,
+  tabs,
+}: IProps) => {
+  const title = namespaceTitle(namespace);
 
-    const title = namespaceTitle(namespace);
+  return (
+    <BaseHeader
+      title={title}
+      logo={
+        namespace.avatar_url && (
+          <Logo
+            alt={t`${title} logo`}
+            className='hub-header-image'
+            fallbackToDefault
+            image={namespace.avatar_url}
+            size='40px'
+            unlockWidth
+          />
+        )
+      }
+      breadcrumbs={<Breadcrumbs links={breadcrumbs} />}
+      pageControls={pageControls}
+      className='hub-header-bordered'
+    >
+      {namespace.description ? (
+        <div>
+          {namespace.description.startsWith('http') &&
+          !namespace.description.match(/\s/) ? (
+            <ExternalLink href={namespace.description}>
+              {namespace.description}
+            </ExternalLink>
+          ) : (
+            namespace.description
+          )}
+        </div>
+      ) : null}
 
-    return (
-      <BaseHeader
-        title={title}
-        logo={
-          namespace.avatar_url && (
-            <Logo
-              alt={t`${title} logo`}
-              className='hub-header-image'
-              fallbackToDefault
-              image={namespace.avatar_url}
-              size='40px'
-              unlockWidth
-            />
-          )
-        }
-        breadcrumbs={<Breadcrumbs links={breadcrumbs} />}
-        pageControls={pageControls}
-        className='hub-header-bordered'
-      >
-        {namespace.description ? (
-          <div>
-            {namespace.description.startsWith('http') &&
-            !namespace.description.match(/\s/) ? (
-              <ExternalLink href={namespace.description}>
-                {namespace.description}
-              </ExternalLink>
-            ) : (
-              namespace.description
-            )}
+      <div className='hub-tab-link-container'>
+        <div className='tabs'>
+          <LinkTabs tabs={tabs} />
+        </div>
+        {namespace.links.length > 0 ? (
+          <div className='links'>
+            {namespace.links.map((x, i) => {
+              return (
+                <div className='link' key={i}>
+                  <ExternalLink href={x.url}>{x.name}</ExternalLink>
+                </div>
+              );
+            })}
           </div>
         ) : null}
+      </div>
 
-        <div className='hub-tab-link-container'>
-          <div className='tabs'>
-            <LinkTabs tabs={tabs} />
-          </div>
-          {namespace.links.length > 0 ? (
-            <div className='links'>
-              {namespace.links.map((x, i) => {
-                return (
-                  <div className='link' key={i}>
-                    <ExternalLink href={x.url}>{x.name}</ExternalLink>
-                  </div>
-                );
-              })}
-            </div>
-          ) : null}
-        </div>
-
-        {filters || null}
-      </BaseHeader>
-    );
-  }
-}
+      {filters || null}
+    </BaseHeader>
+  );
+};
