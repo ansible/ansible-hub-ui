25349f0cec3bc819aa01fa8a318523351e7d30df
Martin Hradil <mhradil@redhat.com>
2024-05-10 21:31:56 +0000

    functional RoleForm

--- src/components/role-form.tsx
+++ src/components/role-form.tsx
@@ -10,149 +10,128 @@ import {
   TextInput,
   Title,
 } from '@patternfly/react-core';
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FormFieldHelper, PermissionCategories, Spinner } from 'src/components';
 
-interface IState {
-  permissions: string[];
-}
-
 interface IProps {
-  nameDisabled?: boolean;
-  name: string;
-  description: string;
-  onNameChange?: (value: string) => void;
-  nameValidated?: 'default' | 'warning' | 'success' | 'error';
-  nameHelperText?: string;
-  descriptionValidated: 'default' | 'warning' | 'success' | 'error';
-  descriptionHelperText: string;
-  onDescriptionChange: (value: string) => void;
-  saveRole: (permissions: string[]) => void;
   cancelRole: () => void;
+  description: string;
+  descriptionHelperText: string;
+  descriptionValidated: 'default' | 'warning' | 'success' | 'error';
   isSavingDisabled: boolean;
-  saving: boolean;
+  name: string;
+  nameDisabled?: boolean;
+  nameHelperText?: string;
+  nameValidated?: 'default' | 'warning' | 'success' | 'error';
+  onDescriptionChange: (value: string) => void;
+  onNameChange?: (value: string) => void;
   originalPermissions?: string[];
+  saveRole: (permissions: string[]) => void;
+  saving: boolean;
 }
 
-export class RoleForm extends Component<IProps, IState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      permissions: [],
-    };
-  }
+export const RoleForm = ({
+  cancelRole,
+  description,
+  descriptionHelperText,
+  descriptionValidated,
+  isSavingDisabled,
+  name,
+  nameDisabled,
+  nameHelperText,
+  nameValidated,
+  onDescriptionChange,
+  onNameChange,
+  originalPermissions,
+  saveRole,
+  saving,
+}: IProps) => {
+  const [permissions, setPermissions] = useState<string[]>([]);
+  useEffect(() => setPermissions(originalPermissions), []);
 
-  componentDidMount() {
-    if (this.props.originalPermissions) {
-      this.setState({ permissions: this.props.originalPermissions });
-    }
-  }
-
-  render() {
-    const { permissions } = this.state;
-    const {
-      name,
-      onNameChange,
-      nameValidated,
-      nameHelperText,
-      description,
-      descriptionValidated,
-      descriptionHelperText,
-      onDescriptionChange,
-      saveRole,
-      cancelRole,
-      nameDisabled,
-      isSavingDisabled,
-      saving,
-    } = this.props;
-
-    return (
-      <>
-        <Form>
-          <div>
-            <div style={{ paddingBottom: '8px' }}>
-              <Title headingLevel='h2'>{t`Details`}</Title>
-            </div>
-
-            <div style={{ display: 'flex', gap: '10px' }}>
-              <FormGroup
-                isRequired
-                key='name'
-                fieldId='name'
-                label={t`Name`}
-                style={{ width: '50%', float: 'left' }}
-              >
-                <InputGroup>
-                  <InputGroupItem isFill>
-                    <TextInput
-                      id='role_name'
-                      isDisabled={nameDisabled}
-                      value={name}
-                      onChange={(_event, value) => onNameChange(value)}
-                      type='text'
-                      validated={nameValidated}
-                      placeholder={t`Role name`}
-                    />
-                  </InputGroupItem>
-                </InputGroup>
-                <FormFieldHelper variant={nameValidated}>
-                  {nameHelperText}
-                </FormFieldHelper>
-              </FormGroup>
-
-              <FormGroup
-                isRequired
-                style={{ width: '50%' }}
-                key='description'
-                fieldId='description'
-                label={t`Description`}
-              >
-                <TextInput
-                  id='role_description'
-                  value={description}
-                  onChange={(_event, value) => onDescriptionChange(value)}
-                  type='text'
-                  validated={descriptionValidated}
-                  placeholder={t`Add a role description here`}
-                />
-                <FormFieldHelper variant={descriptionValidated}>
-                  {descriptionHelperText}
-                </FormFieldHelper>
-              </FormGroup>
-            </div>
-          </div>
-          <div>
-            <br />
-            <Divider />
-            <br />
-            <Title headingLevel='h2'>
-              <Trans>Permissions</Trans>
-            </Title>
-
-            <PermissionCategories
-              permissions={permissions}
-              setSelected={(permissions) => this.setState({ permissions })}
-              showEmpty
-            />
+  return (
+    <>
+      <Form>
+        <div>
+          <div style={{ paddingBottom: '8px' }}>
+            <Title headingLevel='h2'>{t`Details`}</Title>
           </div>
 
-          <ActionGroup>
-            <Button
-              variant='primary'
-              isDisabled={isSavingDisabled}
-              onClick={() => saveRole(permissions)}
+          <div style={{ display: 'flex', gap: '10px' }}>
+            <FormGroup
+              isRequired
+              key='name'
+              fieldId='name'
+              label={t`Name`}
+              style={{ width: '50%', float: 'left' }}
             >
-              {t`Save`}
-            </Button>
+              <InputGroup>
+                <InputGroupItem isFill>
+                  <TextInput
+                    id='role_name'
+                    isDisabled={nameDisabled}
+                    value={name}
+                    onChange={(_event, value) => onNameChange(value)}
+                    type='text'
+                    validated={nameValidated}
+                    placeholder={t`Role name`}
+                  />
+                </InputGroupItem>
+              </InputGroup>
+              <FormFieldHelper variant={nameValidated}>
+                {nameHelperText}
+              </FormFieldHelper>
+            </FormGroup>
 
-            <Button
-              variant='secondary'
-              onClick={cancelRole}
-            >{t`Cancel`}</Button>
-            {saving ? <Spinner /> : null}
-          </ActionGroup>
-        </Form>
-      </>
-    );
-  }
-}
+            <FormGroup
+              isRequired
+              style={{ width: '50%' }}
+              key='description'
+              fieldId='description'
+              label={t`Description`}
+            >
+              <TextInput
+                id='role_description'
+                value={description}
+                onChange={(_event, value) => onDescriptionChange(value)}
+                type='text'
+                validated={descriptionValidated}
+                placeholder={t`Add a role description here`}
+              />
+              <FormFieldHelper variant={descriptionValidated}>
+                {descriptionHelperText}
+              </FormFieldHelper>
+            </FormGroup>
+          </div>
+        </div>
+        <div>
+          <br />
+          <Divider />
+          <br />
+          <Title headingLevel='h2'>
+            <Trans>Permissions</Trans>
+          </Title>
+
+          <PermissionCategories
+            permissions={permissions}
+            setSelected={(permissions) => setPermissions(permissions)}
+            showEmpty
+          />
+        </div>
+
+        <ActionGroup>
+          <Button
+            variant='primary'
+            isDisabled={isSavingDisabled}
+            onClick={() => saveRole(permissions)}
+          >
+            {t`Save`}
+          </Button>
+
+          <Button variant='secondary' onClick={cancelRole}>{t`Cancel`}</Button>
+          {saving ? <Spinner /> : null}
+        </ActionGroup>
+      </Form>
+    </>
+  );
+};
