09dba513bfb838d6ef232841bce2822337e7431e
Martin Hradil <mhradil@redhat.com>
2024-05-16 11:52:58 +0000

    fix login redirect url to also include UI_BASE_PATH (#5047)
    No-Issue

--- src/utilities/login-url.ts
+++ src/utilities/login-url.ts
@@ -1,7 +1,8 @@
 // external login URL, assuming UI_EXETRNAL_LOGIN_URI and featureFlags.external_authentication are set
 export const loginURL = (next, featureFlags) => {
   if (featureFlags.dab_resource_registry && next) {
-    return `/redirect/?next=${encodeURIComponent(next)}`;
+    const fullPath = `${UI_BASE_PATH}/${next}`.replaceAll(/\/+/g, '/');
+    return `/redirect/?next=${encodeURIComponent(fullPath)}`;
   }
 
   return UI_EXTERNAL_LOGIN_URI;
