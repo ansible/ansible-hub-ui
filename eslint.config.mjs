88df492a2328c75e7a022ce27b77b6a66dd5ef49
Martin Hradil <mhradil@redhat.com>
2024-06-10 00:31:15 +0000

    update eslintrc

---
+++ eslint.config.mjs
@@ -0,0 +1,122 @@
+import eslint from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import reactPlugin from 'eslint-plugin-react';
+import globals from 'globals';
+import {
+  config,
+  parser,
+  configs as tsConfigs,
+  plugin as tsPlugin,
+} from 'typescript-eslint';
+
+// require('eslint-plugin-react/configs/recommended') does the right thing but can't be imported
+// and the .configs.recommended export adds flatconfig-invalid .plugins and .parserOptions .. remove
+const reactConfig = {
+  ...reactPlugin.configs.recommended,
+  plugins: { react: reactPlugin }, // fix for plugins: ['react']
+};
+delete reactConfig.parserOptions;
+
+export default config(
+  eslint.configs.recommended,
+  reactConfig,
+  ...tsConfigs.recommended,
+  ...tsConfigs.stylistic,
+  prettierConfig,
+  {
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      react: reactPlugin,
+    },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    rules: {
+      curly: ['error', 'all'],
+      'eol-last': ['error', 'always'],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { fixStyle: 'inline-type-imports' },
+      ],
+      'no-restricted-imports': [
+        'error',
+        {
+          paths: [
+            {
+              importNames: [
+                'ClipboardCopy',
+                'FileUpload',
+                'LabelGroup',
+                'LoginForm',
+                'Pagination',
+                'Tabs',
+                'Tooltip',
+              ],
+              message: 'Import from src/components instead.',
+              name: '@patternfly/react-core',
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: ['config/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-var-requires': 'off',
+    },
+  },
+  {
+    files: ['src/components/patternfly-wrappers/*.{js,jsx,ts,tsx}'],
+    rules: {
+      'no-restricted-imports': 'off',
+    },
+  },
+  {
+    files: ['test/**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        Cypress: 'readonly',
+        after: 'readonly',
+        before: 'readonly',
+        beforeEach: 'readonly',
+        cy: 'readonly',
+        describe: 'readonly',
+        expect: 'readonly',
+        it: 'readonly',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-var-requires': 'off',
+    },
+  },
+);
