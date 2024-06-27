22786a45b86285ce55c2e5cdafdaeaf3045813a8
Martin Hradil <mhradil@redhat.com>
2024-06-27 11:28:19 +0000

    add feature flag tests

---
+++ test/cypress/e2e/approval-modal/feature-flags.js
@@ -0,0 +1,21 @@
+const apiPrefix = Cypress.env('apiPrefix');
+
+describe('Feature flags', () => {
+  it('match expectations', () => {
+    cy.request(`${apiPrefix}_ui/v1/feature-flags/`).then(({ body }) => {
+      expect(body._messages).to.be.empty;
+      expect(body).to.include({ ai_deny_index: false });
+      expect(body).to.include({ can_create_signatures: true });
+      expect(body).to.include({ can_upload_signatures: false });
+      expect(body).to.include({ collection_auto_sign: true });
+      expect(body).to.include({ collection_signing: true });
+      expect(body).to.include({ container_signing: true });
+      expect(body).to.include({ display_repositories: true });
+      expect(body).to.include({ display_signatures: true });
+      expect(body).to.include({ execution_environments: true });
+      expect(body).to.include({ legacy_roles: false });
+      expect(body).to.include({ require_upload_signatures: false });
+      expect(body).to.include({ signatures_enabled: true });
+    });
+  });
+});
